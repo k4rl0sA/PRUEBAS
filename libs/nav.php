d6a509c3d52c2e53f6926b7db0009f7a394c38d1
k4rl0sA <carloseduardoacevedo@gmail.com>
2023-09-30 16:24:17 -0500

    2023-09-30v2

--- libs/nav.php
+++ libs/nav.php
@@ -14,7 +14,7 @@
 <?php
 // require_once 'config.php';
 ini_set('display_errors','1');
-$vers='1.09.301';
+$vers='1.09.30';
 // if (!isset($_SESSION["us_riesgo"])){ die("<script>window.top.location.href = '/';</script>");}
 require_once $_SERVER['DOCUMENT_ROOT'].'/libs/gestion.php';
   $sql="SELECT *

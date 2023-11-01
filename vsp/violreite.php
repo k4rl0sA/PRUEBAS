e0f25bf8e43a329c9b187aa36914e284834fec0c
k4rl0sA <carloseduardoacevedo@gmail.com>
2023-11-01 10:22:54 -0500

    11-01Fechas

--- vsp/violreite.php
+++ vsp/violreite.php
@@ -75,7 +75,7 @@ function cmp_violreite(){
   
 
 	$c[]=new cmp('id_violreite','h','50',$_POST['id'],$w.' '.$o,'','id_violreite',null,null,false,true,'','col-2');
-  $c[]=new cmp('fecha_seg','d','10',$d,$w.' '.$o,'Fecha Seguimiento','fecha_seg',null,null,true,true,'','col-2','validDate(this,-20,0)');
+  $c[]=new cmp('fecha_seg','d','10',$d,$w.' '.$o,'Fecha Seguimiento','fecha_seg',null,null,true,true,'','col-2','validDate(this,-32,0)');
   $c[]=new cmp('numsegui','s','3',$d,$w.' '.$o,'Seguimiento N°','numsegui',null,null,true,true,'','col-2');
   $c[]=new cmp('evento','s','3',$ev,$w.' '.$o,'Evento','evento',null,null,false,false,'','col-2');
   $c[]=new cmp('estado_s','s','3',$d,$w.' '.$o,'Estado','estado_s',null,null,true,true,'','col-2',"enabFielSele(this,true,['motivo_estado'],['3']);EnabEfec(this,['hab','acc'],['Ob'],['nO'],['bL']);");

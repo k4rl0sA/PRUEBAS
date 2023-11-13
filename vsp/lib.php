9a5ab0aff61833f42ddfdcb4f6569372f1f9367a
k4rl0sA <carloseduardoacevedo@gmail.com>
2023-11-13 14:18:41 -0500

    11-13GestantesV3

--- vsp/lib.php
+++ vsp/lib.php
@@ -84,12 +84,12 @@ function whe_homes() {
 			$sql .= " AND DATE(H.fecha_create) BETWEEN '".$_POST['fdes']."' AND '".$_POST['fdes']."'";
 		}
 	}
-	if(!$_POST['fbinas'] && !$_POST['fsector'] && !$_POST['fmanz'] && !$_POST['fpred']){
+	// if(!$_POST['fbinas'] && !$_POST['fsector'] && !$_POST['fmanz'] && !$_POST['fpred']){
 		$sql.=" AND ((H.usu_creo='{$_SESSION['us_sds']}' AND D.doc_asignado='{$_SESSION['us_sds']}') OR (H.equipo in(select equipo from usuarios where id_usuario = '{$_SESSION['us_sds']}') AND estado_v=7))";
 		//OR (H.equipo in(select equipo from usuarios where id_usuario = '{$_SESSION['us_sds']}') AND (D.doc_asignado='{$_SESSION['us_sds']}'))";
-	}else{
+	/* }else{
 		$sql.=" AND H.usu_creo='{$_SESSION['us_sds']}' AND D.doc_asignado='{$_SESSION['us_sds']}'";
-	}
+	} */
 	return $sql;
 }
 

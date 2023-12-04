1f4f63ac346fe3a8ae7384df41685d393b6b26af
Ferchokar <114622263+Ferchokar@users.noreply.github.com>
2023-12-04 13:57:30 -0500

    04-12-23_comentar

--- claves.php
+++ claves.php
@@ -3,10 +3,10 @@ $claves=[
     'publica'=>'6Ld47tUoAAAAAHpJPDuP5w3keyiFw9A9RhjRO0GM',
     'privada'=>'6Ld47tUoAAAAAKdFwES-feKB0XOPl1QE9K-GS4Hw'];
 
-$fechas=[
+/*$fechas=[
 
 'vsp'='-70',
 
 'hog'='-60'
-];
+];*/
     

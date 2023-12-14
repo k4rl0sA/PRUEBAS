4f48d0c184c2604395d7bcae0e2c7e59c7c88b8f
k4rl0sA <carloseduardoacevedo@gmail.com>
2023-12-14 15:08:06 -0500

    12-14Ajustes

--- ajustes/lib.php
+++ ajustes/lib.php
@@ -160,8 +160,8 @@ function gra_ajustar(){
 		$sexo_old=$info['responseResult'][0]['sexo_old'];
 
 		$sql="INSERT INTO ajustes VALUES (null,
-		$cod_pred,$cod_fam,$cod_individuo,$formulario,$accion,$cod_delete,$cod_traslada,$cmp_editar,
-		$tipodoc_old,$tipo_doc_new,$doc_old,$documento_new,$fecha_old,$fecha_new,$sexo_old,$sexo_new,'',
+		'$cod_pred','$cod_fam','$cod_individuo','$formulario','$accion','$cod_delete','$cod_traslada','$cmp_editar',
+		'$tipodoc_old','$tipo_doc_new','$doc_old','$documento_new','$fecha_old','$fecha_new','$sexo_old','$sexo_new','',
 		'{$_SESSION['us_sds']}',DATE_SUB(NOW(), INTERVAL 5 HOUR),'',NULL,'A');";
 		echo $sql;
 		$rta=dato_mysql($sql);

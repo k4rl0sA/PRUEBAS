c06d691cbb7e4809e2e3df725c5a1e1ff6baca38
k4rl0sA <carloseduardoacevedo@gmail.com>
2023-12-22 23:33:01 -0500

    12-21ConsulPredios

--- consultar/consulpred.php
+++ consultar/consulpred.php
@@ -46,7 +46,7 @@ function lis_predios(){
 	$predio=  ($_REQUEST['predio'])??'';
 	$unidad=  ($_REQUEST['unidad'])??'';
 	if($sector!==''){
-		$sql="select idgeo 'Codigo',FN_CATALOGODESC(42,G.estrategia) Estrategia,FN_CATALOGODESC(72,G.subred) Subred,territorio,direccion,asignado,equipo,estado_v 'estado',usu_creo Creo 
+		$sql="select idgeo 'Codigo',FN_CATALOGODESC(42,hg.estrategia) Estrategia,FN_CATALOGODESC(72,hg.subred) Subred,territorio,direccion,asignado,equipo,estado_v 'estado',usu_creo Creo 
 		from hog_geo hg
 		left join usuarios u ON hg.asignado=u.nombre";
 		$sql.=" WHERE sector_catastral=".$sector." AND nummanzana=".$manzana."  

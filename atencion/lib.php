3ef721cce5decc75eb638977d2639e512df1938e
k4rl0sA <carloseduardoacevedo@gmail.com>
2023-12-24 00:40:36 -0500

    12-21ConsulPredios

--- atencion/lib.php
+++ atencion/lib.php
@@ -419,6 +419,7 @@ function lista_persons(){ //revisar
 		IF(a.atencion_cronico = 'SI',IF((SELECT COUNT(*) FROM eac_enfermedades c WHERE c.enfermedades_documento = p.idpersona) > 0,'CON','SIN'),'NO') AS Cronico,
 		IF(a.gestante = 'SI',IF((SELECT COUNT(*) FROM eac_gestantes g WHERE g.gestantes_documento=p.idpersona) > 0, 'CON', 'SIN'),'NO') AS Gestante	
 		FROM `personas` 
+			LEFT JOIN eac_atencion a ON p.idpersona=a.atencion_idpersona
 			WHERE vivipersona='".$id[0]."'";
 		$sql.=" ORDER BY fecha_create";
 		// echo $sql;

34cbe184e781cdf6b6573f77e1d765ffd91661ae
k4rl0sA <carloseduardoacevedo@gmail.com>
2023-12-26 23:04:47 -0500

    12-26Tamizajes

--- oms/lib.php
+++ oms/lib.php
@@ -21,16 +21,21 @@ else {
 
 
 function lis_tamoms(){
-	$info=datos_mysql("SELECT COUNT(*) total from hog_tam_oms O LEFT JOIN personas P ON O.idpersona = P.idpersona where 1 ".whe_tamoms() ." AND O.usu_creo ='".$_SESSION['us_sds']."'");
+	$info=datos_mysql("SELECT COUNT(*) total from hog_tam_oms O LEFT JOIN personas P ON O.documento = P.idpersona
+	LEFT JOIN hog_viv V ON P.vivipersona = V.idviv
+	LEFT JOIN hog_geo G ON V.idpre = G.idgeo 
+	LEFT JOIN usuarios U ON O.usu_creo=id_usuario where ".whe_tamoms());
 	$total=$info['responseResult'][0]['total'];
 	$regxPag=12;
 	$pag=(isset($_POST['pag-tamoms']))? ($_POST['pag-tamoms']-1)* $regxPag:0;
 	$sql="SELECT ROW_NUMBER() OVER (ORDER BY 1) R,concat(O.idpersona,'_',O.tipodoc) ACCIONES,idoms 'Cod Registro',O.idpersona Documento,FN_CATALOGODESC(1,O.tipodoc) 'Tipo de Documento',CONCAT_ws(' ',P.nombre1,P.nombre2,P.apellido1,P.apellido2) Nombres,`puntaje` Puntaje,descripcion  
 	FROM hog_tam_oms O 
-	LEFT JOIN personas P ON O.idpersona = P.idpersona 
-	WHERE '1'='1'";
+	LEFT JOIN personas P ON O.documento = P.idpersona
+		LEFT JOIN hog_viv V ON P.vivipersona = V.idviv
+		LEFT JOIN hog_geo G ON V.idpre = G.idgeo 
+		LEFT JOIN usuarios U ON O.usu_creo=id_usuario
+	WHERE ";
 	$sql.=whe_tamoms();
-	$sql .= " AND O.usu_creo ='".$_SESSION['us_sds']."'";
 	$sql.=" ORDER BY O.fecha_create DESC";
 	//echo $sql;
 	$datos=datos_mysql($sql);
@@ -39,9 +44,14 @@ function lis_tamoms(){
 }
 
 function whe_tamoms() {
-	$sql = "";
-	if ($_POST['fidentificacion'])
-		$sql .= " AND O.idpersona like '%".$_POST['fidentificacion']."%'";
+	$fefin=date('Y-m-d');
+	$feini=date('Y-m-d',strtotime($fefin.'- 4 days')); 
+	$sql = " G.subred=(SELECT subred FROM usuarios where id_usuario='".$_SESSION['us_sds']."')";
+	if ($_POST['fidentificacion']){
+		$sql .= " AND O.documento = '".$_POST['fidentificacion']."'";
+	}else{
+		$sql.=" AND DATE(O.fecha_create) BETWEEN '$feini' and '$fefin'"; 
+	}
 	return $sql;
 }
 

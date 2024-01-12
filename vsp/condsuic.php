acff685f2b91e57379579cbfe24354e96ea15cf7
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-01-12 08:35:13 -0500

    01-12VSPCondSuic

--- vsp/condsuic.php
+++ vsp/condsuic.php
@@ -99,10 +99,10 @@ function cmp_condsuic(){
     $c[]=new cmp('resul_gest','s','2',$d,$w.' '.$bl.' '.$pu.' '.$gp.' '.$o,'Resultado de la gestación','resul_gest',null,null,false,$x,'','col-2',"enabOthSi('resul_gest','Rg');");
     $c[]=new cmp('meto_fecunda','s','2',$d,$w.' '.$bl.' '.$pu.' '.$gp.' '.$o,'¿Cuenta Con Método de Regulación de la fecundidad?','rta',null,null,false,$x,'','col-2',"enabOthSi('meto_fecunda','mF');");
     $c[]=new cmp('cual','s','3',$d,$w.' '.$bl.' mF '.$gp.' '.$o,'¿Cuál?','cual',null,null,false,$x,'','col-2');
-    $c[]=new cmp('peso_nacer','n','4',$d,$w.' Rg '.$bl.' '.$gp.' '.$o,'Peso del recién nacido al nacer (gr)','peso_nacer',null,null,false,$x,'','col-2');
-    $c[]=new cmp('asiste_control','s','2',$d,$w.' Rg '.$bl.' '.$gp.' '.$o,'¿Asiste a Controles de Crecimiento y Desarrollo o plan canguro?','rta',null,null,false,$x,'','col-3');
-    $c[]=new cmp('vacuna_comple','s','2',$d,$w.' Rg '.$bl.' '.$gp.' '.$o,'¿Tiene esquema de vacunación completo para la edad?','rta',null,null,false,$x,'','col-3');
-    $c[]=new cmp('lacmate_exclu','s','2',$d,$w.' Rg '.$bl.' '.$gp.' '.$o,'¿Recibe lactancia materna exclusiva?','rta',null,null,false,$x,'','col-2');
+    $c[]=new cmp('peso_nacer','n','4',$d,$w.' Rg '.$bl.' '.$pu.' '.$gp.' '.$o,'Peso del recién nacido al nacer (gr)','peso_nacer',null,null,false,$x,'','col-2');
+    $c[]=new cmp('asiste_control','s','2',$d,$w.' Rg '.$bl.' '.$pu.' '.$gp.' '.$o,'¿Asiste a Controles de Crecimiento y Desarrollo o plan canguro?','rta',null,null,false,$x,'','col-3');
+    $c[]=new cmp('vacuna_comple','s','2',$d,$w.' Rg '.$bl.' '.$pu.' '.$gp.' '.$o,'¿Tiene esquema de vacunación completo para la edad?','rta',null,null,false,$x,'','col-3');
+    $c[]=new cmp('lacmate_exclu','s','2',$d,$w.' Rg '.$bl.' '.$pu.' '.$gp.' '.$o,'¿Recibe lactancia materna exclusiva?','rta',null,null,false,$x,'','col-2');
 
     $o='hab';
     $c[]=new cmp($o,'e',null,'INFORMACIÓN CONDUCTA SUICIDA',$w);

3c929ca8e30790903b293becf402c614314fec88
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-01-15 10:54:45 -0500

    01-15LisAtenciones

--- atencion/lib.php
+++ atencion/lib.php
@@ -786,8 +786,10 @@ $o='prurap';
 
    function lis_atencion(){
 	// FN_CATALOGODESC(1,F.tipo_doc) Identificación, F.documento 'Número', F.`fecha_create` 'fecha creación' 
-	$id=divide($_POST['id']);
-	$id=divide($_POST['ida']);
+	/* $id=divide($_POST['id']);
+	$id=divide($_POST['ida']); */
+	$id = isset($_POST['id']) ? divide($_POST['id']) : (isset($_POST['ida']) ? divide($_POST['ida']) : null);
+
 	print_r($id);
 	$info=datos_mysql("SELECT COUNT(*) total FROM adm_facturacion F WHERE F.documento ='{$id[0]}' AND F.tipo_doc='{$id[1]}'");
 	$total=$info['responseResult'][0]['total'];

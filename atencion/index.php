199269df630ff63f7a8aab48e1409d8c50d79b0b
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-01-16 08:22:16 -0500

    01-16AtenFilt

--- atencion/index.php
+++ atencion/index.php
@@ -391,7 +391,7 @@ $territorio = ($perfil == 'ADMEAC' || $perfil == 'ADM' || $perfil == 'SUPEAC' ) 
 	</div>
 	<div class="campo"><div>Documento Usuario</div><input class="captura"  size=20 id="fusu" name="fusu" OnChange="searPers(this);"></div>
 	<div class="campo"><div>Codigo del Predio</div><input class="captura" type="number" size=20 id="fpred" name="fpred" OnChange="actualizar();"></div>
-	<!-- <div class="campo">
+	<div class="campo">
 		<div>Fecha Asignado Desde</div>
 		<input type="date" class="captura" size=10 id="fdes" name="fdes" value='<?php echo $ayer; ?>' OnChange="actualizar();" disabled="true">
 		
@@ -399,7 +399,7 @@ $territorio = ($perfil == 'ADMEAC' || $perfil == 'ADM' || $perfil == 'SUPEAC' ) 
 	<div class="campo">
 		<div>Fecha Asignado Hasta</div>
 		<input type="date" class="captura" size=10 id="fhas" name="fhas" value='<?php echo $hoy; ?>' OnChange="actualizar();" disabled="true">
-	</div>  -->
+	</div> 
 
 	<?php
 		$rta="";

f08445d3226f2c7c7f6020d8aee2a9b2172b265a
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-01-17 13:34:39 -0500

    01-17VspAcmpsi

--- vsp/acompsic.php
+++ vsp/acompsic.php
@@ -77,7 +77,7 @@ function cmp_acompsic(){
   $c[]=new cmp('fecha_seg','d','10',$d,$w.' '.$o,'Fecha Seguimiento','fecha_seg',null,null,true,true,'','col-2','validDate(this,-60,0)');
   $c[]=new cmp('numsegui','s','3',$d,$w.' '.$o,'Seguimiento N°','numsegui',null,null,true,true,'','col-2',"staEfe('numsegui','sta');EnabEfec(this,['hab','acc'],['Ob'],['nO'],['bL'])");
   $c[]=new cmp('evento','s','3',$ev,$w.' '.$o,'Evento','evento',null,null,false,false,'','col-2');
-  $c[]=new cmp('estado_s','s','3',$d,$w.' sTa '.$o,'Estado','estado_s',null,null,true,true,'','col-2',"enabFielSele(this,true,['motivo_estado'],['2','3','4']);");//EnabEfec(this,['hab','acc'],['Ob'],['nO'],['bL']);
+  $c[]=new cmp('estado_s','s','3',$d,$w.' sTa '.$o,'Estado','estado_s',null,null,true,true,'','col-2',"enabFielSele(this,true,['motivo_estado'],['4']);");//EnabEfec(this,['hab','acc'],['Ob'],['nO'],['bL']);
   $c[]=new cmp('motivo_estado','s','3',$d,$w.' '.$o,'Motivo de Estado','motivo_estado',null,null,false,$x,'','col-2');
 //fin agregar en todas
     

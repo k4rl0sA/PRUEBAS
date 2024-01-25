209e32affe4e63367d7461f8b2f2da2fb350a33f
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-01-25 18:30:25 -0500

    01-25Ruteo

--- ruteo/lib.php
+++ ruteo/lib.php
@@ -165,7 +165,7 @@ function opc_estado_res($id='') {
 
 		return	opc_sql("SELECT estado_v,FN_CATALOGODESC(44,estado_v)
 			from hog_geo where 
-			sector_catastral='$co[0]' AND nummanzana='$co[1]' AND predio_num='$co[2]' AND unidad_habit='$co[3]'",$id); 
+			sector_catastral='$co[0]' AND nummanzana='$co[1]' AND predio_num='$co[2]' AND unidad_habit='$co[3]' AND estado_v>3",$id); 
 			
 			// var_dump($id);
 }

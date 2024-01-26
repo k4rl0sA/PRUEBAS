b9e96bb29be2e12ae3fbd5333126fdd2ca3763a7
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-01-26 12:05:47 -0500

    01-25Ruteo

--- ruteo/lib.php
+++ ruteo/lib.php
@@ -167,8 +167,28 @@ function opc_estado_res($id='') {
 }
 
 function opc_famili($id=''){
+	var_dump($_REQUEST);
+	var_dump($_POST);
+	var_dump($_GET);
+
+	$id=divide($_REQUEST['id']);
+	$sql="SELECT concat_ws('_',sector_catastral,nummanzana,predio_num,unidad_habit) cod
+		 FROM `eac_ruteo` WHERE  id_ruteo='{$id[0]}'";
+		$info=datos_mysql($sql);
+    // var_dump($info['responseResult'][0]);
+		$cod= $info['responseResult'][0]['cod'];
+		$co=divide($cod);
+
+		return	opc_sql("SELECT estado_v,FN_CATALOGODESC(44,estado_v)
+			from hog_geo where 
+			sector_catastral='$co[0]' AND nummanzana='$co[1]' AND predio_num='$co[2]' AND unidad_habit='$co[3]' AND estado_v>3",$id); 
+			// var_dump($id);
+
+
+
 	return opc_sql("SELECT `idcatadeta`, descripcion FROM `catadeta` WHERE idcatalogo=0 AND estado='A' ORDER BY 1", $id);
 }
+
 function opc_usuario($id=''){
 	return opc_sql("SELECT `idcatadeta`, descripcion FROM `catadeta` WHERE idcatalogo=0 AND estado='A' ORDER BY 1", $id);
 }

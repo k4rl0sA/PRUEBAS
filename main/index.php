e3de393231fe7e423c964fb2a2fe58cbf9e123b2
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-02-08 02:13:25 -0500

    02-08PsiCodAdmin

--- main/index.php
+++ main/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Security-Policy" content="img-src 'self' https://drive.google.com">
 <!--
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 -->

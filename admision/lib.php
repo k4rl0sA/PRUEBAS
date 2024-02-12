a501995dda89cf077b370154310a5872bbf4c1f2
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-02-12 11:14:35 -0500

    02-12LisAdmisi

--- admision/lib.php
+++ admision/lib.php
@@ -38,7 +38,7 @@ function lis_adm(){
 	return create_table($total,$datos["responseResult"],"adm",$regxPag,'lib.php');
  */
 	$id=divide($_POST['id']);
- 	$info=datos_mysql("SELECT COUNT(*) total FROM adm_facturacion F 
+ 	$info=datos_mysql("SELECT COUNT(concat(tipo_doc,'_',documento,'_',id_factura)) total FROM adm_facturacion F 
  	WHERE F.documento ='{$id[0]}' AND F.tipo_doc='{$id[1]}'");
 	$total=$info['responseResult'][0]['total'];
 	$regxPag=4;

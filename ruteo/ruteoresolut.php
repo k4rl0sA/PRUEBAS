1f15800d7d3e657085a4b927f3c977e9b6e0744c
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-02-19 16:51:19 -0500

    02-19RuteoResol

--- ruteo/ruteoresolut.php
+++ ruteo/ruteoresolut.php
@@ -102,8 +102,12 @@ function opc_estadofamili(){
 	} 
 }
 function opc_famili($id=''){
-	// var_dump($id);
-	return opc_sql("SELECT idviv 'id',concat(idviv,' - ','FAMILIA ',numfam) FROM hog_viv hv where idviv={$id} ORDER BY 1", $id);
+	var_dump($id);
+	if ($id=''){
+
+	}else{
+		return opc_sql("SELECT idviv 'id',concat(idviv,' - ','FAMILIA ',numfam) FROM hog_viv hv where idviv={$id} ORDER BY 1", $id);
+	}
 }
 function opc_usuario($id=''){
 	// var_dump($id);

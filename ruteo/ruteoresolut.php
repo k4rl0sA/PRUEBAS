8d29c6f71567a1b9065d63af9b4b216f5f2313f2
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-02-18 22:17:27 -0500

    02-18RuteoResol

--- ruteo/ruteoresolut.php
+++ ruteo/ruteoresolut.php
@@ -80,7 +80,9 @@ function opc_idgeo($a){
 function opc_estado($id=''){
 	$id=opc_idgeo($_REQUEST['id']);
 		$co=divide($id);
-		var_dump($co);
+		var_dump("SELECT idgeo,FN_CATALOGODESC(44,estado_v)
+		from hog_geo where 
+		sector_catastral='$co[0]' AND nummanzana='$co[1]' AND predio_num='$co[2]' AND unidad_habit='$co[3]' AND estado_v>3");
 		return	opc_sql("SELECT idgeo,FN_CATALOGODESC(44,estado_v)
 			from hog_geo where 
 			sector_catastral='$co[0]' AND nummanzana='$co[1]' AND predio_num='$co[2]' AND unidad_habit='$co[3]' AND estado_v>3",$id); 

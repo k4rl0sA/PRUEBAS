cb7f1d50731f3e7bac9e0d22eae150a315f1d66a
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-02-18 21:28:12 -0500

    02-18RuteoResol

--- ruteo/ruteoresolut.php
+++ ruteo/ruteoresolut.php
@@ -106,10 +106,10 @@ function cmp_ruteresol(){
 
  $o='gesres';
  $c[]=new cmp($o,'e',null,'PROCESO GESTIÓN RESOLUTIVA',$w);
- $c[]=new cmp('estado','s',3,'',$w.' PuE StG '.$o,'estado','estado',null,null,false,false,'','col-2',"changeSelect('estado','famili');");
- $c[]=new cmp('famili','s',3,'',$w.' PuE StG '.$o,'famili','famili',null,'',false, false,'','col-3',"changeSelect('famili','usuario');");//N° FAMILIA
- $c[]=new cmp('usuario','s',3,'',$w.' PuE StG '.$o,'usuario','usuario',null,'',false, false,'','col-3',"changeSelect('usuario','cod_admin');"); //TIPO_DOC,DOCUMENTO Y NOMBRE USUARIO
- $c[]=new cmp('cod_admin','s',3,$d['cod_admin'],$w.' PuE StG '.$o,'cod_admin','cod_admin',null,'',false, false,'','col-2');//traer los codigos del usuario de atencion
+ $c[]=new cmp('estado','s',3,'',$w.' PuE StG '.$o,'estado','estado',null,null,false,true,'','col-2',"changeSelect('estado','famili');");
+ $c[]=new cmp('famili','s',3,'',$w.' PuE StG '.$o,'famili','famili',null,'',false, true,'','col-3',"changeSelect('famili','usuario');");//N° FAMILIA
+ $c[]=new cmp('usuario','s',3,'',$w.' PuE StG '.$o,'usuario','usuario',null,'',false, true,'','col-3',"changeSelect('usuario','cod_admin');"); //TIPO_DOC,DOCUMENTO Y NOMBRE USUARIO
+ $c[]=new cmp('cod_admin','s',3,$d['cod_admin'],$w.' PuE StG '.$o,'cod_admin','cod_admin',null,'',false, true,'','col-2');//traer los codigos del usuario de atencion
 
  for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
  return $rta;

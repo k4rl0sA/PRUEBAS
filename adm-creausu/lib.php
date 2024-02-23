34e05e852ab304049e5abf283ded646720400fee
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-02-22 19:38:18 -0500

    02-20CreaUsu

--- adm-creausu/lib.php
+++ adm-creausu/lib.php
@@ -116,6 +116,7 @@ function gra_creausu(){
    $rta=datos_mysql("select FN_USUARIO('".$_SESSION['us_sds']."') as usu;");
    $usu=divide($rta["responseResult"][0]['usu']);
 
+   var_dump("select FN_CATALOGODESC(218,'".$usu[1]."') AS perfil;");
    $rta=datos_mysql("select FN_CATALOGODESC(218,'".$usu[1]."') AS perfil;");
    $per=divide($rta["responseResult"][0]['perfil']);
 /* 

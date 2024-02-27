cda8ebe467578098bc9e5cfe466fcef0f24b0cbb
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-02-26 19:20:01 -0500

    02-26PsiCoSes2

--- psicologia/sesiones.php
+++ psicologia/sesiones.php
@@ -245,6 +245,7 @@ function gra_sesiones_psi(){
 }
 
 function opc_cod_admin4($id='') {
+	var_dump($_REQUEST['id']);
 	$cod=divide($_REQUEST['id']);
 	return opc_sql("SELECT cod_admin,CONCAT_WS(' - ',cod_admin,FN_CATALOGODESC(127,final_consul))  from adm_facturacion af WHERE af.tipo_doc='".$cod[0]."' AND af.documento='".$cod[1]."' AND cod_cups=9 AND final_consul=17 ORDER BY 1", $id);
 }

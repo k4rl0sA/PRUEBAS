5f199f9f64b5795d5ed7f220132c5f837c81e058
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-03-03 10:35:47 -0500

    03-03Gestion

--- libs/gestion.php
+++ libs/gestion.php
@@ -115,7 +115,7 @@ function cleanTx($val) {
   $val = trim($val);
   $val = addslashes($val);
   $val = htmlspecialchars($val, ENT_QUOTES, 'UTF-8');
-  $pattern = '/[;\|\/\*><\[\{\]\}\x00-\x1F\x7F]/';
+  $pattern = '/[;\|\/\*><\[\{\]\}\x1F\x7F]/';
   $val = preg_replace($pattern,'', $val);
   $val = str_replace(array("\n", "\r", "\t"),'', $val);
   return $val;

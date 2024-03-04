8b8a9279a8a6c04d2b6a0034ba057b58cc4fd152
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-03-04 16:15:17 -0500

    03-04Gestion

--- libs/gestion.php
+++ libs/gestion.php
@@ -126,8 +126,7 @@ function fechas_app($modu){
     case 'vsp':
       $sql="SELECT valor FROM `catadeta` WHERE idcatalogo='224' and estado='A' and idcatadeta=1;";
       $info=datos_mysql($sql);
-      var_dump($info['responseResult'][0]['valor']);
-      $dias=$info['responseResult'][0];
+      var_dump($dias=$info['responseResult'][0]['valor']);
     break;
     case 'vivienda':
       $dias=-7;

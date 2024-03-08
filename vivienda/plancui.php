d312b2cccabd07002fa4dc8d9ad3944507d9513d
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-03-08 11:24:30 -0500

    03-08PlanCuid

--- vivienda/plancui.php
+++ vivienda/plancui.php
@@ -64,6 +64,7 @@ function cmp_planDCui(){
 	$o='accide';
 	$e="";
 	$key='pln';
+    $days=fechas_app('vivienda');
 	$c[]=new cmp($o,'e',null,'ACCIONES PROMOCIONALES Y DE IDENTIFICACIÓN DE RIESGOS REALIZADOS EN LA CARACTERIZACIÓN FAMILIAR',$w);
 	$c[]=new cmp('idp','h',15,$_POST['id'],$w.' '.$key.' '.$o,'id','id',null,'####',false,false);
 	$c[]=new cmp('fecha_caracteriza','d','10',$d['fecha'],$w.' '.$o,'fecha_caracteriza','fecha_caracteriza',null,null,true,true,'','col-2','validDate(this,-3,0);');

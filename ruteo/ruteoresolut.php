375408f15b3e339e2e3cd9a95ce4a03aea29559a
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-03-13 12:33:19 -0500

    03-13Ruteo

--- ruteo/ruteoresolut.php
+++ ruteo/ruteoresolut.php
@@ -132,7 +132,7 @@ function opc_usuariocod_admin(){
 	// var_dump($_REQUEST['id']);
 	if($_REQUEST['id']!=''){
 		$id=divide($_REQUEST['id']);
-		$sql="SELECT f.cod_admin cod,concat(f.cod_admin,' - ',FN_CATALOGODESC(127,f.final_consul)) FROM adm_facturacion f WHERE f.tipo_doc='{$id[0]}' AND f.documento='{$id[1]}' ORDER BY 1";
+		$sql="SELECT f.cod_admin cod,concat_ws('-',f.cod_admin,FN_CATALOGODESC(127,f.final_consul)) FROM adm_facturacion f WHERE f.tipo_doc='{$id[0]}' AND f.documento='{$id[1]}' ORDER BY 1";
 		$info=datos_mysql($sql);
 		// print_r($sql);
 		return json_encode($info['responseResult']);

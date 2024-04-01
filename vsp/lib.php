d3413f5742a3002123618e4ff547be06e5444948
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-04-01 16:40:30 -0500

    04-01CaracteVSP

--- vsp/lib.php
+++ vsp/lib.php
@@ -773,7 +773,19 @@ function gra_person(){
 	}
 
  function opc_equipo($id=''){
-	return opc_sql("SELECT equipo,equipo FROM usuarios WHERE id_usuario= '{$_SESSION['us_sds']}' and estado='A' ORDER BY 1",$id);
+	$fam=divide($_POST['id']);
+	$sql="SELECT CASE WHEN EXISTS (SELECT 1 FROM hog_viv c WHERE c.idviv = $fam[0] AND NOT EXISTS (
+        SELECT 1 FROM usuarios u WHERE u.id_usuario ='{$_SESSION['us_sds']}' AND u.equipo = c.equipo_car)) 
+	THEN FALSE
+    ELSE TRUE
+    END AS RTA";
+	$info = datos_mysql($sql);
+	$rta = $info['responseResult'][0]['RTA'];
+	if ($rta===0){
+		return opc_sql("SELECT equipo_car,equipo_car FROM hog_viv WHERE idviv='$fam[0]' and estado='A' ORDER BY 1",$id);
+	}else{
+		return opc_sql("SELECT equipo,equipo FROM usuarios WHERE id_usuario= '{$_SESSION['us_sds']}' and estado='A' ORDER BY 1",$id);
+	}
 }
 
 function opc_equipo1($id=''){

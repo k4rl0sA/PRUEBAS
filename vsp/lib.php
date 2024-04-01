f59de4866e9c049579381ecc2a1d6359e2aed1fe
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-04-01 18:34:09 -0500

    04-01CaracteVSP

--- vsp/lib.php
+++ vsp/lib.php
@@ -784,7 +784,7 @@ function gra_person(){
 
 	$sql="SELECT equipo_car,equipo_car equipo FROM hog_viv WHERE idviv='$fam[0]' and estado='A' ORDER BY 1";
 		$info = datos_mysql($sql);
-		$rta = $info['responseResult'][0]['equipo'];
+		$rta = $info['responseResult']['equipo'];
 		var_dump(opc_sql("SELECT equipo_car,equipo_car equipo FROM hog_viv WHERE idviv='$fam[0]' and estado='A' ORDER BY 1",''));
 
 	if ($rta1===0){

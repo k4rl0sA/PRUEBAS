e2d0ca9ae755c4a146fc94e3251b74d83935b92e
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-04-03 17:10:38 -0500

    04-03GestUsu

--- adm-gestusu/lib.php
+++ adm-gestusu/lib.php
@@ -146,22 +146,36 @@ switch ($_POST['gestion']) {
 	return $rta;
 }
 
+function adm(){
+	$info=datos_mysql("SELECT perfil FROM usuarios WHERE id_usuario='{$_SESSION['us_sds']}'");
+	$adm=$info['responseResult'][0]['perfil'];
+	return $adm;
+}
+
 function opc_perfilusuario($id=''){
 	if($_REQUEST['id']!=''){	
-		$sql="SELECT *,id_usuario id,CONCAT(id_usuario,'-',nombre) usuario FROM usuarios WHERE 
+		if(adm()='ADM'){	
+			$sql="SELECT *,id_usuario id,CONCAT(id_usuario,'-',nombre) usuario FROM usuarios WHERE 
+		perfil=(select descripcion from catadeta c where idcatalogo=218 and idcatadeta='{$_REQUEST['id']}' and estado='A') 
+		 ORDER BY nombre";
+		// var_dump($sql);
+		$info=datos_mysql($sql);		
+		return json_encode($info['responseResult']);	
+		}else {
+			$sql="SELECT *,id_usuario id,CONCAT(id_usuario,'-',nombre) usuario FROM usuarios WHERE 
 		perfil=(select descripcion from catadeta c where idcatalogo=218 and idcatadeta='{$_REQUEST['id']}' and estado='A') 
 		and componente=(SELECT componente FROM usuarios WHERE id_usuario ='{$_SESSION['us_sds']}') 
 		and subred=(SELECT subred FROM usuarios WHERE id_usuario ='{$_SESSION['us_sds']}') ORDER BY nombre";
 		// var_dump($sql);
 		$info=datos_mysql($sql);		
-		return json_encode($info['responseResult']);
+		return json_encode($info['responseResult']);	
+		}
 	} 
 }
 
 function opc_perfil($id=''){
-	$info=datos_mysql("SELECT perfil FROM usuarios WHERE id_usuario='{$_SESSION['us_sds']}'");
-	$adm=$info['responseResult'][0]['perfil'];
-	if ($adm=='ADM') {
+	
+	if (adm()=='ADM') {
 		return opc_sql("SELECT idcatadeta, descripcion FROM `catadeta` WHERE idcatalogo = 218 AND estado = 'A'",$id);
 	}else {
 	$com=datos_mysql("SELECT CASE WHEN componente = 'EAC' THEN 2 WHEN componente = 'HOG' THEN 1 END as componente FROM usuarios WHERE id_usuario ='{$_SESSION['us_sds']}'");

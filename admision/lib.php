3e676b73ab47f96e92fdcdb2684f45aa86bfedef
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-04-05 13:46:33 -0500

    04-05AdmisionBloqueoFactura

--- admision/lib.php
+++ admision/lib.php
@@ -329,7 +329,7 @@ function formato_dato($a,$b,$c,$d){
 	if ($a=='adm' && $b=='acciones'){
 		$rta="<nav class='menu right'>";		
 		$cmps = (fac($c['ACCIONES'])=='0000-00-00') ? '':"['fecha_consulta','tipo_consulta','cod_cups','final_consul']";
-		$rta.="<li class='icono editar ' title='Editar Facturación' id='".$c['ACCIONES']."' Onclick=\"setTimeout(getData,500,'admision',event,this,{$cmps},'lib.php');Color('adm-lis');\"></li>";  //act_lista(f,this);
+		$rta.="<li class='icono editar ' title='Editar Facturación' id='".$c['ACCIONES']."' Onclick=\"setTimeout(getData,500,'admision',event,this,{$cmps});Color('adm-lis');\"></li>";  //act_lista(f,this);
 		// $rta.="<li class='icono editar' title='Editar Información de Facturación' id='".$c['ACCIONES']."' Onclick=\"getData('admision','pro',event,'','lib.php',7);\"></li>"; //setTimeout(hideExpres,1000,'estado_v',['7']);
 	}
  return $rta;

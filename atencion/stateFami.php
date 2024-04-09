df8fcb6d6d23fbcd2f2e3386d73448a6c8d958a8
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-04-09 11:59:38 -0500

    04-09DerivaEac

--- atencion/stateFami.php
+++ atencion/stateFami.php
@@ -65,10 +65,9 @@ function focus_statFam(){
       }else{
          $id=divide($_POST['id']);
         $sql="SELECT id_eacfam id,cod_fam,estado_fam,motivo_estafam from eac_fam WHERE cod_fam='{$id[0]}' limit 1 ";
-        echo $sql;
         $info=datos_mysql($sql);
         if(isset($info['responseResult'][0])){ 
-          echo $sql;
+          var_dump($info);
             return $info['responseResult'][0];
         }else{
           return "";

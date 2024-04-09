9649087ec8040183911cf3b1b6f27b1bbeb829ad
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-04-09 09:27:42 -0500

    04-09DerivaEac

--- atencion/stateFami.php
+++ atencion/stateFami.php
@@ -74,7 +74,7 @@ function focus_statFam(){
     }
 
     function opc_estado_fam($id=''){
-      return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=44 and estado='A' ORDER BY 1",$id);
+      return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=227 and estado='A' ORDER BY 1",$id);
     }
     function opc_motivo_estafam($id=''){
       return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=5 and estado='A' ORDER BY 1",$id);

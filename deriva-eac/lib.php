01096c404949c053ab8a324a0fa344b2f1b4a3a5
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-04-16 12:09:03 -0500

    04-16AdmisEstado

--- deriva-eac/lib.php
+++ deriva-eac/lib.php
@@ -43,7 +43,7 @@ function lis_derivaeac(){
 	LEFT JOIN hog_geo G ON V.idpre = G.idgeo
 	LEFT JOIN usuarios U ON A.asignado_eac=U.id_usuario
 	LEFT JOIN eac_fam E ON V.idviv=E.cod_fam
-	WHERE A.deriva_eac = 1 AND A.necesidad_eac IS NOT null AND E.estado_fam=1 ";
+	WHERE A.deriva_eac = 1 AND A.necesidad_eac IS NOT null ";//AND E.estado_fam=1 
 	$sql.=whe_derivaeac();
 	$sql.=" ORDER BY E.fecha_create";
 	$sql.=' LIMIT '.$pag.','.$regxPag;

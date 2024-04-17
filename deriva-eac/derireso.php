90827171fa1093019e4fbd8c30c0d2cf873a28e5
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-04-16 22:17:46 -0500

    04-16AdmiState

--- deriva-eac/derireso.php
+++ deriva-eac/derireso.php
@@ -151,6 +151,7 @@ function opc_usuariocod_admin(){
 	if($_REQUEST['id']!=''){
 	$id=divide($_REQUEST['id']);
 	$sql="SELECT f.cod_admin cod,concat_ws('_',cod_admin,FN_CATALOGODESC(127,final_consul)) FROM adm_facturacion f WHERE documento='{$id[0]}'";
+	var_dump($sql);
 	$info=datos_mysql($sql);
 	return json_encode($info['responseResult']);
 	}

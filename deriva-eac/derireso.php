26fff06a6c0109eaca9adc84fb30a8d92ceb7d96
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-04-17 11:21:04 -0500

    04-16AdmisEstado

--- deriva-eac/derireso.php
+++ deriva-eac/derireso.php
@@ -50,7 +50,7 @@ function cmp_ruteresol(){
 var_dump($_POST);
  $o='gesres';
  $c[]=new cmp($o,'e',null,'PROCESO GESTIÓN RESOLUTIVA',$w);
- $c[]=new cmp('id','h','20',$d['id'],$w.' '.$o,'','',null,null,true,$u,'','col-1');
+ $c[]=new cmp('id','h','20',$_POST['id'],$w.' '.$o,'','',null,null,true,$u,'','col-1');
  /* $c[]=new cmp('estado','s',3,$d['predio'],$w.' PuE '.$o,'estado','estado',null,null,true,$u,'','col-2',"changeSelect('estado','famili','derireso.php');enabDepeInner('estado','StG',['RECHAZADA','FALLIDO','NO RESIDENCIAL']);");
  //N° FAMILIA */
 /*  $c[]=new cmp('famili','s',3,$d['famili'],$w.' PuE StG '.$o,'famili','famili',null,'',true, $u,'','col-15',"changeSelect('famili','usuario','derireso.php');"); */

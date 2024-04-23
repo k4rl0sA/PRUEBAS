aba299db69f2920e50e34d6421ef508677879cfe
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-04-23 11:37:47 -0500

    04-23MedidasImcDeriva

--- libs/js/x.js
+++ libs/js/x.js
@@ -1022,3 +1022,15 @@ if (ele.length!=7 && ele.length!=10){
 	}
 }
 
+document.getElementById('fselmul10[]').addEventListener('change', function() {
+    // Obtener referencias a los elementos select
+    var select = document.getElementById('fselmul10[]');
+    var targetSelect = document.getElementById('deriva_eac');
+
+    // Verificar si se han seleccionado las opciones especificadas
+    var selectedOptions = Array.from(select.selectedOptions).map(option => option.value);
+    var shouldBeEnabled = selectedOptions.some(option => ['5A', 'SB'].includes(option));
+
+    // Habilitar o deshabilitar el select simple basado en la condición
+    targetSelect.disabled = !shouldBeEnabled;
+});

ab3a4a354babd75226e89d92695800d2c4ca70b3
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-04-26 14:01:27 -0500

    Update reports

--- reports/index.php
+++ reports/index.php
@@ -49,7 +49,9 @@ $estados=array('A'=>'Activo','I'=>'Inactivo');
 <div class="col-2 menu-filtro" id='<?php echo $mod; ?>-fil'>
 	
 
-<div class="campo"><div>Caso</div><input class="captura" type="number" size=20 id="fcaso" name="fcaso" onChange="actualizar();"></div>
+<div class="campo"><div>Reportes</div>
+	<select class="captura" id="festado" name="freporte" onChange="actualizar();">'.<?php echo $estados; ?></select>
+</div>
 <div class="campo"><div>Documento Colaborador</div><input class="captura" type="number" size=10 id="fdoc" name="fdoc" onChange="actualizar();"></div>
 
 <!-- <div class="campo"><div>Estado</div>

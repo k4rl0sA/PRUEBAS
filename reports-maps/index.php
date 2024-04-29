ea87f28bf644baf7076deff9178a8e3461e88d0c
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-04-29 17:38:27 -0500

    04-29ReportMaps

--- reports-maps/index.php
+++ reports-maps/index.php
@@ -66,7 +66,7 @@ function myAjax(a){
 			}}
 			xmlhttp.open("POST",'lib.php',false);
 			xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			xmlhttp.send('a=opc&tb=1&'+ form_input('fapp'));
+			xmlhttp.send('a=opc&tb=3&'+ form_input('fapp'));
 			return JSON.parse(data);
 }
 

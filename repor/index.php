56ece6f2547b943c2f7dc4d4b00bfe70d8e5f394
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-04-28 20:31:41 -0500

    04-28Reports

--- repor/index.php
+++ repor/index.php
@@ -87,7 +87,9 @@ function graficar() {
 
         // Realizar la solicitud para obtener los datos
             // Convertir la respuesta a un objeto JSON
-            var data = myAjax(tb);
+            // var data = myAjax(tb);
+            var data = JSON.parse("[['Mushrooms', 1], ['Onions', 1], ['Olives', 2], ['Zucchini', 2], ['Pepperoni', 1]]");
+
             console.error(data);
 
             // Crear el objeto de opciones del gráfico

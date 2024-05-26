7fb48c346d68cafe08f14999f9082e2160b52b1b
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-05-26 18:24:46 -0500

    05-25DerivEacStat

--- deriva-eac/derireso.php
+++ deriva-eac/derireso.php
@@ -52,11 +52,11 @@ function cmp_ruteresol(){
  $o='gesres';
  $c[]=new cmp($o,'e',null,'PROCESO GESTIÓN RESOLUTIVA',$w);
  $c[]=new cmp('id','h','20',$_POST['id'],$w.' '.$o,'','',null,null,true,$u,'','col-1');
- $c[]=new cmp('estado','s',3,$d['estado'],$w.' PuE '.$o,'estado','estado',null,null,true,$u,'','col-2',"enabStatPreAdm(this,['motivo_estafam'],false);");
+ $c[]=new cmp('estado','s',3,$d['estado'],$w.' '.$o,'estado','estado',null,null,true,$u,'','col-2',"enbValue('estado',clsCmp,v);");
  //N° FAMILIA */
 /*  $c[]=new cmp('famili','s',3,$d['famili'],$w.' PuE StG '.$o,'famili','famili',null,'',true, $u,'','col-15',"changeSelect('famili','usuario','derireso.php');"); */
- $c[]=new cmp('usuario','s',3,$d['usuario'],$w.' PuE StA '.$o,'usuario','usuario',null,'',true,$u,'','col-25',"changeSelect('usuario','cod_admin','derireso.php');"); //TIPO_DOC,DOCUMENTO Y NOMBRE USUARIO
- $c[]=new cmp('cod_admin','s',3,$d['cod_admin'],$w.' PuE StG '.$o,'cod_admin','cod_admin',null,'',true,$u,'','col-4');//traer los codigos del usuario de atencion
+ $c[]=new cmp('usuario','s',3,$d['usuario'],$w.' PuE '.$o,'usuario','usuario',null,'',true,$u,'','col-25',"changeSelect('usuario','cod_admin','derireso.php');"); //TIPO_DOC,DOCUMENTO Y NOMBRE USUARIO
+ $c[]=new cmp('cod_admin','s',3,$d['cod_admin'],$w.' PuE '.$o,'cod_admin','cod_admin',null,'',true,$u,'','col-4');//traer los codigos del usuario de atencion
  for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
  return $rta;
 }

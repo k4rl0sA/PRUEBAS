83d36f2cd67cd04ff63ad75cbb4b55658d6c9ea2
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-06-05 17:10:31 -0500

    06-05RptIndiv

--- rpt-indv/lib.php
+++ rpt-indv/lib.php
@@ -169,7 +169,7 @@ function cmp_rptindv(){
 			<div class="user-info section">
 				<div class="user-details">
 					<div title="Self Reporting Questionnaire. Identifica pacientes con alta probabilidad de estar sufriendo una enfermedad mental.(Población mayor a 16 años)"><b>SRQ :</b> 20</div>
-					<div title="(Finnish Diabetes Risk Score. Evalua el riesgo de una persona de desarrollar diabetes mellitus tipo 2 en los próximos 10 años.)"><b>Findrisc :</b>'.$d["Puntaje_Findrisc"].' '.$d["Riesgo_Findrisc"].'</div>
+					<div title="Finnish Diabetes Risk Score. Evalua el riesgo de una persona de desarrollar diabetes mellitus tipo 2 en los próximos 10 años.(Población mayor a 17 años)"><b>Findrisc :</b>'.$d["Puntaje_Findrisc"].' '.$d["Riesgo_Findrisc"].'</div>
 			 	</div>
 				<div class="user-details">
 					<div title="Reporting Questionnaire for Children. Identifica problemas de salud mental en población infantil no psiquiátrica.(Población entre 5 y 15 años)"><b>RQC :</b> 30</div>

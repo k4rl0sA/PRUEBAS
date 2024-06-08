afadc4dae6a161ccd745f2a6216b96a4e789cc3b
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-06-07 23:38:22 -0500

    06-07RptIndiv

--- rpt-indv/lib.php
+++ rpt-indv/lib.php
@@ -176,17 +176,17 @@ function cmp_rptindv(){
 					<div class="tooltip"><b>COPE 28 :</b>'.$d["Puntaje_Cope"].' '.$d["Riesgo_Cope"].'<span class="tooltiptext">Cuestionario Multidimensional de Afrontamiento (Evalua las diferentes formas de respuesta ante el estrés.)</span></div>
 				</div>
 				<div class="user-details">
-					<div class="tooltip"><b>EPOC :</b> '.$d["Puntaje_Epoc"].' '.$d["Riesgo_Epoc"].'Enfermedad Pulmonar Obstructiva Crónica (Población de 40 años o más)</span></div>
+					<div class="tooltip"><b>EPOC :</b> '.$d["Puntaje_Epoc"].' '.$d["Riesgo_Epoc"].'<span class="tooltiptext">Enfermedad Pulmonar Obstructiva Crónica (Población de 40 años o más)</span></div>
 				</div>
 			</div>
 		<div class="user-info section">
 			<div class="user-details">
-				<div class="tooltip"><b>Zarith :</b> '.$d["Puntaje_Zarit"].' '.$d["Riesgo_Zarit"].'El Zarit Burden Inventory, conocido en nuestro medio como cuestionario de Zarit (aunque tiene diversas denominaciones tanto en inglés como en español), es un instrumento que cuantifica el grado de sobrecarga que padecen los cuidadores de las personas dependientes</span></div>
-				<div class="tooltip"><b>Hamilton :</b> '.$d["Puntaje_Hamilton"].' '.$d["Riesgo_Hamilton"].'Self Reporting Questionnaire. Para identificar pacientes con alta probabilidad de estar sufriendo una enfermedad mental</span></div>
+				<div class="tooltip"><b>Zarith :</b> '.$d["Puntaje_Zarit"].' '.$d["Riesgo_Zarit"].'<span class="tooltiptext">El Zarit Burden Inventory, conocido en nuestro medio como cuestionario de Zarit (aunque tiene diversas denominaciones tanto en inglés como en español), es un instrumento que cuantifica el grado de sobrecarga que padecen los cuidadores de las personas dependientes</span></div>
+				<div class="tooltip"><b>Hamilton :</b> '.$d["Puntaje_Hamilton"].' '.$d["Riesgo_Hamilton"].'<span class="tooltiptext">Self Reporting Questionnaire. Para identificar pacientes con alta probabilidad de estar sufriendo una enfermedad mental</span></div>
 			</div>
 			<div class="user-details">
-				<div class="tooltip"><b>Zung :</b> '.$d["Puntaje_Zung"].' '.$d["Riesgo_Zung"].'Self Reporting Questionnaire. Para identificar pacientes con alta probabilidad de estar sufriendo una enfermedad mental</span></div>
-				<div class="tooltip"><b>Ophi II :</b> '.$d["Puntaje_Ophi"].'Self Reporting Questionnaire. Para identificar pacientes con alta probabilidad de estar sufriendo una enfermedad mental</span></div>
+				<div class="tooltip"><b>Zung :</b> '.$d["Puntaje_Zung"].' '.$d["Riesgo_Zung"].'<span class="tooltiptext">Self Reporting Questionnaire. Para identificar pacientes con alta probabilidad de estar sufriendo una enfermedad mental</span></div>
+				<div class="tooltip"><b>Ophi II :</b> '.$d["Puntaje_Ophi"].'<span class="tooltiptext">Self Reporting Questionnaire. Para identificar pacientes con alta probabilidad de estar sufriendo una enfermedad mental</span></div>
 			</div>
 		</div>
 	</div>

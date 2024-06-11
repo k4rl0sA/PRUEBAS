aeb749e9e59515bf7a11217157c51ca61906afef
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-06-10 22:14:30 -0500

    06-10RptIndv

--- rpt-indv/lib.php
+++ rpt-indv/lib.php
@@ -1,6 +1,7 @@
 <?php
 require_once "../libs/gestion.php";
 ini_set('display_errors','1');
+var_dump($_POST);
 if ($_POST['a']!='opc') $perf=perfil($_POST['tb']);
 if (!isset($_SESSION['us_sds'])) die("<script>window.top.location.href='/';</script>");
 else {

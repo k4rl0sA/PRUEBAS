815eca514dd312aeb0bec937ce604c9cbbb4bc1b
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-06-10 19:35:22 -0500

    06-10RptIndv

--- rpt-indv/lib.php
+++ rpt-indv/lib.php
@@ -166,8 +166,8 @@ function cmp_rptindv(){
 
 
 	var_dump('Total/Suma='.$total.'/'.$sum.'='.$sum/$total);
-	$Roms+$Rimc+$Repoc+$Rfind;
-	var_dump($Rcope);
+	$Roms+$Rimc+$Repoc+$Rfind+$Rcope;
+	var_dump($Rzari);
 
 /* 	if($suma>=($total/2)){
 

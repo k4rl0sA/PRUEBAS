1efb7f52a7a0a44cb2c9aa349f200166680b4ebf
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-06-10 22:51:11 -0500

    06-10RptIndv

--- libs/js/graph.js
+++ libs/js/graph.js
@@ -48,28 +48,3 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-
-
-/* document.addEventListener("DOMContentLoaded", function() {
-    // Función para cargar el gráfico en el contenedor
-    function cargarGrafico() {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == XMLHttpRequest.DONE) {
-                if (xhr.status == 200) {
-                    document.getElementById('graficoContainer').innerHTML = xhr.responseText;
-                }
-            }
-        };
-        xhr.open('GET', 'lib.php', true);
-        xhr.send('a=gra&tb='+mod+'&type=radar');
-    } 
-
-    // Cargar el gráfico al cargar la página
-    cargarGrafico();
-
-    // Actualizar el gráfico cada 5 segundos
-    setInterval(cargarGrafico, 5000);
-});
-
-*/

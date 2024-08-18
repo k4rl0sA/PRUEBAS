330ab4fa4e97e6a61e7dd23556daba8b4e02f831
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-08-18 02:59:43 -0500

    08-18New

--- lib/php/nav.php
+++ lib/php/nav.php
@@ -1,6 +1,5 @@
 <head>
 		<link href="../libs/css/styleNav.css" rel="stylesheet" type="text/css">
-		<link href="../lib/css/s.css" rel="stylesheet">
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">

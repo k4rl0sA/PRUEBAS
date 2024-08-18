bbfdd872b96d335f4b62dd112b2e3f2be05954cf
k4rl0sA <carloseduardoacevedo@gmail.com>
2024-08-18 03:35:52 -0500

    08-18New

--- lib/php/nav.php
+++ lib/php/nav.php
@@ -6,7 +6,7 @@ require_once 'gestion.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../libs/css/nav.css">
+    <link rel="stylesheet" href="../lib/css/nav.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
   </head>
 

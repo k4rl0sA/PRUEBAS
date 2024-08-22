079149458e5130e4a5e877c71028ebbf5f28d709
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-08-22 08:27:19 -0500

    08-21 08:27

--- creacita/index.php
+++ creacita/index.php
@@ -1,113 +1,149 @@
 <?php
-ini_set('display_errors','1');
-include $_SERVER['DOCUMENT_ROOT'].'/libs/nav.php';
+ini_set('display_errors', '1');
+include $_SERVER['DOCUMENT_ROOT'] . '/lib/php/nav.php';
+require_once "../lib/php/gestion.php";
+if (!isset($_SESSION["us_sds"])) {
+	die("<script>window.top.location.href = '/';</script>");
+}
+$mod = 'catalogo';
+$estados = opc_sql("SELECT idcatadeta,descripcion from catadeta where idcatalogo=2 and estado='A' order by 1", '');
+$catalogos = opc_sql("SELECT `idcatalogo`,concat(idcatalogo,' - ',nombre) FROM `catalogo` ORDER BY 1", '');
 ?>
+
 <head>
-<meta charset="utf-8"/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Solicitar Cita || Sigrev</title>
-<link href="../libs/css/stylePop.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Cabin+Sketch&family=Chicle&family=Merienda&family=Rancho&family=Boogaloo&display=swap" rel="stylesheet">
-<script src="../libs/js/a.js"></script>
-<script src="../libs/js/x.js"></script>
-<script src="../libs/js/d.js"></script>
-<script src="../libs/js/popup.js"></script>
-<script>
-var mod='solcita';	
-var ruta_app='lib.php';
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Catalogo || Proteger</title>
+	<link rel="stylesheet" href="../lib/css/main.css">
+	<script src="../lib/js/a.js"></script>
+	<script src="../lib/js/d.js"></script>
+	<script src="../lib/js/popup.js"></script>
+	<script>
+		var mod = 'catalogo';
+		var ruta_app = 'lib.php';
 
+		function csv(b) {
+			var myWindow = window.open("../lib/gestion.php?a=exportar&b=" + b, "Descargar archivo");
+		}
 
-function actualizar(){
-	act_lista(mod);
-}
+		function actualizar() {
+			act_lista(mod);
+		}
 
-function grabar(tb='',ev){
-  if (tb=='' && ev.target.classList.contains(proc)) tb=proc;
-  var f=document.getElementsByClassName('valido '+tb);
-   for (i=0;i<f.length;i++) {
-     if (!valido(f[i])) {f[i].focus(); return};
-  }
-    myFetch(ruta_app,"a=gra&tb="+tb,mod);  
-}   
-
-function enableUpd(a,b){
-	const update= document.querySelectorAll('input.'+b);
-	if(a.value=='SI'){
-		enaFie(update[0],false);
-  	}else{
-		enaFie(update[0],true);
-	}
-}
-
-</script>
+		function grabar(tb = '', ev) {
+			if (tb == '' && ev.target.classList.contains(proc)) tb = proc;
+			var f = document.getElementsByClassName('valido ' + tb);
+			for (i = 0; i < f.length; i++) {
+				if (!valido(f[i])) {
+					f[i].focus();
+					return
+				};
+			}
+			var res = confirm("Desea guardar la información, recuerda que no se podrá editar posteriormente?");
+			if (res == true) {
+				myFetch(ruta_app, "a=gra&tb=" + tb, mod);
+				setTimeout(actualizar, 1000);
+			}
+		}
+	</script>
 </head>
+
 <body Onload="actualizar();">
-<?php
+	<div class="wrapper main" id='<?php echo $mod; ?>-main'>
+		<div class="top-menu">
+			<input type="radio" name="slider"  id="filtros">
+			<input type="radio" name="slider" checked id="datos">
+			<nav>
+				<label for="filtros" class="filtros"><i class="fa-solid fa-sliders fa-rotate-90"></i>Filtros</label>
+				<label for="datos" class="datos"><i class="fas fa-table"></i>Datos</label>
+				<div class="slider"></div>
+			</nav>
+			<section>
+				<div class="content content-1">
+					<div class="title txt-center"><h2>Filtros</h2></div>
 
-require_once "../libs/gestion.php";
-if (!isset($_SESSION["us_sds"])){ die("<script>window.top.location.href = '/';</script>");}
+					<div class="frm-filter poppins-font" id='<?php echo $mod; ?>-fil'>
+    					<form method='post' id='fapp'>
 
-$mod='solcita';
-$ya = new DateTime();
-$estados=opc_sql("select idcatadeta,descripcion from catadeta where idcatalogo=145 and estado='A' order by 1",'');
-$digitadores=opc_sql("SELECT `id_usuario`,nombre FROM `usuarios` WHERE perfil IN('ADM')",$_SESSION['us_sds']);
-?>
+							<div class="input-box">
+    					        <select class='captura' id="fidcata" name="fidcata" OnChange="actualizar();" required>
+									<?php echo $catalogos; ?>
+    					        </select>
+    					        <label for="fidcata">Catalogo</label>
+    					    </div>
+
+    					    <div class="input-box">
+    					        <input class='captura' type="text" id="fcatalogo" name="fcatalogo" OnChange="actualizar();" required>
+    					        <label for="fcatalogo">Nombre del catalogo</label>
+    					    </div>
+
+							<div class="input-box">
+    					        <select class='captura' id="festado" name="festado" OnChange="actualizar();" required>
+									<?php echo $estados; ?>
+    					        </select>
+    					        <label for="festado">Estado</label>
+    					    </div>
+
+    					    <!-- <div class="input-box">
+    					        <input class='captura' type="date" id="birthdate" name="birthdate" OnChange="actualizar();" required>
+    					        <label for="birthdate">fecha de nacimiento</label>
+    					    </div>
+							<div class="input-box">
+    					        <input class='captura' type="checkbox" id="agree" name="agree" OnChange="actualizar();" required>
+    					        <label for="agree">Yo acepto los terminos y condiciones</label>
+    					    </div>
+							<div class="input-box">
+    					        <textarea class='captura' id="message" name="message" rows="4" required></textarea>
+    					        <label for="message">Observaciones</label>
+    					    </div>
+						-->
+
+    					    
+
+							
+    					    
+
+    					    
+
+    					    <button type="submit" class="btn">Aplicar</button>
+    					</form>
+					</div>
 
 
-<form method='post' id='fapp'>
-<div class="col-2 menu-filtro" id='<?php echo $mod; ?>-fil'>
-
-<div class="campo">
-		<div>N° Documento</div>
-		<input class="captura" size=50 id="fidpersona" name="fidpersona" OnChange="actualizar();">
+					
+						<div class='load' id='loader' z-index='0'></div>
+				</div>
+				<div class="content content-2">
+					<div class="title txt-center"><h2>CATALOGO</h2></div>
+					<div class='panel' id='<?php echo $mod; ?>'>
+							<!-- <nav class='menu left'>
+								<li class='icono exportar' title='Exportar CSV' Onclick="csv(mod);"></li>
+								<li class='icono actualizar' title='Actualizar' Onclick="actualizar();">
+								<li class='icono crear' title='Crear Catalogo' Onclick="mostrar(mod,'pro');"></li>
+							</nav> 
+							<nav class='menu right'>
+								<li class='icono ayuda' title='Necesitas Ayuda' Onclick=" window.open('https://sites.google.com/', '_blank');"></li>
+								<li
+								 class='icono cancelar' title='Salir' Onclick="location.href='../main/'"></li>
+							</nav>-->
+						</form>
+						<span class='mensaje' id='<?php echo $mod; ?>-msj'></span>
+						<div class='contenido' id='<?php echo $mod; ?>-lis'></div>
+					</div>
+				</div>
+			</section>
+		</div>
 	</div>
-	
-	<div class="campo"><div>Estado Solicitud</div>
-		<select class="captura" id="festado" name="festado" OnChange="actualizar();">
-			<?php echo $estados; ?>
-		</select>
-	</div>
 
-    <div class="campo"><div>Colaborador</div>
-        <select class="captura" id="fdigita" name="fdigita" OnChange="actualizar();">
-			<?php echo $digitadores; ?>
-		</select>
-    </div>
-
-
-	<div class="campo">
-		<div>Fecha Desde</div>
-		<input type="date" class="captura" size=10 id="fdes" name="fdes" OnChange="actualizar();">
+	<div class="overlay" id="overlay" onClick="closeModal();">
+		<div class="popup" id="popup" z-index="0" onClick="closeModal();">
+			<div class="btn-close-popup" id="closePopup" onClick="closeModal();">&times;</div>
+			<h3>
+				<div class='image' id='<?php echo $mod; ?>-image'></div>
+			</h3>
+			<h4>
+				<div class='message' id='<?php echo $mod; ?>-modal'></div>
+			</h4>
+		</div>
 	</div>
-	<div class="campo">
-		<div>Fecha Hasta</div>
-		<input type="date" class="captura" size=10 id="fhas" name="fhas" OnChange="actualizar();">
-	</div>
-</div>
-<div class='col-8 panel' id='<?php echo $mod; ?>'>
-      <div class='titulo' > SOLICITAR CITA
-		<nav class='menu left' >
-    <li class='icono actualizar'    title='Actualizar'      Onclick="actualizar();">
-    <li class='icono crear'       title='Solicitar Cita'     Onclick="mostrar('solcita','pro',event,'','lib.php',7,'Solicitar Cita');"></li> <!--hideMotiv();-->
-    </nav>
-		<nav class='menu right' >
-			<li class='icono ayuda'      title='Necesitas Ayuda'            Onclick=" window.open('https://drive.google.com/drive/folders/1JGd31V_12mh8-l2HkXKcKVlfhxYEkXpA', '_blank');"></li>
-            <li class='icono cancelar'      title='Salir'            Onclick="location.href='../main/'"></li>
-        </nav>               
-      </div>
-  </form>
-		<span class='mensaje' id='<?php echo $mod; ?>-msj' ></span>
-     <div class='contenido' id='<?php echo $mod; ?>-lis' ></div>
-	 <div class='contenido' id='cmprstss' ></div>
-</div>			
-		
-<div class='load' id='loader' z-index='0' ></div>
-
-<div class="overlay" id="overlay" onClick="closeModal();">
-	<div class="popup" id="popup" z-index="0" onClick="closeModal();">
-		<div class="btn-close-popup" id="closePopup" onClick="closeModal();">&times;</div>
-		<h3><div class='image' id='<?php echo$mod; ?>-image'></div></h3>
-		<h4><div class='message' id='<?php echo$mod; ?>-modal'></div></h4>
-	</div>
-</div>
 </body>
--- creacita/lib.php
+++ creacita/lib.php
@@ -1,228 +1,123 @@
 <?php
-require_once '../libs/gestion.php';
-ini_set('display_errors','1');
-if (!isset($_SESSION['us_sds'])) die("<script>window.top.location.href='/';</script>");
-else {
-  $rta="";
-  switch ($_POST['a']){
-  case 'csv': 
-    header_csv ($_REQUEST['tb'].'.csv');
-    $rs=array('','');    
-    echo csv($rs,'');
-    die;
-    break;
-  default:
-    eval('$rta='.$_POST['a'].'_'.$_POST['tb'].'();');
-    if (is_array($rta)) json_encode($rta);
-	else echo $rta;
-  }   
+require_once "../lib/php/gestion.php";
+ini_set('display_errors', '1');
+$perf = perfil($_POST['tb']);
+if (!isset($_SESSION['us_sds'])) {
+	die("<script>window.top.location.href='/';</script>");
+} else {
+	$rta = "";
+	switch ($_POST['a']) {
+		case 'csv':
+			header_csv($_REQUEST['tb'] . '.csv');
+			$rs = array('', '');
+			echo csv($rs, '');
+			die;
+			break;
+		default:
+			eval('$rta=' . $_POST['a'] . '_' . $_POST['tb'] . '();');
+			if (is_array($rta)) json_encode($rta);
+			else echo $rta;
+	}
 }
 
-function lis_solcita(){
-	$info=datos_mysql("SELECT COUNT(*) total FROM adm_usunew
-	WHERE 1 ".whe_solcita());
-	$total=$info['responseResult'][0]['total'];
-	$regxPag=10;
-	$pag=(isset($_POST['pag-creausu']))? ($_POST['pag-creausu']-1)* $regxPag:0; 
-
-	
-	$sql="SELECT id_usu Caso,DOCUMENTO,NOMBRES,CORREO,PERFIL,TERRITORIO,BINA,COMPONENTE,USU_CREO CREO,FECHA_CREATE CREO,ESTADO
-	FROM adm_usunew 
-	 WHERE 1 ";
-	$sql.=whe_solcita();
-	$sql.=" ORDER BY fecha_create";
-	$sql.=' LIMIT '.$pag.','.$regxPag;
-	// echo $sql;
-		$datos=datos_mysql($sql);
-	return create_table($total,$datos["responseResult"],"creausu",$regxPag);
-} 
-
-function whe_solcita() {
-    $sql = "";
-	if ($_POST['fidpersona'])
-		$sql .= " AND id_persona like '%".$_POST['fidpersona']."%'";
-	if ($_POST['fdigita'])
-		$sql .= " AND usu_creo ='".$_POST['fdigita']."' ";
+function whe_catalogo() {
+	$sql = "";
+	if ($_POST['fidcata'])
+		$sql .= " AND idcatalogo='".$_POST['fidcata']."' ";
+	if ($_POST['fcatalogo'])
+		$sql .= " AND descripcion like '%".$_POST['fcatalogo']."%' ";
 	if ($_POST['festado'])
 		$sql .= " AND estado = '".$_POST['festado']."' ";
-	if ($_POST['fdes']) {
-		if ($_POST['fhas']) {
-			$sql .= " AND fecha_create >='".$_POST['fdes']." 00:00:00' AND fecha_create <='".$_POST['fhas']." 23:59:59'";
-		} else {
-			$sql .= " AND fecha_create >='".$_POST['fdes']." 00:00:00' AND fecha_create <='". $_POST['fdes']." 23:59:59'";
-		}
-	}
 	return $sql;
 }
 
+function lis_catalogo(){	
+	$info=datos_mysql("SELECT COUNT(*) total FROM catadeta	WHERE 1 ".whe_catalogo());
+	$total=$info['responseResult'][0]['total'];
+	$regxPag=17;
+	$pag=(isset($_POST['pag-catalogo']))? ($_POST['pag-catalogo']-1)* $regxPag:0; 
 
-function focus_solcita(){
- return 'creausu';
+	$sql="SELECT idcatalogo ACCIONES,idcatadeta ID,descripcion,estado,valor from catadeta
+	 WHERE 1  ";
+	$sql.=whe_catalogo();
+	$sql.=" ORDER BY 1,2,CAST(idcatadeta AS UNSIGNED), idcatadeta";
+	$sql.=' LIMIT '.$pag.','.$regxPag;
+	// echo $sql;
+		$datos=datos_mysql($sql);
+	return create_table($total,$datos["responseResult"],"catalogo",$regxPag);
 }
 
-function men_solcita(){
- $rta=cap_menus('creausu','pro');
- return $rta;
-} 
-
-
-function cap_menus($a,$b='cap',$con='con') {
-  $rta = ""; 
-  if ($a=='creausu'){  
-	$rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
-  	$rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";
-  }
-  return $rta;
-}
-
-
-function cmp_solcita(){
-	$t=['id_persona'=>'','tipo_doc'=>'','nombre1'=>'','nombre2'=>'','apellido1'=>'','apellido2'=>'',
- 'fecha_nacimiento'=>'','genero'=>'','observaciones'=>'','fecha'=>'','direccion'=>'','nacionalidad'=>'',
- 'tipo_cita'=>'','tel1'=>'','tel2'=>'','tel3'=>'','fecha_create'=>'','motivo'=>'',
- 'sexo'=>'','tipo'=>'','card'=>'','gast'=>'','gine'=>'','mein'=>'','nudi'=>'','ofta'=>'','ortr'=>'','pedi'=>'',
- 'psic'=>'','psiq'=>'','urol'=>'','pyd'=>''];
- $w='frecuencia';
-  $d='';//get_solcita(); 
-  //~ var_dump($d);
- if ($d=="") {$d=$t;}
- $u=($d['id_persona']=='')?true:false;
- $o='percit';
- $rta=" <span class='mensaje' id='".$w."-msj' ></span>";
- $c[]=new cmp($o,'e',null,'SOLICITAR CITA',$w);
- $c[]=new cmp('key','h',50,$_POST['id'],$w.' '.$o,'',0,'','','',false,'','col-4');
-  //~ $c[]=new cmp('ipe','h',10,$_POST['id'],$w,'','idp',null,'','',''); 
- $c[]=new cmp('idp','n',18,$d['id_persona'],$w.' '.$o,'N° Identificación',0,'rgxdfnum','#################',true,$u,'','col-4');
- $c[]=new cmp('tdo','s',3,$d['tipo_doc'],$w.' '.$o,'Tipo Documento','tipo_doc',null,null,true,$u,'','col-3','getPerson');
- $c[]=new cmp('no1','t',20,$d['nombre1'],$w.' '.$o,'Primer Nombre','nombre1',null,null,false,false,'','col-3');
- $c[]=new cmp('no2','t',20,$d['nombre2'],$w.' '.$o,'Segundo Nombre','nombre2',null,null,false,false,'','col-4');
- $c[]=new cmp('ap1','t',20,$d['apellido1'],$w.' '.$o,'Primer Apellido','apellido1',null,null,false,false,'','col-3');
- $c[]=new cmp('ap2','t',20,$d['apellido2'],$w.' '.$o,'Segundo Apellido','apellido2',null,null,false,false,'','col-3');
- $c[]=new cmp('edi','o',2,'',$w.' '.$o,'Actualiza Fecha ?','edi',null,null,false,true,'','col-2','enableUpd(this,\'afec\');');
- $c[]=new cmp('fen','d',10,$d['fecha_nacimiento'],$w.' afec '.$o,'Fecha de Nacimiento','fecha_nacimiento',null,null,false,false,'','col-4');
- $c[]=new cmp('gen','s',3,$d['sexo'],$w.' '.$o,'Sexo','genero',null,null,false,false,'','col-3');
- $c[]=new cmp('te1','t',10,$d['tel1'],$w.' '.$o,'Teléfono 1','',null,null,true,false,'','col-3');
- $c[]=new cmp('te2','t',10,$d['tel2'],$w.' '.$o,'Teléfono 2','',null,null,true,false,'','col-4');
- $c[]=new cmp('te3','h',10,$d['tel3'],$w.' '.$o,'Teléfono 3','',null,null,true,false,'','col-4');
- $c[]=new cmp('edi2','o',2,'',$w.' '.$o,'Actualiza Dirección ?','edi',null,null,false,true,'','col-2','enableUpd(this,\'adir\');');
- $c[]=new cmp('dir','t',20,$d['direccion'],$w.' adir '.$o,'Direccion','',null,null,false,false,'','col-6');
- $c[]=new cmp('tipo','s',3,$d['tipo'],$w.' '.$o,'Tipo de Cita','tipo_cita',null,null,false,true,'','col-3');
- 
-
- $w='especialidades';
- $o='espec';
- $c[]=new cmp(null,'e',null,'ESPECIALIDADES',$w);
- $c[]=new cmp('card','o',2,$d['card'],$w.' '.$o,'Cardiologia',null,null,true,false,'','col-0');
-$c[]=new cmp('gast','o',2,$d['gast'],$w.' '.$o,'Gastroenterologia',null,null,true,false,'','col-0');
-$c[]=new cmp('gine','o',2,$d['gine'],$w.' '.$o,'Ginecobstetricia',null,null,true,false,'','col-0');
-$c[]=new cmp('mein','o',2,$d['mein'],$w.' '.$o,'Medicina Interna',null,null,true,false,'','col-0');
-$c[]=new cmp('nudi','o',2,$d['nudi'],$w.' '.$o,'Nutricion y Dietetica',null,null,true,false,'','col-0');
-$c[]=new cmp('ofta','o',2,$d['ofta'],$w.' '.$o,'Oftalmologia',null,null,true,false,'','col-0');
-$c[]=new cmp('ortr','o',2,$d['ortr'],$w.' '.$o,'Ortopedia y/o Traumatologia',null,null,true,false,'','col-0');
-$c[]=new cmp('pedi','o',2,$d['pedi'],$w.' '.$o,'Pediatria',null,null,true,false,'','col-0');
-$c[]=new cmp('psic','o',2,$d['psic'],$w.' '.$o,'Psicologia',null,null,true,false,'','col-0');
-$c[]=new cmp('psiq','o',2,$d['psiq'],$w.' '.$o,'Psiquiatria',null,null,true,false,'','col-0');
-$c[]=new cmp('urol','o',2,$d['urol'],$w.' '.$o,'Urologia',null,null,true,false,'','col-0');
-
- $w='promocion';
- $o='pyd';
- $c[]=new cmp(null,'e',null,'PROMOCION Y DETECCCIÒN TEMPRANA',$w);
- $c[]=new cmp('pyd','s',3,$d['pyd'],$w.' '.$o,'Promocion y Detecciòn','pyd',null,null,false,true,'','col-0');
- for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
- return $rta;
-}
-
-function gra_creausu(){
-  
-	$rta=datos_mysql("select FN_USUARIO('".$_SESSION['us_sds']."') as usu;");
-	$usu=divide($rta["responseResult"][0]['usu']);
- 
-	$rta=datos_mysql("select FN_CATALOGODESC(218,'".$_POST['perfil']."') AS perfil ,FN_CATALOGODESC(202,'".$_POST['territorio']."') AS terr,FN_CATALOGODESC(217,'".$_POST['bina']."') AS bina;");
-	$data=$rta["responseResult"][0];
-
-
-	$sql1 = "INSERT INTO usuarios VALUES (?,?,?,?,?,?,?,?,?)";
-	if (isset($data['bina'])) {
-		$equ =$data['bina'];
-	} elseif(isset($data['terr'])) {
-		$equ =$data['terr'];
-	}else{
-		$equ ='';
-	}
-	
-	$params1 = [
-		['type' => 'i', 'value' => $_POST['documento']],
-		['type' => 's', 'value' => $_POST['nombre']],
-		['type' => 's', 'value' => $_POST['correo']],
-		['type' => 'z', 'value' => '$2y$10$U1.jyIhJweaZQlJK6jFauOAeLxEOTJX8hlWzJ6wF5YVbYiNk1xfma'],
-		['type' => 's', 'value' => $data['perfil']],
-		['type' => 'i', 'value' => $usu[2]],
-		['type' => 's', 'value' => $equ],
-		['type' => 's', 'value' => $usu[4]],
-		['type' => 's', 'value' => 'P']];
-		$rta2 = mysql_prepd($sql1, $params1);
-
-	if (strpos($rta2, "Correctamente")!== false) {
-		$rta = "Se ha Insertado: 1 Registro Correctamente.";
-		$sql = "INSERT INTO adm_usunew VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+function focus_catalogo(){
+	return 'catalogo';
+   }
    
-
-   $params = [
-	['type' => 'i', 'value' => NULL],
-	['type' => 'i', 'value' => $_POST['documento']],
-	['type' => 's', 'value' => $_POST['nombre']],
-	['type' => 's', 'value' => $_POST['correo']],
-	['type' => 's', 'value' => $data['perfil']],
-	['type' => 's', 'value' => $data['terr']],
-	['type' => 's', 'value' => $data['bina']],
-	['type' => 'i', 'value' => $usu[2]],
-	['type' => 's', 'value' => $usu[4]],
-	['type' => 'i', 'value' => $_SESSION['us_sds']],
-	['type' => 's', 'value' => date("Y-m-d H:i:s")],
-	['type' => 's', 'value' => NULL],
-	['type' => 's', 'value' => NULL],
-	['type' => 's', 'value' => 'R']];
-	$rta1 = mysql_prepd($sql, $params);
-	} else {
-		$rta = "Error: msj['No se puede crear la solicitud, el usuario ya se ha creado anteriormente']";
-	}
+   function men_catalogo(){
+	$rta=cap_menus('catalogo','pro');
 	return $rta;
-}
+   }
+   
+   function cap_menus($a,$b='cap',$con='con') {
+	$rta = "";
+	// $rta .= "<li class='fa-solid fa-floppy-disk $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
+	$rta .= "<button class='frm-btn $a grabar' onclick=\"grabar('$a', this);\"><span class='frm-txt'>Grabar</span><i class='fa-solid fa-floppy-disk icon'></i></button>";
+	/* $rta .="<button class='frm-btn $a actualizar' onclick=\"act_lista('".$a."',this);\"'>Actualizar</button>";
+	$rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('".$a."',this);\"></li>"; */
+	$rta .= "<li class='icono $a cancelar'    title='Cerrar'          Onclick=\"ocultar('".$a."','".$b."');\" >";
+	return $rta;
+  }
 
-function opc_tipo_doc($id=''){
-	return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=1 and estado='A' ORDER BY 1",$id);
-}
-
-function opc_genero($id=''){
-	return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=21 and estado='A' ORDER BY 1",$id);
-}
-
-function opc_pyd($id=''){
-	return opc_sql("SELECT `idcatadeta`,concat(idcatadeta,' - ',descripcion) FROM `catadeta` WHERE idcatalogo=39 and estado='A' AND idcatadeta<16 ORDER BY LENGTH(idcatadeta), idcatadeta",$id);	
-}
-function opc_tipo_cita($id=''){
-	return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=146 and estado='A' ORDER BY LENGTH(idcatadeta), idcatadeta",$id);	
-}
-function opc_estados($id=''){
-	return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=11 and estado='A' ORDER BY 1",$id);
-}
-
-
-function formato_dato($a,$b,$c,$d){
- $b=strtolower($b);
- $rta=$c[$d];
-// var_dump($a);
-// var_dump($rta);
-	if ($a=='creausu' && $b=='acciones'){
-		$rta="<nav class='menu right'>";		
-		$rta.="<li class='icono asigna1' title='Asignar Usuario' id='".$c['ACCIONES']."' Onclick=\"mostrar('creausu','pro',event,'','lib.php',7);\"></li>";
+  function cmp_catalogo(){
+	$rta="";
+	$t=['idcatalogo'=>'','idcatadeta'=>'','descripcion'=>'','valor'=>'','estado'=>'A'];
+	$w='catalogo';
+	//~ $id=explode('-',$_REQUEST['id']);
+	$d=get_catalogo(); 
+	//~ var_dump($d);
+	if ($d=="") {$d=$t;}
+	$d['estado']=($d['estado']=='A')?'SI':'NO';
+	if($d['idcatalogo']){$ids=$d['idcatalogo'].'_'.$d['idcatadeta'];}else{$ids='';}
+	$o='infcat';
+	$c[]=new cmp('id','h',100,$ids,$w,'',0,'','','',false,'','col-1');
+	$c[]=new cmp('cat','s',3,$d['idcatalogo'],$w.' '.$o,'Nombre Catalogo','catalogo',null,'',false,false,'','col-2');
+	$c[]=new cmp('cod','t',12,$d['idcatadeta'],$w.' '.$o,'Codigo Item','',null,'',false,false,'','col-2');
+	$c[]=new cmp('des','t',40,$d['descripcion'],$w.' '.$o,'Texto Descriptivo Item',null,null,'',false,false,'','col-2');
+	$c[]=new cmp('est','o',1,$d['estado'],$w.' '.$o,'Item Activo',null,null,'',false,false,'','col-2');
+	$c[]=new cmp('val','n',10,$d['valor'],$w.' '.$o,'Valor',0,'rgxdfnum','NNNN',false,true,'Número de 4 a 10 Digitos','col-1');
+	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
+	$rta.="</div>";
+	return $rta;
 	}
-	
- return $rta;
-}
+
+	function opc_catalogo(){
+	 return opc_sql("SELECT `idcatalogo`,concat(idcatalogo,' - ',nombre) FROM `catalogo` ORDER BY 1",$id = ($_POST['id'] == '') ? '' : divide($_POST['id'])[0]);
+	}
+
+	function get_catalogo(){
+		if($_POST['id']=='0'){
+			return "";
+		}else{
+			$id=divide($_POST['id']);
+			$sql="SELECT * FROM catadeta WHERE idcatalogo='".$id[0]."' AND idcatadeta='".$id[1]."'";
+			$info=datos_mysql($sql);
+			return $info['responseResult'][0];		
+		} 
+	}
+
+   function formato_dato($a,$b,$c,$d){
+	$b=strtolower($b);
+	$rta=$c[$d];
+	 if ($a=='catalogo'&& $b=='id'){$rta= "<div class='txt-center'>".$c['ID']."</div>";}
+	if (($a=='catalogo') && ($b=='acciones'))    {
+		   $rta="<nav class='menu right'>";
+		   $rta.="<li class='fa-solid fa-pen-to-square' title='Editar $a' id='".$c['ACCIONES']."_".$c['ID']."' Onclick=\"mostrar('catalogo','pro',event,'','lib.php',4);\"></li>";
+		   $rta.="</nav>";
+	   }    
+	return $rta;
+   }
 
 function bgcolor($a,$c,$f='c'){
- $rta="";
- return $rta;
-}
+	$rta="";
+	//~ if ($a=='transacciones'&&$c['ESTADO']=='A') $rta='green';
+	return $rta;
+   }
 ?>

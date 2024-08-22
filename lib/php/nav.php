f312b9ad2d1fb75a161b177ca5f82e411c2b0fe0
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-08-21 19:49:38 -0500

    08-21 7:49

--- lib/php/nav.php
+++ lib/php/nav.php
@@ -93,8 +93,14 @@ foreach ($menu as &$mainMenuItem) {
                         <a href="<?php echo $item['link']; ?>" class="main-item <?php echo isset($item['submenu']) ? 'has-submenu' : ''; ?>">
                             <i class="<?php echo $item['icon']; ?> icon"></i>
                             <span class="text nav-text"><?php echo $item['text']; ?></span>
-                            </a>
+                            <!-- </a> -->
                         <?php if (isset($item['submenu'])): ?>
+
+                            <i class="fa-solid fa-chevron-down submenu-icon"></i>
+                            <?php endif; ?>
+                            </a>
+                            <?php if (isset($item['submenu'])): ?>
+
                             <ul class="sub-menu">
                                 <?php foreach ($item['submenu'] as $subitem): ?>
                                     <li class="nav-link">

d4e47d33df1582ece1275eed2b3f5418c840fc09
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-08-22 14:35:11 -0500

    08-21 14:35

--- creacita/index.php
+++ creacita/index.php
@@ -70,32 +70,24 @@ $digitadores=opc_sql("SELECT `id_usuario`,nombre FROM `usuarios` WHERE`perfil`='
                 			<input type="number" id="fidp" name="fidp">
 						</div>
 
-							<div class="input-box">
-    					        <select class='captura' id="fest" name="fest">
-									<?php echo $estados; ?>
-    					        </select>
-    					        <label for="fest">Estado Solicitud</label>
-							</div>
+						<div class="input-box">
+							<label for="single-select">Estado Solicitud:</label>
+                			<select id="single-select fest" class='captura' id="fest" name="fest">
+								 <?php echo $estados; ?>
+                			</select>
+    					</div>
 
-							<div class="input-box">
-								<label for="single-select">Estado Solicitud:</label>
-                				<select id="single-select fest" class='captura' id="fest" name="fest">
-									 <?php echo $estados; ?>
-                				</select>
-    					    </div>
+						<div class="input-box">
+    					       <input  type="date" class='captura' id="fdes" name="fdes">
+    					       <label for="fdes">Fecha Desde</label>
+    					   </div>
+						
+						<div class="input-box">
+    					       <input  type="date" class='captura' id="fhas" name="fhas">
+    					       <label for="fhas">Fecha Hasta</label>
+    					   </div>
 
-							<div class="input-box">
-    					        <input  type="date" class='captura' id="fdes" name="fdes">
-    					        <label for="fdes">Fecha Desde</label>
-    					    </div>
-
-							<div class="input-box">
-    					        <input  type="date" class='captura' id="fhas" name="fhas">
-    					        <label for="fhas">Fecha Hasta</label>
-    					    </div>
-
-
-    					    <button type="submit" class="btn" OnChange="actualizar();">Aplicar</button>
+   					    <button type="submit" class="btn" OnChange="actualizar();">Aplicar</button>
     					</form>
 					</div>
 				<div class='load' id='loader' z-index='0'></div>

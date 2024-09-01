ad0e5d7401386e374c3a0d9b29b0ffed9b4dffcc
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-09-01 16:54:45 -0500

    09-01 16:54

--- creacita/index.php
+++ creacita/index.php
@@ -169,42 +169,6 @@ button:hover {
 			}
 		}
 
-		document.addEventListener("DOMContentLoaded", () => {
-    const button = document.querySelector("button"),
-        toast = document.querySelector(".toast"),
-        closeIcon = document.querySelector(".close"),
-        progress = document.querySelector(".progress");
-
-    let timer1, timer2;
-
-    if (button && toast && closeIcon && progress) {
-        button.addEventListener("click", () => {
-            console.log('click 1');
-            toast.classList.add("active");
-            progress.classList.add("active");
-
-            timer1 = setTimeout(() => {
-                toast.classList.remove("active");
-            }, 5000);
-
-            timer2 = setTimeout(() => {
-                progress.classList.remove("active");
-            }, 5300);
-        });
-
-        closeIcon.addEventListener("click", () => {
-            console.log('click para cerrar la alerta ya sea manual o automáticamente.');
-            clearTimeout(timer1);
-            clearTimeout(timer2);
-
-            toast.classList.remove("active");
-            progress.classList.remove("active");
-        });
-    } else {
-        console.error('Uno o más elementos no fueron encontrados en el DOM.');
-    }
-});
-
 	</script>
 </head>
 

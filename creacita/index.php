6c740da9910ab375a76cd45187c88c0b55df5951
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-09-01 10:31:41 -0500

    09-01 10:31

--- creacita/index.php
+++ creacita/index.php
@@ -169,39 +169,44 @@ button:hover {
 			}
 		}
 
-		const button = document.querySelector("button"),
-toast        = document.querySelector(".toast");
-closeIcon    = document.querySelector(".close"),
-progress     = document.querySelector(".progress");
+		document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector("button"),
+        toast = document.querySelector(".toast"),
+        closeIcon = document.querySelector(".close"),
+        progress = document.querySelector(".progress");
 
-let timer1, timer2;
+    let timer1, timer2;
 
-button.addEventListener("click", () => {
-    console.log('click 1')
-  toast.classList.add("active");
-  progress.classList.add("active");
+    if (button && toast && closeIcon && progress) {
+        button.addEventListener("click", () => {
+            console.log('click 1');
+            toast.classList.add("active");
+            progress.classList.add("active");
 
-  timer1 = setTimeout(() => {
-    toast.classList.remove("active");
-  }, 5000); //1s = 1000 milliseconds
+            timer1 = setTimeout(() => {
+                toast.classList.remove("active");
+            }, 5000);
 
-  timer2 = setTimeout(() => {
-    progress.classList.remove("active");
-  }, 5300);
+            timer2 = setTimeout(() => {
+                progress.classList.remove("active");
+            }, 5300);
+        });
+
+        closeIcon.addEventListener("click", () => {
+            console.log('click para cerrar la alerta ya sea manual o automáticamente.');
+            toast.classList.remove("active");
+
+            setTimeout(() => {
+                progress.classList.remove("active");
+            }, 300);
+
+            clearTimeout(timer1);
+            clearTimeout(timer2);
+        });
+    } else {
+        console.error('Uno o más elementos no fueron encontrados en el DOM.');
+    }
 });
-
-closeIcon.addEventListener("click", () => {
-    console.log('click para cerrar la alerta ya sea manual o automaticamente.')
-  toast.classList.remove("active");
-
-  setTimeout(() => {
-    progress.classList.remove("active");
-  }, 300);
-
-  clearTimeout(timer1);
-  clearTimeout(timer2);
-});
-
 	</script>
 </head>
 

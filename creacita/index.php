6a45c5a1a1bda130dc72de443413e3f9907104e7
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-09-01 01:41:44 -0500

    09-01 01:41

--- creacita/index.php
+++ creacita/index.php
@@ -10,32 +10,126 @@ $ayer = date("Y-m-d",strtotime($hoy."- 2 days"));
 // $info=datos_mysql("SELECT nombre,perfil FROM usuarios WHERE id_usuario='".$_SESSION["us_riesgo"]."'");
 ?>
 <Style>
-	.loader-wrapper {
-    width: 150px;
-    height: 100px;
-    float: left!important;
+	.toast {
+    position: absolute;
+    top: 25px;
+    right: 30px;
+    border-radius: 12px;
+    background: #fff;
+    padding: 20px 35px 20px 25px;
+    box-shadow: 0 6px 20px -5px rgba(0, 0, 0, 0.1);
+    overflow: hidden;
+    transform: translateX(calc(100% + 30px));
+    transition: all 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.35);
+
+    animation: efectoBounce .3s ease alternate;
 }
-.d-flex {
-    display: flex!important;
+
+.toast.active {
+    transform: translateX(0%);
 }
-.ball-pulse>div:nth-child(1) {
-    animation: scale .75s -.24s infinite cubic-bezier(.2,.68,.18,1.08);
+
+.toast .toast-content {
+    display: flex;
+    align-items: center;
 }
-.ball-pulse>div:nth-child(2) {
-    animation: scale .75s -.12s infinite cubic-bezier(.2,.68,.18,1.08);
+
+.toast-content .check {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 35px;
+    min-width: 35px;
+    background-color: #4070f4;
+    color: #fff;
+    font-size: 20px;
+    border-radius: 50%;
 }
-.ball-pulse>div:nth-child(3) {
-    animation: scale .75s 0s infinite cubic-bezier(.2,.68,.18,1.08);
+
+.toast-content .message {
+    display: flex;
+    flex-direction: column;
+    margin: 0 20px;
 }
-.ball-pulse>div {
-    background-color: #3f6ad8;
-    width: 15px;
-    height: 15px;
-    border-radius: 100%;
-    margin: 2px;
-    animation-fill-mode: both;
-    display: inline-block;
+
+.message .text {
+    font-size: 16px;
+    font-weight: 400;
+    color: #666666;
 }
+
+.message .text.text-1 {
+    font-weight: 600;
+    color: #333;
+}
+
+.toast .close {
+    position: absolute;
+    top: 10px;
+    right: 15px;
+    padding: 5px;
+    cursor: pointer;
+    opacity: 0.7;
+}
+
+.toast .close:hover {
+    opacity: 1;
+}
+
+.toast .progress {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 3px;
+    width: 100%;
+
+}
+
+.toast .progress:before {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    background-color: #4070f4;
+}
+
+.progress.active:before {
+    animation: progress 5s linear forwards;
+}
+
+@keyframes progress {
+    100% {
+        right: 100%;
+    }
+}
+
+button {
+    padding: 12px 20px;
+    font-size: 20px;
+    outline: none;
+    border: none;
+    background-color: #4070f4;
+    color: #fff;
+    border-radius: 6px;
+    cursor: pointer;
+    transition: 0.3s;
+}
+
+button:hover {
+    background-color: #0e4bf1;
+}
+
+.toast.active~button {
+    pointer-events: none;
+}
+
+
+@keyframes efectoBounce{
+    0% {transform: translateY(0px);}
+    100% {transform: translateY(-10px);}
+  }
 </Style>
 <head>
 	<meta charset="UTF-8">
@@ -127,15 +221,19 @@ $ayer = date("Y-m-d",strtotime($hoy."- 2 days"));
 						<!-- <button  type=button class="btn" OnClick="actualizar();">Quitar Filtros</button> -->
 						<button  class="btn" OnClick="actualizar();">Aplicar</button>
 					</div>
-					<div class="loader-wrapper d-flex justify-content-center align-items-center">
-						<div class="loader">
-							<div class="ball-pulse">
-								<div style="background-color: rgb(63, 106, 216);"></div>
-								<div style="background-color: rgb(63, 106, 216);"></div>
-								<div style="background-color: rgb(63, 106, 216);"></div>
-							</div>
-						</div>
+					<div class="toast active">
+						<div class="toast-content">
+					    	<i class="fas fa-solid fa-check check"></i>
+					    	<div class="message">
+					      		<span class="text text-1">Felicitaciones 🥇</span>
+					      		<span class="text text-2">Excelente notificación para tu Sistema.</span>
+					    	</div>
+					  	</div>
+					  	<i class="fa-solid fa-xmark close"></i>
+					  	<div class="progress active"></div>
 					</div>
+
+
 				<div class='load'id='loader' z-index='0'></div>
 				</div>
 				<div class="content content-2">
@@ -170,4 +268,38 @@ $ayer = date("Y-m-d",strtotime($hoy."- 2 days"));
 			</h4>
 		</div>
 	</div>
+	<script>
+		const button = document.querySelector("button"),
+toast        = document.querySelector(".toast");
+closeIcon    = document.querySelector(".close"),
+progress     = document.querySelector(".progress");
+
+let timer1, timer2;
+
+button.addEventListener("click", () => {
+    console.log('click 1')
+  toast.classList.add("active");
+  progress.classList.add("active");
+
+  timer1 = setTimeout(() => {
+    toast.classList.remove("active");
+  }, 5000); //1s = 1000 milliseconds
+
+  timer2 = setTimeout(() => {
+    progress.classList.remove("active");
+  }, 5300);
+});
+
+closeIcon.addEventListener("click", () => {
+    console.log('click para cerrar la alerta ya sea manual o automaticamente.')
+  toast.classList.remove("active");
+
+  setTimeout(() => {
+    progress.classList.remove("active");
+  }, 300);
+
+  clearTimeout(timer1);
+  clearTimeout(timer2);
+});
+	</script>
 </body>

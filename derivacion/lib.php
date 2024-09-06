f509ba3d2960cde0d2366d0c6fd7f001bbc974f7
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-09-06 10:35:06 -0500

    09-06 10:34

--- derivacion/lib.php
+++ derivacion/lib.php
@@ -88,8 +88,8 @@ function focus_deriva(){
 	$c[]=new cmp('tip','s',3,$d['tipo_doc'],$w.' '.$o,'tipo_doc','tipo_doc',null,'',false,false,'','col-2');
 	$c[]=new cmp('pre','h',15,$d['predio'],$w.' '.$o,'Predio',null,null,'',false,false,'','col-2');
 	$c[]=new cmp('asi','s',12,$d['derivado_colaborador'],$w.' '.$o,'Asignado A','asignado',null,'',false,false,'','col-2');
-	$c[]=new cmp('rea','o',1,$d['realizado'],$w.' '.$o,'Realizado',null,null,'',false,false,'','col-2');
-	$c[]=new cmp('obs','t',10,$d['observacion'],$w.' '.$o,'Observacion',null,null,'',false,true,'Número de 4 a 10 Digitos','col-1');
+	$c[]=new cmp('rea','o',1,$d['realizado'],$w.' '.$o,'Realizado',null,null,'',false,true,'','col-2');
+	$c[]=new cmp('obs','t',10,$d['observacion'],$w.' '.$o,'Observacion',null,null,'',false,false,'Número de 4 a 10 Digitos','col-1');
 	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
 	$rta.="</div>";
 	return $rta;

e03b238e66328b8aac55bdf665da3d94fc0d1447
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-09-13 12:55:38 -0500

    09-13 12:55

--- derivacion/lib.php
+++ derivacion/lib.php
@@ -96,7 +96,7 @@ function focus_deriva(){
 	$t=['id_deriva'=>'','documento'=>'','tipo_doc'=>'','predio'=>'','derivado_colaborador'=>'','realizado'=>'','observacion'=>''];
 	$w='deriva';
 	$uPd = explode('-',$_REQUEST['id'])=='' ? true : false;
-	echo explode('-',$_REQUEST['id']);
+	var_dump(explode('-',$_REQUEST['id']));
 	$d=get_deriva(); 
 	//var_dump($d);
 	if ($d=="") {$d=$t;}

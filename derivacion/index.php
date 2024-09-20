a95b73bbd293b21f9f96b0c5ba27e206d5951ed8
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-09-20 11:05:53 -0500

    09-20 11:05

--- derivacion/index.php
+++ derivacion/index.php
@@ -210,9 +210,9 @@ button:hover {
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Crear Cita || SIGREV</title>
-	<link rel="stylesheet" href="../lib/css/app.css">
+	<link rel="stylesheet" href="../lib/css/app.css?v=1.1">
 	<link rel="stylesheet" href="../lib/css/stylePop.css">
-	<script src="../lib/js/main.js?v=3.10.1"></script>
+	<script src="../lib/js/main.js?v=1.1"></script>
 	<script src="../lib/js/d.js"></script>
 	<script src="../lib/js/popup.js"></script>
 	<link rel="stylesheet" href="../lib/css/choices.min.css">

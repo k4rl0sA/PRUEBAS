dd454984ac8c47585d5c2d99d7f1c5f336c39a46
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-09-25 15:46:10 -0500

    09-25 15:46

--- lib/php/gestion.php
+++ lib/php/gestion.php
@@ -463,7 +463,7 @@ function rol($a){ //a=modulo, b=perfil c=componente
 	$rta=array();
 	$sql="SELECT perfil,componente,crear,editar,consultar,exportar,importar FROM adm_roles WHERE modulo = '".$a."' and perfil = FN_PERFIL('".$_SESSION['us_sds']."') AND componente=FN_COMPONENTE('".$_SESSION['us_sds']."') AND estado = 'A'";
 	$data=datos_mysql($sql);
-  print_r($sql);
+  // print_r($sql);
 	if ($data && isset($data['responseResult'][0])) {
         $rta = $data['responseResult'][0];
     }

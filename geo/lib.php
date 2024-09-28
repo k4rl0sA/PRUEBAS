253c05b7a3ef5a5b803baf4c88b8c7c894991ba0
Ferchokar <114622263+Ferchokar@users.noreply.github.com>
2024-09-28 15:37:31 -0500

    Update lib.php

--- geo/lib.php
+++ geo/lib.php
@@ -99,7 +99,7 @@ function get_predio(){
 	$sql="SELECT G.idgeo,G.zona, G.localidad, G.upz, G.barrio, G.sector_catastral, G.nummanzana, G.predio_num, G.unidad_habit, G.direccion, G.vereda, G.cordx, G.cordy, G.territorio 
  	FROM `geo_asig` A 
   	LEFT JOIN hog_geo G ON A.id_geo=G.idgeo 
-   	WHERE A.id_geo ='".$id[0]."'";
+   	WHERE A.estado='A' AND A.id_geo ='".$id[0]."'";
 	$info=datos_mysql($sql);
 	if (!$info['responseResult']) {
 		return json_encode (new stdClass);

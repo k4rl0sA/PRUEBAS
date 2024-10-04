6e0ca89771c9c83865f31bb012fdaf4c588532fd
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-04 12:04:20 -0500

    10-03 asigpredio

--- asignacion/lib.php
+++ asignacion/lib.php
@@ -93,7 +93,7 @@ function cmp_asigpred(){
 
 function get_predio(){
 	// print_r($_POST);
-	$id=divide($_POST['id']);
+	$id=divide($_POST['cod_pre']);
 	$sql="SELECT G.idgeo,G.zona, G.localidad, G.upz, G.barrio, G.sector_catastral, G.nummanzana, G.predio_num, G.unidad_habit, G.direccion, G.vereda, G.cordx, G.cordy, G.territorio 
  	FROM `geo_asig` A 
   	LEFT JOIN hog_geo G ON A.id_geo=G.idgeo 

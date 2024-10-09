44546a13f0d1b45bb394e30e881d3fe69d63809d
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-09 12:30:09 -0500

    10-03 09:20

--- crea-fam/lib.php
+++ crea-fam/lib.php
@@ -207,6 +207,7 @@ function namequipo(){
 		if (!$info['responseResult']) {
 			return '';
 		}
+		print_r($info['responseResult'][0]['equipo']);
 		return json_encode($info['responseResult'][0]['equipo']);
 }
 

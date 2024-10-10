b9ef9e078faa2af499a7ed6a6ef1836ed6a2ab3d
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-09 22:55:28 -0500

    10-09 creaFam

--- crea-fam/lib.php
+++ crea-fam/lib.php
@@ -45,7 +45,7 @@ function opc_usuario(){
 function lis_homes(){
 	$total="SELECT COUNT(*) AS total FROM (
 		SELECT G.idgeo AS ACCIONES,G.idgeo AS Cod_Predio,H.direccion,H.sector_catastral Sector,H.nummanzana AS Manzana,H.predio_num AS predio,H.unidad_habit AS 'Unidad',FN_CATALOGODESC(2,H.localidad) AS 'Localidad',U1.nombre,G.fecha_create,FN_CATALOGODESC(44,G.estado_v) AS estado 
-		FROM geo_gest G	LEFT JOIN hog_geo H ON G.idgeo = H.idgeo LEFT JOIN hog_fam F ON G.idgeo = F.idpre LEFT JOIN usuarios U ON H.subred = U.subred	LEFT JOIN usuarios U1 ON H.usu_creo = U1.id_usuario
+		FROM geo_gest G	LEFT JOIN hog_geo H ON G.idgeo = H.idgeo LEFT JOIN usuarios U ON H.subred = U.subred	LEFT JOIN usuarios U1 ON H.usu_creo = U1.id_usuario
 			WHERE G.estado_v IN ('7') ".whe_homes()."
 			AND U.id_usuario = '{$_SESSION['us_sds']}'
 ) AS Subquery";
@@ -67,7 +67,6 @@ $sql="SELECT G.idgeo AS ACCIONES,
 	G.fecha_create,
 	FN_CATALOGODESC(44,G.estado_v) AS estado
 	FROM geo_gest G
-	LEFT JOIN hog_fam F ON G.idgeo = F.idpre
 	LEFT JOIN hog_geo H ON G.idgeo = H.idgeo
 	LEFT JOIN usuarios U ON H.subred = U.subred
 	LEFT JOIN usuarios U1 ON H.usu_creo = U1.id_usuario

1191bfb255c6f7ad6e82af908a2952df69c1fbb6
Ferchokar <114622263+Ferchokar@users.noreply.github.com>
2024-10-15 11:21:49 -0500

    fer-10-15ruteo

--- ruteo/lib.php
+++ ruteo/lib.php
@@ -96,7 +96,7 @@ function cmp_rute(){
  $c[]=new cmp('tipo_prior','s','3',$d['tipo_prior'],$w.' '.$o,'GRUPO DE POBLACION PRIORIZADA','tipo_prior',null,null,false,$u,'','col-3');
 
  $o='atemed';
- $c[]=new cmp($o,'e',null,'DATOS DEL USUARIO QUE REQUIERE LA ATENCIÓN MEDICA',$w);
+ //$c[]=new cmp($o,'e',null,'DATOS DEL USUARIO QUE REQUIERE LA ATENCIÓN MEDICA',$w);
  $c[]=new cmp('tipo_doc','s','3',$d['tipo_doc'],$w.' '.$o,'TIPO DE DOCUMENTO','tipo_doc',null,null,false,$u,'','col-2');
  $c[]=new cmp('documento','t','20',$d['documento'],$w.' '.$o,'NUMERO DE DOCUMENTO','documento',null,null,false,$u,'','col-2');
  $c[]=new cmp('nombres','t','80',$d['nombres'],$w.' '.$o,'NOMBRES Y APELLIDOS DEL USUARIO','nombres',null,null,false,$u,'','col-4');
@@ -107,14 +107,14 @@ function cmp_rute(){
  $c[]=new cmp('regimen','s','3',$d,$w.' '.$o,'regimen','regimen',null,null,false,$u,'','col-2');
  $c[]=new cmp('eapb','s','3',$d,$w.'  '.$o,'eapb','eapb',null,null,false,$u,'','col-2');
  
- //$o='datacu';
- $c[]=new cmp($o,'e',null,'DATOS DEL ACUDIENTE (Estas variables se diligencian para los menores de edad), o datos del usuario quien coloco la solicitud  PQR',$w);
+ $o='datacu';
+ //$c[]=new cmp($o,'e',null,'DATOS DEL ACUDIENTE (Estas variables se diligencian para los menores de edad), o datos del usuario quien coloco la solicitud  PQR',$w);
  $c[]=new cmp('tipo_doc_acu','s','3',$d['tipo_doc_acu'],$w.' '.$o,'TIPO DE DOCUMENTO ACUDIENTE','tipo_doc_acu',null,null,false,$u,'','col-2');
  $c[]=new cmp('documento_acu','t','20',$d['documento_acu'],$w.' '.$o,'DOCUMENTO ACUDIENTE','documento_acu',null,null,false,$u,'','col-2');
  $c[]=new cmp('nombres_acu','t','80',$d['nombres_acu'],$w.' '.$o,'NOMBRES Y APELLIDOS DEL ACUDIENTE','nombres_acu',null,null,false,$u,'','col-6');
 
- //$o='datcon';
- $c[]=new cmp($o,'e',null,'DATOS DE CONTACTO',$w);
+ $o='datcon';
+ //$c[]=new cmp($o,'e',null,'DATOS DE CONTACTO',$w);
  $c[]=new cmp('direccion','t','90',$d['direccion'],$w.' '.$o,'Direccion','direccion',null,null,false,$u,'','col-4');
  $c[]=new cmp('telefono1','n','10',$d['telefono1'],$w.' '.$o,'Telefono 1','telefono1',null,null,false,$u,'','col-2');
  $c[]=new cmp('telefono2','n','10',$d['telefono2'],$w.' '.$o,'Telefono 2','telefono2',null,null,false,$u,'','col-2');
@@ -122,7 +122,7 @@ function cmp_rute(){
 
  $o='datcup';
  $c[]=new cmp($o,'e',null,'CONSULTA AUTORIZADA',$w);
- $c[]=new cmp('fecha_consulta','d','90',$d['direccion'],$w.' '.$o,'Fecha Ultima Consulta','fecha_consulta',null,null,false,$u,'','col-3');
+ $c[]=new cmp('fecha_consulta','d','90',$d['direccion'],$w.' '.$o,'Fecha Ultima Consulta','fecha_asig',null,null,false,$u,'','col-3');
  $c[]=new cmp('cod_cups','n','10',$d['cod_cups'],$w.' '.$o,'Cups Autorizado por EAPB','cod_cups',null,null,false,$u,'','col-3');
  $c[]=new cmp('per_consul','n','10',$d['per_consul'],$w.' '.$o,'Perfil de la Consulta','per_consul',null,null,false,$u,'','col-4');
  

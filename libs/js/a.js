654ee35e629d94a02bf8640847bd28a80bb8c08f
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-15 01:37:34 -0500

    10-14 creaMenu

--- libs/js/a.js
+++ libs/js/a.js
@@ -379,6 +379,17 @@ function mostrar(tb, a='', ev, m='', lib=ruta_app, w=7, tit='', k='0') {
 		if(document.querySelector('.closePanelAcc')!=undefined)	{
 			const closePanelAcc = document.querySelector('.closePanelAcc');
 		}
+		if(buttons!=undefined)	{
+		const buttons = [
+            {icon: 'fas fa-plus', text: 'Nuevo', color: 'white',short:'Ctrl N'},
+            {icon: 'fas fa-trash', text: 'Eliminar', color: 'white',short:'Ctrl Supr'},
+            {icon: 'fas fa-file-pdf', text: 'PDF', color: 'white',short:'Ctrl P'},
+            {icon: 'fas fa-eye', text: 'Ver', color: 'white',short:'Ctrl E'},
+            {icon: 'fas fa-pregnant', text: 'Gestantes', color: 'white',short:'Ctrl G'},
+            {icon: 'fas fa-vial', text: 'Caracterización', color: 'white',short:'Ctrl R'},
+            {icon: 'fas fa-medkit', text: 'Atención Médica', color: 'white',short:'Ctrl A'},
+        ];
+	}
 		const Id = ev.target.id;
 		crear_menu(Id,lib);
 	}
@@ -386,6 +397,7 @@ function mostrar(tb, a='', ev, m='', lib=ruta_app, w=7, tit='', k='0') {
 	if (document.getElementById(tb+'-msj')!=undefined) document.getElementById(tb+'-msj').innerHTML="";
     foco(inner(id+'-foco'));
 }
+
 
 function crear_menu(itemId,url){
 	fetch(url)

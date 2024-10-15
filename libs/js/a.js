cb9108d1470113d6f78ab7c86a6e94db79cce5d5
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-15 02:16:08 -0500

    10-14 creaMenu

--- libs/js/a.js
+++ libs/js/a.js
@@ -379,7 +379,7 @@ function mostrar(tb, a='', ev, m='', lib=ruta_app, w=7, tit='', k='0') {
 		if(document.querySelector('.closePanelAcc')!=undefined)	{
 			const closePanelAcc = document.querySelector('.closePanelAcc');
 		}
-		buttons=pFetch(lib=ruta_app,'a=opc&tb='+ tb.toLowerCase(), false)
+		const buttons = pFetch(lib=ruta_app,'a=opc&tb='+ tb.toLowerCase());
 		if(buttons!=undefined){
 			const buttons = [];
 		}

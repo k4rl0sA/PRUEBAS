12821d902e103bb96e39c24a8cf86b33f1b22a8e
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-16 13:23:06 -0500

    10-16 Ruteolib 13:11

--- ruteo/lib.php
+++ ruteo/lib.php
@@ -149,9 +149,9 @@ function cmp_rute(){
  $c[]=new cmp('estado_g','s',2,$d['estado_g'],$w.' pRe '.$o,'estado','estado_g',null,null,true,$x,'','col-4',"enabFielSele(this,['motivo_estado']);tipVivi('estado_g','StG');");//
  $c[]=new cmp('motivo_estado','s','3',$d['motivo_estado'],$w.' '.$o,'motivo_estado','motivo_estado',null,null,false,false,'','col-4','validState(this);');
  $c[]=new cmp('direccion_nueva','t','90',$d['direccion_nueva'],$w.' dir '.$o,'Direccion Nueva','direccion_nueva',null,null,false,$u,'','col-25');
- $c[]=new cmp('sector_catastral_n','n','6',$d['sector_catastral_n'],$w.' '.$o,'Sector Catastral (6)','sector_catastral_n',null,null,false,$u,'','col-25');
- $c[]=new cmp('nummanzana_n','n','3',$d['nummanzana_n'],$w.' '.$o,'Nummanzana (3)','nummanzana_n',null,null,false,$u,'','col-25');
- $c[]=new cmp('predio_num_n','n','3',$d['predio_num_n'],$w.' '.$o,'Predio de Num (3)','predio_num_n',null,null,false,$u,'','col-25');
+ $c[]=new cmp('sector_catastral_n','n','6',$d['sect'],$w.' '.$o,'Sector Catastral (6)','sector_catastral_n',null,null,false,$u,'','col-25');
+ $c[]=new cmp('nummanzana_n','n','3',$d['manz'],$w.' '.$o,'Nummanzana (3)','nummanzana_n',null,null,false,$u,'','col-25');
+ $c[]=new cmp('predio_num_n','n','3',$d['pred'],$w.' '.$o,'Predio de Num (3)','predio_num_n',null,null,false,$u,'','col-25');
  $c[]=new cmp('observacion','a',50,$d['observacion'],$w.' '.$o,'Observacion','observacion',null,null,true,true,'','col-10');
  for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
  return $rta;

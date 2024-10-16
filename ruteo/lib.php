d5714d908382c1d83d8955653f38083b239720c8
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-16 13:20:26 -0500

    10-16 Ruteolib 13:11

--- ruteo/lib.php
+++ ruteo/lib.php
@@ -304,7 +304,7 @@ function get_rute(){
 		$id=divide($_POST['id']);
 		$sql="SELECT `id_ruteo`, R.`idgeo`,`fuente`, `fecha_asig`, `priorizacion`,tipo_prior, `tipo_doc`, `documento`, `nombres`, `fecha_nac`, `sexo`, `nacionalidad`, `etnia`,`regimen`,`eapb`, 
 		`tipo_doc_acu`, `documento_acu`, `nombres_acu`, R.direccion,`telefono1`, `telefono2`, `telefono3`, `fecha_consulta`,`cod_cups`,`per_consul`,R.`subred_report`, G.`localidad`, G.`upz`, G.`barrio`, 
-		G.sector_catastral,G.nummanzana,G.predio_num,G.unidad_habit,G.`cordx`, G.`cordy`,RG.fecha_gestion,RG.estado_g,RG.motivo_estado,RG.direccion_nueva, RG.sector_catastral as 'sector_catastral_n' se,RG.nummanzana,RG.predio_num,RG.observaciones
+		G.sector_catastral,G.nummanzana,G.predio_num,G.unidad_habit,G.`cordx`, G.`cordy`,RG.fecha_gestion,RG.estado_g,RG.motivo_estado,RG.direccion_nueva, RG.sector_catastral as 'sector_catastral_n',RG.nummanzana,RG.predio_num,RG.observaciones
 		 FROM `eac_ruteo` R
 		 LEFT JOIN hog_geo G ON R.idgeo=G.idgeo
 		 LEFT JOIN eac_ruteo_ges RG ON R.id_ruteo=RG.idruteo

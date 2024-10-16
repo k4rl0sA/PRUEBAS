c9a8395e79d092b729ce21038a65e6b678027e7d
Ferchokar <114622263+Ferchokar@users.noreply.github.com>
2024-10-16 13:09:24 -0500

    20241016_fer_ruteo

--- ruteo/lib.php
+++ ruteo/lib.php
@@ -307,7 +307,7 @@ function get_rute(){
 		G.sector_catastral,G.nummanzana,G.predio_num,G.unidad_habit,G.`cordx`, G.`cordy`,RG.fecha_gestion,RG.estado_g,RG.motivo_estado,RG.sector_catastral,RG.nummanzana,RG.predio_num,RG.observaciones
 		 FROM `eac_ruteo` R
 		 LEFT JOIN hog_geo G ON R.idgeo=G.idgeo
-		 LEFT JOIN eac_ruteo_ges RG ON R.id_ruteo=RG.idruteo;
+		 LEFT JOIN eac_ruteo_ges RG ON R.id_ruteo=RG.idruteo
 		 WHERE  id_ruteo='{$id[0]}'";
 		$info=datos_mysql($sql);
 		if (!$info['responseResult']) {

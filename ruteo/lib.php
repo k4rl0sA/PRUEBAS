38986f99cc5e39761cfc3a043b81e4325e75b882
Ferchokar <114622263+Ferchokar@users.noreply.github.com>
2024-10-16 11:25:37 -0500

    20241016_fer_ruteo

--- ruteo/lib.php
+++ ruteo/lib.php
@@ -292,7 +292,7 @@ function get_rute(){
 	}else{
 		$id=divide($_POST['id']);
 		$sql="SELECT `id_ruteo`, `fuente`, `fecha_asig`, 'tipo_prior',`priorizacion`, `tipo_doc`, `documento`, `nombres`, `fecha_nac`, `sexo`, `nacionalidad`, 
-		`tipo_doc_acu`, `documento_acu`, `nombres_acu`, `direccion`, `telefono1`, `telefono2`, `telefono3`, `subred`, `localidad`, `upz`, `barrio`, 
+		`tipo_doc_acu`, `documento_acu`, `nombres_acu`, `direccion`, `telefono1`, `telefono2`, `telefono3`, `subred_report`, `localidad`, `upz`, `barrio`, 
 		sector_catastral,nummanzana,predio_num,unidad_habit,`cordx`, `cordy`, `perfil_asignado`,`fecha_gestion`, `estado_g`, `motivo_estado`, `direccion_nueva`, `complemento`, `observacion`,integrantes,predio,cod_admin
 		 FROM `eac_ruteo` WHERE  id_ruteo='{$id[0]}'";
 		$info=datos_mysql($sql);

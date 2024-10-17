531a7d321e5f968bde42a2c2e1db3a2e80c60ab9
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-17 15:31:19 -0500

    10-17 Alertas 15:25

--- crea-fam/alertas.php
+++ crea-fam/alertas.php
@@ -113,13 +113,18 @@ function men_alertas(){
 	$o='infcom';
 	$c[]=new cmp($o,'e',null,'DATOS COMPLEMENTARIOS',$w);
 	
-	$c[]=new cmp('men_dnt','s','2',$d,$w.' '.$o,'Menor de 5 años con DNT Aguda','rta',null,null,true,true,'','col-2')
-	$c[]=new cmp('men_sinctrl','s','2',$d,$w.' '.$o,'Sin Atencion Ruta Alteracion Nutricional','rta',null,null,true,true,'','col-2')
+	if($p['ano']<5){
+		$c[]=new cmp('men_dnt','s','2',$d,$w.' '.$o,'Menor de 5 años con DNT Aguda','rta',null,null,true,true,'','col-2')
+		$c[]=new cmp('men_sinctrl','s','2',$d,$w.' '.$o,'Sin Atencion Ruta Alteracion Nutricional','rta',null,null,true,true,'','col-2')
+	}
 
-	$c[]=new cmp('gestante','s','2',$d,$w.' '.$o,'El usuario es gestante','rta',null,null,$gest,$gest,'','col-2',"valGluc('glucometria');enabOthSi('gestante','eTp');");
+	if($gest){
+		$c[]=new cmp('gestante','s','2',$d,$w.' '.$o,'El usuario es gestante','rta',null,null,$gest,$gest,'','col-2',"valGluc('glucometria');enabOthSi('gestante','eTp');");
 	$c[]=new cmp('etapgest','s','3',$d,$w.' eTp '.$o,'Etapa Gestacional','etapgest',null,'',$gest,$gest,'','col-25');//true
 	$c[]=new cmp('ges_sinctrl','s','3',$d,$w.' eTp '.$o,'Gestante Sin Control','ges_sinctrl',null,'',$gest,$gest,'','col-25');//true
 	$c[]=new cmp('cronico','s','2',$d,$w.' '.$o,'El usuario es cronico','rta',null,null,true,true,'','col-2',"valGluc('glucometria');");
+	}
+	
 	/* $c[]=new cmp('pobladifer','s','3',$d,$w.' '.$o,'Poblacion Direferencial y de Inclusión','pobladifer',null,'',true,true,'','col-25');//true
 	$c[]=new cmp('incluofici','s','3',$d,$w.' '.$o,'Población Inclusion por Oficio','incluofici',null,'',true,true,'','col-25');//true */
 

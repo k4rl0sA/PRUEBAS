f193b8c06cc4c1d63c47d710d0affdadec39c485
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-17 12:55:06 -0500

    10-17 Alertas 12:34

--- crea-fam/signos.php
+++ crea-fam/signos.php
@@ -80,7 +80,7 @@ function cmp_signos(){
 		$c[]=new cmp('frecard','n',3, $d,$w.' '.$o,'Frecuencia Cardiaca Mín=60 - Máx=120','frecard',null,'##',true,true,'','col-2');
 	    $c[]=new cmp('satoxi','n',3, $d,$w.' '.$o,'saturación de Oxigeno Mín=60 - Máx=100','satoxi',null,'##',true,true,'','col-2'); 
         $c[]=new cmp('peri_abdomi','n',4,$d,$w.' AbD '.$o,'Perímetro Abdominal (Cm) Mín=50 - Máx=150','peri_abdomi','rgxperabd','###',$adul,$adul,'','col-3');
-		$c[]=new cmp('glucometria','n',4, $d,$w.' gL '.$o,'Glucometría Mín=5 - Máx=600','glu','','###',false,false,'','col-2',"valGluco('glucometria');");
+		$c[]=new cmp('glucometria','n',4, $d,$w.' gL '.$o,'Glucometría Mín=5 - Máx=600','glu','','###',false,true,'','col-2',"valGluco('glucometria');");
     }
 
     if($meses>= 6 && $meses < 60){

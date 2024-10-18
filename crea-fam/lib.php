662b4a79e39f6191c98c8535fe82dfd085ad8f35
Ferchokar <114622263+Ferchokar@users.noreply.github.com>
2024-10-18 12:43:52 -0500

    fer-10-18fam

--- crea-fam/lib.php
+++ crea-fam/lib.php
@@ -717,7 +717,7 @@ function formato_dato($a,$b,$c,$d){
 		if(plan($c['Cod_Familiar'])===true){
 			$rta.="<li class='icono comp1' title='Compromisos Concertados' id='".$c['ACCIONES']."' Onclick=\"mostrar('compConc','pro',event,'','plncon.php',7);Color('famili-lis');\"></li>";
 		}
-		$rta.="<li class='icono ambi1' title='Ambiental' id='".$c['ACCIONES']."' Onclick=\"mostrar('ambient','pro',event,'','amb.php',7);Color('famili-lis');\"></li>";
+		$rta.="<li title='Ambiental' Onclick=\"mostrar('ambient','pro',event,'','amb.php',7);Color('famili-lis');\"><i class='fa-solid fa-house-fire ico' id='".$c['ACCIONES']."' ></i></li>";
 		$rta.="<li class='icono crear' title='Crear Integrante Familia' id='".$c['ACCIONES']."' Onclick=\"mostrar('person','pro',event,'','lib.php',7,'person');Color('famili-lis');\"></li>";
 
 		/* $rta.="<li class='icono crear' title='Crear Integrante Familia' id='".$c['ACCIONES']."' Onclick=\"mostrar('person','pro',event,'','lib.php',7,'person');Color('famili-lis');\"></li>";

e87f4bbe4e8720625c24b86df75c1c9de302100c
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-23 00:52:53 -0500

    10-23 btnMenu 24:52

--- libs/js/a.js
+++ libs/js/a.js
@@ -506,7 +506,7 @@ function cargarRecursosCSSyFontAwesome() {
     // Cargar el archivo CSS externo (menuCntx.css)
     const cssLink = document.createElement('link');
     cssLink.rel = 'stylesheet';
-    cssLink.href = '../libs/css/menu1.css?v=1.0';
+    cssLink.href = '../libs/css/menu1.css?v=2.0';
     document.head.appendChild(cssLink);
 
     // Cargar FontAwesome

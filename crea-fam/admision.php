7ecc654905fa75713a4f79eaa56ad71483981008
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-24 14:35:59 -0500

    10-24 admision 14:35

--- crea-fam/admision.php
+++ crea-fam/admision.php
@@ -47,7 +47,7 @@ function focus_admision(){
 	// id_factura ACCIONES,
 	$sql="SELECT `cod_admin` 'Codigo Ingreso', FN_CATALOGODESC(126,cod_cups) 'Codigo CUPS', FN_CATALOGODESC(127,final_consul) 'Finalidad de la Consulta' 
 	FROM `adm_facturacion` WHERE idpeople ='{$id[0]}'";
-	// echo $sql;
+	echo $sql;
 	$datos=datos_mysql($sql);
 	return panel_content($datos["responseResult"],"admision-lis",10);
    }

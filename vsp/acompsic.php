d6a813093f04abc78fa0c03cc5bec59c4aa362a7
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-25 13:12:07 -0500

    10-25 vspAcompPsico  13:11

--- vsp/acompsic.php
+++ vsp/acompsic.php
@@ -79,7 +79,7 @@ function cmp_acompsic(){
   $x=false;
   $block=['hab','acc'];
   $event=divide($_POST['id']);
-  $ev=$event[2];
+  $ev=$event[2];//CAMBIAO 3 A 2
   $days=fechas_app('vsp');
 	//este campo no cambiar
   $c[]=new cmp('id_acompsic','h','50',$_POST['id'],$w.' '.$o,'Id de Acompsic','id_acompsic',null,null,false,false,'','col-2');

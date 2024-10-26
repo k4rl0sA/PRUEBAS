c8288c9f17aa78f620527776b7a7e0771111dbe1
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-25 21:03:50 -0500

    10-25 vspViolgest 21:03

--- vsp/cancinfa.php
+++ vsp/cancinfa.php
@@ -46,7 +46,7 @@ function focus_cancinfa(){
 	// var_dump($_POST['id']);
 	$id = isset($_POST['id']) ? divide($_POST['id']) : (isset($_POST['id_cancinfa']) ? divide($_POST['id_cancinfa']) : null);
   $info=datos_mysql("SELECT COUNT(*) total FROM vsp_cancinfa A LEFT JOIN  usuarios U ON A.usu_creo=U.id_usuario 
-  WHERE tipo_doc='".$id[1]."' AND documento='".$id[0]."'");
+  WHERE idpeople='".$id[0]."'");
 	$total=$info['responseResult'][0]['total'];
 	$regxPag=4;
   $pag=(isset($_POST['pag-cancinfa']))? ($_POST['pag-cancinfa']-1)* $regxPag:0;

c58160c433b14a38269432088775be247fdfe9f5
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-26 19:38:50 -0500

    10-26 gestion 19:38

--- libs/gestion.php
+++ libs/gestion.php
@@ -500,7 +500,7 @@ function rol($a){ //a=modulo, b=perfil c=componente
 
 function perfil($a){
 	$perf=rol($a);
-	// var_dump($perf);
+	 var_dump($perf);
 	if (empty($perf['perfil']) || $perf['perfil'] === array()){
 		echo "<H1>ACCESO NO AUTORIZADO,PARA {$a} VALIDA TUS PERMISOS CON EL ADMINISTRADOR DEL SISTEMA</H1><div class='messag rtawarn'></div>";
 		exit();
@@ -516,7 +516,7 @@ function perfil1($a = null) {
 
 function acceso($a){
   $acc=rol($a);
-  // print_r($acc);
+  print_r($acc);
   if (!empty($acc['perfil'])){
     return true;
   }else{

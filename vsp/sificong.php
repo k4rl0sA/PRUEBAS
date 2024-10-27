e6e7db2f5bb37debb9159600a357818dc002ee78
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-26 19:22:31 -0500

    10-26 vspSifCong 19:22

--- vsp/sificong.php
+++ vsp/sificong.php
@@ -104,7 +104,7 @@ function cmp_sificong(){
     $c[]=new cmp('trata_rn','s','2',$d,$w.' sL '.$bl.' '.$o,'Tratamiento del recién nacido','rta',null,null,false,$x,'','col-15');
     $c[]=new cmp('ctrl_serolo','s','3',$d,$w.' sL '.$bl.' '.$o,'Control de Serología','rta',null,null,false,$x,'','col-1',"enabOthSi('ctrl_serolo','Cs');");
     $c[]=new cmp('fecha_controlser','d','10',$d,$w.' Cs '.$bl.' '.$no.' '.$o,'Fecha Control Serológico primer Trimestre','fecha_controlser',null,null,false,$x,'','col-15');
-    $c[]=new cmp('resul_controlser','s','3',$d,$w.' Cs '.$bl.' '.$o,'Resultado Control serología','resul_controlser',null,null,false,$x,'','col-2');
+    $c[]=new cmp('resul_controlser','s','3',$d,$w.' Cs '.$bl.' '.$no.' '.$o,'Resultado Control serología','resul_controlser',null,null,false,$x,'','col-2');
     
     $o='acc';
     $c[]=new cmp($o,'e',null,'INFORMACIÓN ACCIONES',$w);

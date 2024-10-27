4f705c8c1b6f3d1a7bb4adcfc5a53243396d8232
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-10-26 19:56:04 -0500

    10-26 IconoCreaFam 19:56

--- crea-fam/lib.php
+++ crea-fam/lib.php
@@ -795,8 +795,8 @@ function formato_dato($a,$b,$c,$d){
 		$rta.="<li title='Crear Integrante Familia' Onclick=\"mostrar('person','pro',event,'','lib.php',7,'person');Color('famili-lis');\"><i class='fa-solid fa-person-circle-plus ico' id='".$c['Cod_Familiar']."'></i></li>";
 		$rta.="<li title='Mostrar Integrantes' Onclick=\"mostrar('person1','fix',event,'','lib.php',0,'person1');Color('famili-lis');\"><i class='fa-solid fa-people-group ico' id='".$c['Cod_Familiar']."'></i></li>";
 
-		$rta. = (acceso('caract')) ? "<li title='Crear Caracterización Familiar' Onclick=\"mostrar('caract','pro',event,'','../crea-caract/lib.php',7,'caract');Color('famili-lis');\"><i class='fa-solid fa-file-circle-plus ico' id='".$c['Cod_Familiar']."'></i></li>" : "" ;
-	
+		$carac = (acceso('caract')) ? "<li title='Crear Caracterización Familiar' Onclick=\"mostrar('caract','pro',event,'','../crea-caract/lib.php',7,'caract');Color('famili-lis');\"><i class='fa-solid fa-file-circle-plus ico' id='".$c['Cod_Familiar']."'></i></li>" : "" ;
+		$rta.=$carac;
 		//$rta.=(acceso('planDcui')) ? "<li title='Planes de Cuidado Familiar' Onclick=\"mostrar('planDcui','pro',event,'','plancui.php',7);Color('famili-lis');\"><i class='fa-solid fa-file-contract ico'  id='".$c['Cod_Familiar']."'></i></li>" : "" ;
 
 		if(plan($c['Cod_Familiar'])===true){

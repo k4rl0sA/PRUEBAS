e3b69139b6c2bb2bb4766626b1096658f0098a43
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-02 07:55:12 -0500

    11-02 Gestion 07:55

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ session_start();
 //require_once 'gestion.php';
 
 try {
-    require_once '/libs/config.php';
+    require_once __DIR__ . '/libs/config.php';
 } catch (Exception $e) {
     echo "Error en config.php: " . $e->getMessage();
 }
  
 try {
-    require_once '/libs/gestion.php';
+    require_once __DIR__ . '/libs/gestion.php';
 } catch (Exception $e) {
     echo "Error en gestion.php: " . $e->getMessage();
 }
@@ -40,7 +40,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }catch(Exception $e){
         echo "Error en index.php Raiz: " . $e->getMessage();
     }
-    
 }
 
 function login($username, $password) {

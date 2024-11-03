6d986411a7a91a661cd9b900a8ae64ffb4ce3af9
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-03 10:43:08 -0500

    11-03 Index 10:42

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
-require_once 'config.php';
-require_once 'gestion.php';
+/* require_once 'config.php';
+require_once 'gestion.php'; */
+
 if ($mostrar_errores) {
     ini_set('display_errors', '1');
     error_reporting(E_ALL);
 } else {
     ini_set('display_errors', '0');
 }
-session_start();
-
-/* try {
+try {
     require_once __DIR__ . '/libs/config.php';
 } catch (Exception $e) {
     echo "Error en config.php: " . $e->getMessage();
@@ -19,7 +18,8 @@ try {
     require_once __DIR__ . '/libs/gestion.php';
 } catch (Exception $e) {
     echo "Error en gestion.php: " . $e->getMessage();
-} */
+}
+session_start();
 
 // Procesa el formulario cuando se envía por POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {

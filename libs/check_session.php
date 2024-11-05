dcb091acf7e5a25f7d07810b0e1cecf1588e3fd4
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-05 00:25:25 -0500

    11-04 Index 24:25

--- libs/check_session.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_name('us_sds_test_unico');
-session_start();
-echo "Valor en sesión: ";
-var_dump($_SESSION);
-phpinfo();
--- libs/set_session.php
+++ libs/set_session.php
@@ -1,5 +1,13 @@
 <?php
-session_name('us_sds_test_unico');
-session_start();
-echo "Valor en sesión: ";
-var_dump($_SESSION);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/config.php'; // Asegúrate de que el archivo de configuración sea correcto
+
+try {
+    $pdo = new PDO("mysql:host={$dbConfig['s']};dbname={$dbConfig['bd']}", $dbConfig['u'], $dbConfig['p']);
+    echo "Conexión exitosa";
+} catch (PDOException $e) {
+    echo "Error en la conexión: " . $e->getMessage();
+}
+?>

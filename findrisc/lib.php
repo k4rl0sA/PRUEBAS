27470a462628f6ce1e54e46abe2e0d4b46d3b528
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-05 15:30:46 -0500

    11-05 Findrisc 15:30

--- findrisc/lib.php
+++ findrisc/lib.php
@@ -22,21 +22,22 @@ else {
 
 function lis_tamfindrisc(){
 	$info=datos_mysql("SELECT COUNT(*) total from hog_tam_findrisc O 
-	LEFT JOIN personas P ON O.idpersona = P.idpersona
-		LEFT JOIN hog_viv V ON P.vivipersona = V.idviv
+	LEFT JOIN person P ON O.idpeople = P.idpeople
+		LEFT JOIN hog_fam V ON P.vivipersona = V.id_fam
 		LEFT JOIN hog_geo G ON V.idpre = G.idgeo 
 		LEFT JOIN usuarios U ON O.usu_creo=id_usuario
-	 where ".whe_tamfindrisc());
+	 where ".whe_tamfindrisc());//CAMBIO LEFT JOIN Personas y hog_fam antes hog_viv lineas 25-26
 	$total=$info['responseResult'][0]['total'];
 	$regxPag=12;
 	$pag=(isset($_POST['pag-tamfindrisc']))? ($_POST['pag-tamfindrisc']-1)* $regxPag:0;
+
 	$sql="SELECT ROW_NUMBER() OVER (ORDER BY 1) R,concat(O.idpersona,'_',O.tipodoc) ACCIONES,id_findrisc 'Cod registro',O.idpersona Documento,FN_CATALOGODESC(1,O.tipodoc) 'Tipo de Documento',CONCAT_ws(' ',P.nombre1,P.nombre2,P.apellido1,P.apellido2) Nombres,`puntaje` Puntaje,`descripcion` descripcion, U.nombre Creo,U.perfil perfil 
 	FROM hog_tam_findrisc O 
-		LEFT JOIN personas P ON O.idpersona = P.idpersona
-		LEFT JOIN hog_viv V ON P.vivipersona = V.idviv
+		LEFT JOIN person P ON O.idpeople = P.idpeople
+		LEFT JOIN hog_fam V ON P.vivipersona = V.id_fam
 		LEFT JOIN hog_geo G ON V.idpre = G.idgeo 
 		LEFT JOIN usuarios U ON O.usu_creo=id_usuario
-	WHERE ";
+	WHERE ";//CAMBIO LEFT JOIN Personas y hog_fam antes hog_viv lineas 36-37
 	$sql.=whe_tamfindrisc();
 	$sql.=" ORDER BY O.fecha_create DESC";
 	// echo $sql;
@@ -45,31 +46,48 @@ function lis_tamfindrisc(){
 	
 }
 
-function whe_tamfindrisc() {
-	$fefin=date('Y-m-d');
-	$feini=date('Y-m-d',strtotime($fefin.'- 4 days')); 
-	$sql = " G.subred=(SELECT subred FROM usuarios where id_usuario='".$_SESSION['us_sds']."')";
-	if ($_POST['fidentificacion']){
-		$sql .= " AND O.idpersona = '".$_POST['fidentificacion']."'";
-	}else{
-		$sql.=" AND DATE(O.fecha_create) BETWEEN '$feini' and '$fefin'"; 
-	}
-	if($_POST['friesgo'])
-	$sql .= " AND O.descripcion = '".$_POST['friesgo']."'";
-	return $sql;
+function whe_tamfindrisc() {//CAMBIO FILTROS DEJAR ESTOS cambiar todo lo que este dentro de la function
+	$sql = '1';
+    if (!empty($_POST['fidentificacion'])) {
+        $sql .= " AND P.idpersona = '".$_POST['fidentificacion']."'";
+    }
+    if (!empty($_POST['ffam'])) {
+        $sql .= " AND V.id_fam = '".$_POST['ffam']."'";
+    }
+    return $sql;
+}
+
+
+function lis_findrisc(){//CAMBIO INGRESAR ESTA FUNCION ACORDE AL TAMIZAJE todos los campos 
+	// var_dump($_POST['id']);
+	$id=divide($_POST['id']);
+	$sql="SELECT id_findrisc ACCIONES,
+	id_findrisc 'Cod Registro',fecha_toma,descripcion,`nombre` Creó,`fecha_create` 'fecha Creó'
+	FROM hog_tam_findrisc A
+	LEFT JOIN  usuarios U ON A.usu_creo=U.id_usuario ";
+	$sql.="WHERE idpeople='".$id[0];
+	$sql.="' ORDER BY fecha_create";
+	// echo $sql;
+	$datos=datos_mysql($sql);
+	return panel_content($datos["responseResult"],"findrisc-lis",5);
 }
 
 function cmp_tamfindrisc(){
-	$rta="";
-	$t=['id_findrisc'=>'','tipodoc'=>'','idpersona'=>'','nombre'=>'','fechanacimiento'=>'','edad'=>'',
-	'sexo'=>'','diabetes'=>'','peso'=>'','talla'=>'','imc'=>'','perimcint'=>'','actifisica'=>'','verduras'=>'','hipertension'=>'','glicemia'=>'','diabfam'=>'','puntaje'=>'','descripcion'=>'']; 
+	//CAMBIO INICIA ACA AJUSTAR ACORDE AL TAMIZAJE y los campos de la BD
+	$rta="<div class='encabezado findrisc'>TABLA FINDRISC</div><div class='contenido' id='findrisc-lis'>".lis_findrisc()."</div></div>";
+	$a=['id_findrisc'=>'','tipodoc'=>'','idpersona'=>'','nombre'=>'','fechanacimiento'=>'','edad'=>'','sexo'=>'','diabetes'=>'','peso'=>'','talla'=>'','imc'=>'','perimcint'=>'','actifisica'=>'','verduras'=>'','hipertension'=>'','glicemia'=>'','diabfam'=>'','puntaje'=>'','descripcion'=>'']; 
+	$p=['id_findrisc'=>'','idpersona'=>'','tipo_doc'=>'','findrisc_nombre'=>'','findrisc_fechanacimiento'=>'','findrisc_edad'=>'','puntaje'=>'','descripcion'=>'']; //CAMBIO ADD LINEA
 	$w='tamfindrisc';
-	$d=get_tamfindrisc(); 
-	if ($d=="") {$d=$t;}
+	$d=get_tamfindrisc();
+	if (!isset($d['id_findrisc'])) {
+		$d = array_merge($d,$a);
+	}
 	$u = ($d['id_findrisc']!='') ? false : true ;
+	//CAMBIO HASTA AQUI
 	$o='datos';
     $key='find';
-	$c[]=new cmp($o,'e',null,'DATOS DE IDENTIFICACIÓN',$w);
+	$days=fechas_app('vivienda');//CAMBIO DE ADD ESTA LINEA
+	$c[]=new cmp($o,'e',null,'DATOS DE IDENTIFICACIÓN',$w);//CAMBIO DE $d[''] POR '' EN LOS CAMPOS NO PERSONALES
 	$c[]=new cmp('id','h',15,$_POST['id'],$w.' '.$o,'','',null,'####',false,false);
 	$c[]=new cmp('idpersona','t','20',$d['idpersona'],$w.' '.$o.' '.$key,'N° Identificación','idpersona',null,'',false,$u,'','col-3');
 	$c[]=new cmp('tipodoc','s','3',$d['tipodoc'],$w.' '.$o.' '.$key,'Tipo Identificación','tipodoc',null,'',false,$u,'','col-3',"getDatForm('find','person','datos');setTimeout(hiddxedad,500,'edad','prufin');");//setTimeout(hiddxedad,1000,\'edad\',\'find\');
@@ -77,54 +95,94 @@ function cmp_tamfindrisc(){
 	$c[]=new cmp('sexo','s','3',$d['sexo'],$w.' '.$o,'Sexo','sexo',null,'',false,false,'','col-2');
 	$c[]=new cmp('fechanacimiento','d','10',$d['fechanacimiento'],$w.' '.$o,'fecha nacimiento','fechanacimiento',null,'',false,false,'','col-3');
     $c[]=new cmp('edad','n','3',$d['edad'],$w.' '.$o,'edad en Años','edad',null,'',true,false,'','col-2');
-	$c[]=new cmp('diabetes','s',3,$d['diabetes'],$w.' '.$o,'Tiene Diabetes','diabetes',null,null,false,true,'','col-3',"setTimeout(hiddxdiab,500,'diabetes','prufin');");
+	$c[]=new cmp('fecha_toma','d','10','',$w.' '.$o,'fecha de la Toma','fecha_toma',null,'',true,true,'','col-2',"validDate(this,$days,0);"); //CAMBIO SE ADD ESTA LINEA
+	$c[]=new cmp('diabetes','s',3,'',$w.' '.$o,'Tiene Diabetes','diabetes',null,null,false,true,'','col-3',"setTimeout(hiddxdiab,500,'diabetes','prufin');");
 
-	$o='prufin oculto';
+	$ed=false;
+	$o='prufin';
  	$c[]=new cmp($o,'e',null,'PRUEBA FINDRISC',$w);
- 	$c[]=new cmp('peso','t',6,$d['peso'],$w.' '.$o,'Peso (Kg) Mínimo=0.50 (Kg) - Máximo=150.00 (Kg)','peso','rgxpeso','###.##',true,true,'','col-25');
- 	$c[]=new cmp('talla','n',3,$d['talla'],$w.' '.$o,'Talla (Cm) Mínimo=120 (Cm) - Máximo=210 (Cm)','talla','rgxtalla',null,false,true,'','col-25',"calImc('peso','talla','imc');");
-	$c[]=new cmp('imc','t',6, $d['imc'],$w.'IMC','imc',null,null,false,false,'','col-2');
-	$c[]=new cmp('perimcint','n',3,$d['perimcint'],$w.' '.$o,'Perimetro de cintura (Cm) Mínimo=50 (Cm) - Máximo=210 (Cm)','perimcint','rgxperabd',null,false,true,'','col-3');
- 	$c[]=new cmp('actifisica','s',3,$d['actifisica'],$w.' '.$o,'Hace habitualmente (a diario) al menos 30 minutos de actividad física en el trabajo o durante su tiempo libre?','actifisica',null,null,false,true,'','col-5');
- 	$c[]=new cmp('verduras','s',3,$d['verduras'],$w.' '.$o,'Come verduras o frutas Todos los dias ?','verduras',null,null,false,true,'','col-2');
- 	$c[]=new cmp('hipertension','s',3,$d['hipertension'],$w.' '.$o,'Toma regularmente medicación para la hipertensión ?','hipertension',null,null,false,true,'','col-3');
- 	$c[]=new cmp('glicemia','s',3,$d['glicemia'],$w.' '.$o,'Le han encontrado alguna vez valores de glucosa altos ?','glicemia','rgxgluco',null,false,true,'','col-5');
- 	$c[]=new cmp('diabfam','s',3,$d['diabfam'],$w.' '.$o,'Se le ha diagnosticado diabetes (tipo 1 o tipo 2) a alguno de sus familiares ?','diabfam',null,null,false,true,'','col-5');
+ 	$c[]=new cmp('peso','t',6,'',$w.' '.$o,'Peso (Kg) Mínimo=0.50 (Kg) - Máximo=150.00 (Kg)','peso','rgxpeso','###.##',true,true,'','col-25');
+ 	$c[]=new cmp('talla','n',3,'',$w.' '.$o,'Talla (Cm) Mínimo=120 (Cm) - Máximo=210 (Cm)','talla','rgxtalla',null,false,true,'','col-25',"calImc('peso','talla','imc');");
+	$c[]=new cmp('imc','t',6, '',$w.'IMC','imc',null,null,false,false,'','col-2');
+	$c[]=new cmp('perimcint','n',3,'',$w.' '.$o,'Perimetro de cintura (Cm) Mínimo=50 (Cm) - Máximo=210 (Cm)','perimcint','rgxperabd',null,false,true,'','col-3');
+ 	$c[]=new cmp('actifisica','s',3,'',$w.' '.$o,'Hace habitualmente (a diario) al menos 30 minutos de actividad física en el trabajo o durante su tiempo libre?','actifisica',null,null,false,true,'','col-5');
+ 	$c[]=new cmp('verduras','s',3,'',$w.' '.$o,'Come verduras o frutas Todos los dias ?','verduras',null,null,false,true,'','col-2');
+ 	$c[]=new cmp('hipertension','s',3,'',$w.' '.$o,'Toma regularmente medicación para la hipertensión ?','hipertension',null,null,false,true,'','col-3');
+ 	$c[]=new cmp('glicemia','s',3,'',$w.' '.$o,'Le han encontrado alguna vez valores de glucosa altos ?','glicemia','rgxgluco',null,false,true,'','col-5');
+ 	$c[]=new cmp('diabfam','s',3,'',$w.' '.$o,'Se le ha diagnosticado diabetes (tipo 1 o tipo 2) a alguno de sus familiares ?','diabfam',null,null,false,true,'','col-5');
 
 	$o='totalresul';
 	$c[]=new cmp($o,'e',null,'TOTAL',$w);
-	$c[]=new cmp('puntaje','t','2',$d['puntaje'],$w.' '.$o,'Puntaje','puntaje',null,null,false,false,'','col-5');
-	$c[]=new cmp('descripcion','t','3',$d['descripcion'],$w.' '.$o,'Descripcion','descripcion',null,null,false,false,'','col-5');
+	$c[]=new cmp('puntaje','t','2','',$w.' '.$o,'Puntaje','puntaje',null,null,false,false,'','col-5');
+	$c[]=new cmp('descripcion','t','3','',$w.' '.$o,'Descripcion','descripcion',null,null,false,false,'','col-5');
 
 	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
 	
 	return $rta;
    }
 
-   function get_tamfindrisc(){
-	if($_POST['id']==0){
-		return "";
-	}else{
-		 $id=divide($_POST['id']);
+   function get_tamfindrisc(){//CAMBIO FUNCION NUEVA ACORDE AL TAMIZAJE
+	if (empty($_REQUEST['id'])) {
+        return "";
+    }
+	$id = divide($_REQUEST['id']);
 		// print_r($_POST);
-		$sql="SELECT `id_findrisc`,O.`idpersona`,O.`tipodoc`,diabetes,
-		peso,talla,imc,perimcint,actifisica,verduras,hipertension,glicemia,diabfam,puntaje,descripcion,
-        O.estado,P.idpersona,P.tipo_doc,concat_ws(' ',P.nombre1,P.nombre2,P.apellido1,P.apellido2) nombre,sexo,P.fecha_nacimiento fechanacimiento, TIMESTAMPDIFF(YEAR,P.fecha_nacimiento, CURDATE()) edad
+		$sql="SELECT `id_findrisc`, P.idpersona, P.tipo_doc,
+            concat_ws(' ', P.nombre1, P.nombre2, P.apellido1, P.apellido2) findrisc_nombre,
+            P.fecha_nacimiento findrisc_fechanacimiento, 
+            YEAR(CURDATE()) - YEAR(P.fecha_nacimiento) AS findrisc_edad,
+		diabetes,peso,talla,imc,perimcint,actifisica,verduras,hipertension,glicemia,diabfam,puntaje,descripcion
 		FROM `hog_tam_findrisc` O
-		LEFT JOIN personas P ON O.idpersona = P.idpersona and O.tipodoc=P.tipo_doc
-		WHERE O.idpersona ='{$id[0]}' AND O.tipodoc='{$id[1]}'";
+		LEFT JOIN person P ON A.idpeople = P.idpeople
+            WHERE A.id_findrisc='{$id[0]}'";
 		// echo $sql;
-		$info=datos_mysql($sql);
-				return $info['responseResult'][0];
-		}
+		$info = datos_mysql($sql);
+		$data = $info['responseResult'][0];
+		$baseData = [
+			'id_findrisc' => $data['id_findrisc'],
+			'idpersona' => $data['idpersona'],
+			'tipo_doc' => $data['tipo_doc'],
+			'findrisc_nombre' => $data['findrisc_nombre'],
+			'findrisc_fechanacimiento' => $data['findrisc_fechanacimiento'],
+			'findrisc_edad' => $data['findrisc_edad'],
+			'fecha_toma' => $data['fecha_toma']
+		];
+		return json_encode($baseData);
 	} 
+
+	function get_tfindrisc(){//CAMBIO function nueva
+		if($_POST['id']==0){
+			return "";
+		}else{
+			 $id=divide($_POST['id']);
+			// print_r($_POST);
+			$sql="SELECT id_findrisc,O.idpeople,diabetes,peso,talla,imc,perimcint,actifisica,verduras,hipertension,glicemia,diabfam,puntaje,descripcion,
+			O.estado,P.idpersona,P.tipo_doc,concat_ws(' ',P.nombre1,P.nombre2,P.apellido1,P.apellido2) findrisc_nombre,P.fecha_nacimiento findrisc_fechanacimiento,YEAR(CURDATE())-YEAR(P.fecha_nacimiento) findrisc_edad
+			FROM `hog_tam_findrisc` O
+			LEFT JOIN person P ON O.idpeople = P.idpeople
+				WHERE P.idpeople ='{$id[0]}'";
+			// echo $sql;
+			$info=datos_mysql($sql);
+				if (!$info['responseResult']) {
+					$sql="SELECT P.idpersona,P.tipo_doc,concat_ws(' ',P.nombre1,P.nombre2,P.apellido1,P.apellido2) findrisc_nombre,
+					P.fecha_nacimiento findrisc_fechanacimiento,
+					YEAR(CURDATE())-YEAR(P.fecha_nacimiento) findrisc_edad
+					FROM person P 
+					WHERE P.idpeople ='{$id[0]}'";
+					// echo $sql;
+					$info=datos_mysql($sql);
+				return $info['responseResult'][0];
+				}
+			return $info['responseResult'][0];
+		}
+	}
 
 
 function get_person(){
 	// print_r($_POST);
 	$id=divide($_POST['id']);
 	$sql="SELECT idpersona,tipo_doc,concat_ws(' ',nombre1,nombre2,apellido1,apellido2) nombres,sexo ,fecha_nacimiento,TIMESTAMPDIFF(YEAR,fecha_nacimiento, CURDATE()) edad
-from personas
+from person
 WHERE idpersona='".$id[0]."' AND tipo_doc=upper('".$id[1]."');";
 	
 	// return json_encode($sql);
@@ -234,10 +292,16 @@ function gra_tamfindrisc(){
 					$des='Error en el rango, por favor valide';
 					break;
 			}
-
-	if($_POST['id']==0){
+			$id=divide($_POST['id']);
+			if(count($id)!==2){
+				return "No es posible actualizar el tamizaje";
+	}else{
 		$id=$_POST['id'];
-		
+		$idp=datos_mysql("SELECT idpeople FROM person 
+		WHERE idpersona = {$_POST['idpersona']} AND tipo_doc ='{$_POST['tipodoc']}'");//CAMBIO ADD linea
+		if (isset($idp['responseResult'][0])){//CAMBIO ADD linea
+			$idper = $idp['responseResult'][0]['idpeople'];//CAMBIO ADD linea
+		}//CAMBIO ADD linea
 		
 			// echo "ES MENOR DE EDAD ".$ed.' '.print_r($_POST);
 
@@ -251,14 +315,6 @@ function gra_tamfindrisc(){
 		$rta=dato_mysql($sql);
 		// print_r($_POST);
 		// return 'TAMIZAJE NO APLICA PARA LA EDAD';
-	}else{
-		$id=divide($_POST['id']);
-		$sql="UPDATE hog_tam_findrisc SET  
-		diabetes=trim(upper('{$_POST['diabetes']}')),peso=trim(upper('{$_POST['peso']}')),talla=trim(upper('{$_POST['talla']}')),imc=trim(upper('{$_POST['imc']}')),perimcint=trim(upper('{$_POST['perimcint']}')),actifisica=trim(upper('{$_POST['actifisica']}')),verduras=trim(upper('{$_POST['verduras']}')),hipertension=trim(upper('{$_POST['hipertension']}')),glicemia=trim(upper('{$_POST['glicemia']}')),diabfam=trim(upper('{$_POST['diabfam']}')),
-		puntaje=trim(upper('{$suma_findrisc}')),descripcion=trim(upper('{$des}')),
-		usu_update=TRIM(UPPER('{$_SESSION['us_sds']}')),fecha_update=DATE_SUB(NOW(), INTERVAL 5 HOUR)
-		where tipodoc='{$id[0]}' AND idpersona='$id[1]'";
-		$rta=dato_mysql($sql);
 	}
 
   return $rta; 
@@ -300,7 +356,7 @@ function gra_tamfindrisc(){
 	   // var_dump($rta);
 		   if ($a=='tamfindrisc' && $b=='acciones'){
 			$rta="<nav class='menu right'>";		
-				$rta.="<li class='icono editar ' title='Editar' id='".$c['ACCIONES']."' Onclick=\"mostrar('tamfindrisc','pro',event,'','lib.php',7,'tamfindrisc');setTimeout(hiddxedad,1000,'edad','prufin');\"></li>";  //act_lista(f,this);
+				$rta.="<li title='Ver'><i class='fa-solid fa-eye ico' id='".$c['ACCIONES']."' Onclick=\"setTimeout(getDataFetch,500,'tamfindrisc',event,this,'../findrisc/lib.php',['puntaje','descripcion']);\"></i></li>";  //act_lista(f,this);setTimeout(hiddxedad,1000,'edad','prufin');
 			}
 		return $rta;
 	   }

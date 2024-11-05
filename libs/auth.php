ff34f0829a00a220c72350a950102c56e8f9189b
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-05 00:22:01 -0500

    11-04 Index 24:21

--- libs/auth.php
+++ libs/auth.php
@@ -6,50 +6,19 @@ function is_logged_in() {
 
 // Función para iniciar sesión
 function login($username, $password) {
-    global $pdo;
-    
-    echo 'valor de pdo';
-    var_dump($pdo);
-    
-    // Busca al usuario en la base de datos
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username");
-    $stmt->bindParam(':username', $username);
-    $stmt->execute();
+    global $pdo; // Asegúrate de que $pdo esté definido y conectado a la base de datos
+    $sql = "SELECT id_usuario, nombre, clave FROM usuarios WHERE id_usuario = :username AND estado = 'A'";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([':username' => $username]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if (!$user) {
-        echo "Usuario no encontrado.";
-        return false;
-    }
-
-    // Depuración de contraseña ingresada y almacenada
-    var_dump('Contraseña ingresada:', $password);
-    var_dump('Contraseña guardada en BD:', $user['password']);
-
-    if (password_verify($password, $user['password'])) {
-        if (session_status() === PHP_SESSION_NONE) {
-            session_start();
-        }
-        $_SESSION[SESSION_NAME] = strtolower($username);
-        
-        // Verificación inmediata de la variable de sesión
-        var_dump('Estado de sesión justo después de asignación:', $_SESSION);
-        sleep(2); // Pausa de 2 segundos para permitir verificación
-        
-        //session_write_close();
+    if ($user && password_verify($password, $user['clave'])) {
+        $_SESSION['us_sds'] = $user['id_usuario'];
+        $_SESSION['nomb'] = $user['nombre'];
         return true;
     } else {
-        echo "Error: Contraseña incorrecta.";
+        return false;
     }
-
-    // Información adicional de depuración
-    var_dump('Ruta de guardado de sesión:', session_save_path());
-    if (!is_writable(session_save_path())) {
-        echo "Error: No hay permisos de escritura en la ruta de sesión.";
-    }
-    
-    var_dump('Autenticación fallida para usuario: ', $username);
-    return false;
 }
 
 //

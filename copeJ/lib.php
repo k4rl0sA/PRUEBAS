82a1fc0d7a1f44dc1237710d10b357d76a4790a3
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-06 22:11:16 -0500

    11-06 CopeJ 22:11

--- copeJ/lib.php
+++ copeJ/lib.php
@@ -68,7 +68,7 @@ function whe_tamcope() {
 function lis_cope(){
 	// var_dump($_POST['id']);
 	$id=divide($_POST['id']);
-	$sql="SELECT id_apgar ACCIONES,id_apgar 'Cod Registro',fecha_toma,descripcion,`nombre` Creó,`fecha_create` 'fecha Creó'
+	$sql="SELECT tam_cope ACCIONES,tam_cope 'Cod Registro',fecha_toma,descripciona,descripcione,`nombre` Creó,`fecha_create` 'fecha Creó'
 	FROM hog_tam_apgar A
 	LEFT JOIN  usuarios U ON A.usu_creo=U.id_usuario ";
 	$sql.="WHERE idpeople='".$id[0];

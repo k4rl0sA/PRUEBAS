b80a1428e08c0c4dd48fadb9ac543a8ecfc02aff
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-07 12:30:39 -0500

    11-07 CompromisosAjuste 12:30

--- crea-fam/plnsegcon.php
+++ crea-fam/plnsegcon.php
@@ -53,10 +53,10 @@ function cmp_segComp(){
       $c[]=new cmp($o,'e',null,'PLAN DE CUIDADO FAMILIAR CONCERTADO',$w);
         $c[]=new cmp('idp','h',15,$_POST['id'],$w.' '.$key.' '.$o,'id','id',null,'####',false,false);
         $c[]=new cmp('compromiso','a',50,$d['compromiso'],$w.''.$o,'Compromisos concertados','observaciones',null,null,true,false,'','col-0');
-        $c[]=new cmp('fecha','d','3',$e,$w.' '.$o,'Fecha de Seguimiento','fecha',null,null,true,true,'','col-2');
+        $c[]=new cmp('fecha','d','3',$e,$w.' '.$o,'Fecha de Seguimiento','fecha',null,null,true,true,'','col-1');
         $c[]=new cmp('tipo','s','2',$e,$w.' '.$o,'Tipo de Seguimiento','tipo',null,null,false,true,'','col-1');
         $c[]=new cmp('cumplio','s','2',$e,$w.' '.$o,'cumplio','cumplio',null,null,false,true,'','col-1');
-        $c[]=new cmp('observacion','a',50,'',$w.''.$o,'Observación del Incumplimiento','observaciones',null,null,true,false,'','col-0');
+        $c[]=new cmp('observacion','a',50,'',$w.''.$o,'Observación del Incumplimiento','observaciones',null,null,true,false,'','col-7');
 
       for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
       return $rta;

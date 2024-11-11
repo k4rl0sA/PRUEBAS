b1dbb9fa7ca7d93a8f6186c76c1ed910e7908cc2
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-11 10:56:50 -0500

    11-11 Findrisc 10:56

--- findrisc/lib.php
+++ findrisc/lib.php
@@ -313,10 +313,11 @@ function gra_tamfindrisc(){
 				return "No es posible actualizar el tamizaje";
 	}else{
 		$id=$_POST['id'];
-		$idp=datos_mysql("SELECT idpeople FROM person 
-		WHERE idpersona = {$_POST['idpersona']} AND tipo_doc ='{$_POST['tipodoc']}'");//CAMBIO ADD linea
+		$sql="SELECT idpeople FROM person 
+		WHERE idpersona = {$_POST['idpersona']} AND tipo_doc ='{$_POST['tipodoc']}'";
+		$idp=datos_mysql($sql);//CAMBIO ADD linea
 		if (isset($idp['responseResult'][0])){//CAMBIO ADD linea
-			$idper =json_encode($idp['responseResult'][0]['idpeople']);//CAMBIO ADD linea
+			$idper =$idp['responseResult'][0]['idpeople'];//CAMBIO ADD linea
 		}//CAMBIO ADD linea
 		
 			// echo "ES MENOR DE EDAD ".$ed.' '.print_r($_POST);

2146531d828aae35c18a0d77a2dee7b796ee02d9
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-12 18:43:24 -0500

    11-12 omsO 18:43

--- omsO/lib.php
+++ omsO/lib.php
@@ -85,17 +85,17 @@ function cmp_tamoms(){
 	$w='tamoms';
 	// $d=get_tamoms();
 	var_dump($d);
-	/* if (!isset($d['idoms'])) {
+	 if (!isset($d['idoms'])) {
 		$d = array_merge($d,$a);
 	} 
 	$u = ($d['idoms']!='') ? false : true ;
-	*/
+	
 	$o='datos';
     $key='oms';
 	$days=fechas_app('vivienda');//CAMBIO DE ADD ESTA LINEA
 	$c[]=new cmp($o,'e',null,'DATOS DE IDENTIFICACIÓN',$w);
 	$c[]=new cmp('id','h',15,$_POST['id'],$w.' '.$o,'','',null,'####',false,false);
-	/* $c[]=new cmp('idpersona','t','20',$d['idpersona'],$w.' '.$o.' '.$key,'N° Identificación','idpersona',null,'',false,$u,'','col-3');
+	$c[]=new cmp('idpersona','t','20',$d['idpersona'],$w.' '.$o.' '.$key,'N° Identificación','idpersona',null,'',false,$u,'','col-3');
 	$c[]=new cmp('tipodoc','s','3',$d['tipodoc'],$w.' '.$o.' '.$key,'Tipo Identificación','tipodoc',null,'',false,$u,'','col-3',"getDatForm('oms','person','datos');setTimeout(function() {hiddxTamiz('edad', 'pruoms',17);}, 1000);");
 	$c[]=new cmp('nombre','t','50',$d['nombre'],$w.' '.$o,'nombres','nombre',null,'',false,false,'','col-4');
 	$c[]=new cmp('sexo','s','3',$d['sexo'],$w.' '.$o,'Sexo','sexo',null,'',false,false,'','col-2');
@@ -112,7 +112,7 @@ function cmp_tamoms(){
 	$c[]=new cmp($o,'e',null,'TOTAL',$w);
 	$c[]=new cmp('puntaje','t','10',$d['puntaje'],$w.' '.$o,'Puntaje','puntaje',null,null,false,false,'','col-5');
 	$c[]=new cmp('descripcion','t','50',$d['descripcion'],$w.' '.$o,'Descripcion','descripcion',null,null,false,false,'','col-5');
- */
+ 
 	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
 	
 	return $rta;

522965b5f21d7b1ed0e94110b2b560e31edc347a
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-12 12:40:52 -0500

    11-12 addCope 12:40

--- crea-fam/lib.php
+++ crea-fam/lib.php
@@ -739,6 +739,7 @@ function get_Tamiz($fec) {
     }
     if ($edad >= 18) {
         $tamiz[] = 'tamfindrisc';
+		$tamiz[] .= 'tamcope';
     }
     if ($edad >= 40) {
         $tamiz[] = 'tamoms';
@@ -848,6 +849,9 @@ function formato_dato($a,$b,$c,$d){
 		}
 		if (is_array($tamiz) && in_array('tamoms', $tamiz)) {
 			$rta .= acceso('tamoms') ? "<li title='Tamizaje OMS' onclick=\"mostrar('tamoms','pro',event,'','../omsO/lib.php',7);Color('datos-lis');\"><i class=' fa-solid fa-heart-circle-bolt ico' id='".$c['ACCIONES']."'></i></li>": '';
+		}
+		if (is_array($tamiz) && in_array('tamcope', $tamiz)) {
+			$rta .= acceso('tamcope') ? "<li title='Tamizaje COPE' onclick=\"mostrar('tamcope','pro',event,'','../copeJ/lib.php',7);Color('datos-lis');\"><i class=' fa-solid fa-heart-circle-bolt ico' id='".$c['ACCIONES']."'></i></li>": '';
 		}
 		/**********************TAMIZAJES*************************/
 

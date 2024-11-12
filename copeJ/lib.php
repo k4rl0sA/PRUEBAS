6b01472d1971787c262a9a151873e719955bc358
Jcurrea91 <ingsoportesds1@gmail.com>
2024-11-12 17:55:28 -0500

    11-12 CopeJ 17:55

--- copeJ/lib.php
+++ copeJ/lib.php
@@ -77,7 +77,7 @@ function lis_cope(){
 	return panel_content($datos["responseResult"],"cope-lis",5);
 }
 
-function cmp_tamcope(){
+/*function cmp_tamcope(){
 	$rta="<div class='encabezado cope'>TABLA COPE</div>
 	<div class='contenido' id='cope-lis'>".lis_cope()."</div></div>";
 	$a=['tam_cope'=>'','pregunta1'=>'','pregunta2'=>'','pregunta3'=>'','pregunta4'=>'','pregunta5'=>'','pregunta6'=>'','pregunta7'=>'','pregunta8'=>'','pregunta9'=>'','pregunta10'=>'','pregunta11'=>'','pregunta12'=>,'pregunta13'=>'','pregunta14'=>'','pregunta15'=>'','pregunta16'=>'','pregunta16'=>'','pregunta17'=>'','pregunta18'=>'','pregunta19'=>'','pregunta20'=>'','pregunta21'=>'','pregunta22'=>'','pregunta23'=>,'pregunta24'=>'','pregunta25'=>'','pregunta26'=>'','pregunta27'=>'','pregunta28'=>'']; //CAMBIO con relacion a los campos de la bd
@@ -143,7 +143,7 @@ function cmp_tamcope(){
 	$c[]=new cmp('cope_descripcione','t',3,'',$w.' '.$o,'Descripcion Caracterización','cope_descripcione',null,'',false,false,'','col-5');
  
 	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
-	
+	*/
 	return $rta;
    }
 

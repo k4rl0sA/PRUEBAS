c10caa81f2d2011f75bbaa7310e55e8b85502967
Jcurrea91 <ingsoportesds1@gmail.com>
2024-11-12 19:27:01 -0500

    11-12 CopeJ 19:27

--- copeJ/lib.php
+++ copeJ/lib.php
@@ -76,7 +76,7 @@ function lis_cope(){
 	 //echo $sql;
 	$datos=datos_mysql($sql);
 	return panel_content($datos["responseResult"],"cope-lis",5);
-
+}
 function cmp_tamcope(){
 	$rta="<div class='encabezado cope'>TABLA COPE</div><div class='contenido' id='cope-lis'>".lis_cope()."</div></div>";
 	$a=['tam_cope'=>'','pregunta1'=>'','pregunta2'=>'','pregunta3'=>'','pregunta4'=>'','pregunta5'=>'','pregunta6'=>'','pregunta7'=>'','pregunta8'=>'','pregunta9'=>'','pregunta10'=>'','pregunta11'=>'','pregunta12'=>,'pregunta13'=>'','pregunta14'=>'','pregunta15'=>'','pregunta16'=>'','pregunta16'=>'','pregunta17'=>'','pregunta18'=>'','pregunta19'=>'','pregunta20'=>'','pregunta21'=>'','pregunta22'=>'','pregunta23'=>,'pregunta24'=>'','pregunta25'=>'','pregunta26'=>'','pregunta27'=>'','pregunta28'=>'']; //CAMBIO con relacion a los campos de la bd

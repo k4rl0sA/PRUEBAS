da9c47241c16ba335ef8a3c53a9350f4e3676fcf
Jcurrea91 <ingsoportesds1@gmail.com>
2024-11-12 19:35:46 -0500

    11-12 CopeJ 19:36

--- copeJ/lib.php
+++ copeJ/lib.php
@@ -83,7 +83,7 @@ function cmp_tamcope(){
 	$a=['tam_cope'=>'','pregunta1'=>'','pregunta2'=>'','pregunta3'=>'','pregunta4'=>'','pregunta5'=>'','pregunta6'=>'','pregunta7'=>'','pregunta8'=>'','pregunta9'=>'','pregunta10'=>'','pregunta11'=>'','pregunta12'=>,'pregunta13'=>'','pregunta14'=>'','pregunta15'=>'','pregunta16'=>'','pregunta17'=>'','pregunta18'=>'','pregunta19'=>'','pregunta20'=>'','pregunta21'=>'','pregunta22'=>'','pregunta23'=>,'pregunta24'=>'','pregunta25'=>'','pregunta26'=>'','pregunta27'=>'','pregunta28'=>'','puntajea'=>'','descripociona'=>'','puntajee'=>'','descripcione'=>'',]; //CAMBIO con relacion a los campos de la bd
 	$p=['tam_cope'=>'','idpersona'=>'','tipo_doc'=>'','cope_nombre'=>'','cope_fechanacimiento'=>'','cope_edad'=>'','sati_famayu'=>'','puntajea'=>'','descripciona'=>'','puntajee'=>'','descripcione'=>'']; //CAMBIO ADD LINEA
 	$w='tamcope';
- 	$d=get_tapgar();
+ 	/*$d=get_tapgar();
 	var_dump($d);
 	 if (!isset($d['id_apgar'])) {
 		$d = array_merge($d,$a);

e6ddbdd62b955354661cacdece24ecfd3cb104ed
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-14 09:43:45 -0500

    11-14 Cope 9:43

--- cope/lib.php
+++ cope/lib.php
@@ -52,7 +52,7 @@ function lis_tamcope(){
 }
 
 function lis_cope(){
-	/* $id=divide($_POST['id']);
+	$id=divide($_POST['id']);
 	$sql="SELECT tam_cope ACCIONES,
 	tam_cope 'Cod Registro',fecha_toma,descripciona Afrontamiento,descripcione Evitación,`nombre` Creó,`fecha_create` 'fecha Creó'
 	FROM hog_tam_cope A
@@ -61,8 +61,7 @@ function lis_cope(){
 	$sql.="' ORDER BY fecha_create";
 	// echo $sql;
 	$datos=datos_mysql($sql);
-	return panel_content($datos["responseResult"],"cope-lis",5); */
-	return '';
+	return panel_content($datos["responseResult"],"cope-lis",5);
 
 }
 
@@ -87,9 +86,9 @@ function cmp_tamcope(){
 	$a=['tam_cope'=>'','idpeople'=>'','fecha_toma'=>'','reporta'=>'','pregunta1'=>'','pregunta2'=>'','pregunta3'=>'','pregunta4'=>'','pregunta5'=>'','pregunta6'=>'','pregunta7'=>'','pregunta8'=>'','pregunta9'=>'','pregunta10'=>'','pregunta11'=>'','pregunta12'=>'','pregunta13'=>'','pregunta14'=>'','pregunta15'=>'','pregunta16'=>'','pregunta17'=>'','pregunta18'=>'','pregunta19'=>'','pregunta20'=>'','pregunta21'=>'','pregunta22'=>'','pregunta23'=>'','pregunta24'=>'','pregunta25'=>'','pregunta26'=>'','pregunta27'=>'','pregunta28'=>'','puntajea'=>'','descripciona'=>'','puntajee'=>'','descripcione'=>''];//,'nombre'=>'','fechanacimiento'=>'','edad'=>''
 	$p=['tam_cope'=>'','idpersona'=>'','tipo_doc'=>'','nombre'=>'','sexo'=>'','fechanacimiento'=>'','edad'=>''];
 	$w='tamcope';
-	/*$d=get_tcope();
+	$d=get_tcope();
 	var_dump($d);
-	 if (!isset($d['tam_cope'])) {
+	if (!isset($d['tam_cope'])) {
 		$d = array_merge($d,$a);
 	}
 	$o='datos';
@@ -103,7 +102,6 @@ function cmp_tamcope(){
 	$c[]=new cmp('sexo','s','3',$d['sexo'],$w.' '.$o,'Sexo','sexo',null,'',false,false,'','col-2');
 	$c[]=new cmp('fechanacimiento','d','10',$d['fechanacimiento'],$w.' '.$o,'fecha nacimiento','fechanacimiento',null,'',false,false,'','col-3');
     $c[]=new cmp('edad','n','3',$d['edad'],$w.' '.$o,'edad en Años','edad',null,'',true,false,'','col-2');
-	$c[]=new cmp('fecha_toma','d','10','',$w.' '.$o,'fecha de la Toma','fecha_toma',null,'',true,true,'','col-2',"validDate(this,$days,0);");
 
 	$o='info';
 	$u=true;
@@ -144,9 +142,9 @@ function cmp_tamcope(){
 	$c[]=new cmp('cope_puntajee','t',3,'',$w.' '.$o,'Puntaje Caracterización','cope_puntajee',null,'',false,false,'','col-5');
 	$c[]=new cmp('cope_descripcione','t',3,'',$w.' '.$o,'Descripcion Caracterización','cope_descripcione',null,'',false,false,'','col-5');
 
-	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();*/
+	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
 
-	return $rta; 
+	return $rta;
 
    }
 
@@ -207,12 +205,7 @@ left JOIN personas_datocomp ON idpersona=dc_documento and tipo_doc=dc_tipo_doc
 	WHERE idpersona='".$id[0]."' AND tipo_doc=upper('".$id[1]."')";
 	// return print_r(json_encode($sql));
 	$info=datos_mysql($sql);
-	if (!$info['responseRe$id=$_POST['idcope'];
-	//print_r($_POST);
-	if($id != "0"){
-		return "No es posible actualizar el tamizaje";
-	}else{
-sult']) {
+	if (!$info['responseResult']) {
 		return json_encode (new stdClass);
 	}
 return json_encode($info['responseResult'][0]);
@@ -236,12 +229,37 @@ function men_tamcope(){
 	return $rta;
   }
    
-/* function gra_tamcope(){
-	$id=divide($_POST['id']);
-	if(count($id)!==2){
+function gra_tamcope(){
+	$id=$_POST['idcope'];
+	//print_r($_POST);
+	if($id != "0"){
 		return "No es posible actualizar el tamizaje";
 	}else{
-		$suma_afronta = (
+
+	/* $infodata_cope=datos_mysql("SELECT cope_momento,cope_idpersona FROM hog_tam_cope
+		 WHERE cope_idpersona = {$_POST['cope_idpersona']} AND cope_momento = 2 ");
+	if (isset($infodata_cope['responseResult'][0])){
+		return "Ya se realizo los dos momentos";
+	}else{
+		$infodata2_cope=datos_mysql("SELECT cope_momento,cope_idpersona FROM hog_tam_cope
+		 WHERE cope_idpersona = {$_POST['cope_idpersona']} AND cope_momento = 1 ");
+		if (isset($infodata2_cope['responseResult'][0])){
+			$idmomento = 2;
+		}else{
+			$idmomento = 1;
+		} */
+	
+
+	/* $sql3="UPDATE personas_datocomp SET 
+	estadocivil=TRIM(UPPER('{$_POST['cope_estadocivil']}')),
+	nivel_educativo=TRIM(UPPER('{$_POST['cope_escolaridad']}')),
+	ocupacion=TRIM(UPPER('{$_POST['cope_ocupacion']}')),
+	`usu_update`=TRIM(UPPER('{$_SESSION['us_sds']}')),
+	`fecha_update`=DATE_SUB(NOW(), INTERVAL 5 HOUR) 
+	WHERE idpersona ={$_POST['cope_idpersona']} AND tipo_doc={$_POST['cope_tipodoc']}";
+	 $rta3=dato_mysql($sql3); */
+
+	$suma_afronta = (
 		
 		$_POST['cope_pregunta2']+
 		$_POST['cope_pregunta6']+
@@ -310,56 +328,52 @@ function men_tamcope(){
 	}
 	// $suma_cope = ($su	ma_afronta+$suma_comp+$suma_ambi);
 
-	
-	$sql = "INSERT INTO hog_tam_cope VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?, DATE_SUB(NOW(),INTERVAL 5 HOUR),?,?,?)";
-	$params = [
-	['type' => 'i', 'value' => NULL],
-	['type' => 's', 'value' => $id[0]],
-	['type' => 's', 'value' => $_POST['fecha_toma']],
-	['type'=>'s','value'=>$_POST['cope_reporta']],
-	['type'=>'s','value'=>$_POST['cope_pregunta1']],
-	['type'=>'s','value'=>$_POST['cope_pregunta2']],
-	['type'=>'s','value'=>$_POST['cope_pregunta3']],
-	['type'=>'s','value'=>$_POST['cope_pregunta4']],
-	['type'=>'s','value'=>$_POST['cope_pregunta5']],
-	['type'=>'s','value'=>$_POST['cope_pregunta6']],
-	['type'=>'s','value'=>$_POST['cope_pregunta7']],
-	['type'=>'s','value'=>$_POST['cope_pregunta8']],
-	['type'=>'s','value'=>$_POST['cope_pregunta9']],
-	['type'=>'s','value'=>$_POST['cope_pregunta10']],
-	['type'=>'s','value'=>$_POST['cope_pregunta11']],
-	['type'=>'s','value'=>$_POST['cope_pregunta12']],
-	['type'=>'s','value'=>$_POST['cope_pregunta13']],
-	['type'=>'s','value'=>$_POST['cope_pregunta14']],
-	['type'=>'s','value'=>$_POST['cope_pregunta15']],
-	['type'=>'s','value'=>$_POST['cope_pregunta16']],
-	['type'=>'s','value'=>$_POST['cope_pregunta17']],
-	['type'=>'s','value'=>$_POST['cope_pregunta18']],
-	['type'=>'s','value'=>$_POST['cope_pregunta19']],
-	['type'=>'s','value'=>$_POST['cope_pregunta20']],
-	['type'=>'s','value'=>$_POST['cope_pregunta21']],
-	['type'=>'s','value'=>$_POST['cope_pregunta22']],
-	['type'=>'s','value'=>$_POST['cope_pregunta23']],
-	['type'=>'s','value'=>$_POST['cope_pregunta24']],
-	['type'=>'s','value'=>$_POST['cope_pregunta25']],
-	['type'=>'s','value'=>$_POST['cope_pregunta26']],
-	['type'=>'s','value'=>$_POST['cope_pregunta27']],
-	['type'=>'s','value'=>$_POST['cope_pregunta28']],
-	['type' => 's', 'value' => $suma_afronta],
-	['type' => 's', 'value' => $desafr],
-	['type' => 's', 'value' => $suma_evita],
-	['type' => 's', 'value' => $desevit],
-	['type' => 'i', 'value' => $_SESSION['us_sds']],
-	['type' => 's', 'value' => date("Y-m-d H:i:s")],
-	['type' => 's', 'value' => NULL],
-	['type' => 's', 'value' => NULL],
-	['type' => 's', 'value' => 'A']
-	];
-	$rta = mysql_prepd($sql, $params);
-	return $rta;
+		$sql="INSERT INTO hog_tam_cope VALUES (
+			null,
+		TRIM(UPPER('{$_POST['cope_tipodoc']}')),
+		TRIM(UPPER('{$_POST['cope_idpersona']}')),
+		TRIM(UPPER('{$_POST['cope_momento']}')),
+		TRIM(UPPER('{$_POST['cope_reporta']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta1']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta2']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta3']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta4']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta5']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta6']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta7']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta8']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta9']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta10']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta11']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta12']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta13']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta14']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta15']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta16']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta17']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta18']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta19']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta20']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta21']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta22']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta23']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta24']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta25']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta26']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta27']}')),
+		TRIM(UPPER('{$_POST['cope_pregunta28']}')),
+		'{$suma_afronta}',
+		'{$desafr}',
+		'{$suma_evita}',
+		'{$desevit}',
+		TRIM(UPPER('{$_SESSION['us_sds']}')),
+		DATE_SUB(NOW(), INTERVAL 5 HOUR),NULL,NULL,'A')";
+		// echo $sql;
 	}
+	  $rta=dato_mysql($sql);
+	  return $rta; 
 }
- */
+
 
 	function opc_reporta($id=''){
 		return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=120 and estado='A' ORDER BY 1",$id);

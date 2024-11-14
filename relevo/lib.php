73912ecffc6cd87cc90733c8be1a5ca74a2d1b0a
Ferchokar <114622263+Ferchokar@users.noreply.github.com>
2024-11-13 21:09:36 -0500

    20241113_fer_relevo

--- relevo/lib.php
+++ relevo/lib.php
@@ -186,7 +186,7 @@ function get_relevo(){
 		$sql="SELECT P.tipo_doc AS Tipo_Doc,P.idpersona AS Documento,concat_ws(' ',P.nombre1,P.nombre2,P.apellido1,P.apellido2) AS Persona_Cuidadora ,P.fecha_nacimiento,P.sexo,P.genero,P.etnia,P.nacionalidad,P.regimen,P.eapb
 		
 		FROM rel_relevo R 
-		LEFT JOIN personas P ON R.id_people=P.idpeople
+		LEFT JOIN person P ON R.id_people=P.idpeople
 		WHERE R.id_people='{$id[0]}'";
 
 		$info=datos_mysql($sql);

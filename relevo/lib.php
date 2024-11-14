de967bc40e89292b4391feaa9a964407e06fdfcc
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-14 10:51:32 -0500

    11-14 Relev 10:51

--- relevo/lib.php
+++ relevo/lib.php
@@ -145,7 +145,7 @@ function cmp_relevo() {
 	$o='infgen';
 	var_dump($_POST);
 	$c[]=new cmp($o,'e',null,'INFORMACIÓN DE LA PERSONA CUIDADORA',$w);	
-	$c[]=new cmp('idrel','h','20',$_POST['id'] . "_" . $i['idpersona'] ,$w.' '.$o,'','',null,null,false,$u,'','col-1');
+	$c[]=new cmp('idrel','h','20',$_POST['id'],$w.' '.$o,'','',null,null,false,$u,'','col-1');
 	$c[]=new cmp('acep_rbc','s','3',$j['acep_rbc'],$w.' '.$o,'Acepta Participar en la Estrategia RBC','aler',null,null,true,true,'','col-25',"enabDateRel(this,['fre']);");
 	$c[]=new cmp('fecha_acep','d','10',$j['fecha_acep'],$w.' fre '.$o,'Fecha de la Aceptacion','fecha_acep',null,null,false,true,'','col-25','validDate(this,-22)');
 	$c[]=new cmp('persona_cuidadora','t','50',$i['Persona_Cuidadora'],$w.' '.$o,'Nombre Cuidador','persona_cuidadora',null,'',false,false,'','col-5');

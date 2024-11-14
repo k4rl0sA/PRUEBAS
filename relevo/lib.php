da1f92b2aefcf76cbe26cef78a92f5f5139c80d8
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-14 12:08:11 -0500

    11-14 Relev 12:08

--- relevo/lib.php
+++ relevo/lib.php
@@ -167,13 +167,13 @@ function cmp_relevo() {
 	$c[]=new cmp('antecedentes_2','s','3',$j['antecedentes_2'],$w.' cr2 '.$o,'ANTECEDENTES PATOLOGICOS','antecedentes',null,null,false,false,'','col-25',"othePath(this,'ot1');");
 	$c[]=new cmp('otro_2','t','50',$j['otro_2'],$w.' ot1 '.$o,'Otro, Cual','otro_2',null,null,false,false,'','col-2');
 	$c[]=new cmp('whodas2','t','3',$j['whodas2'],$w.' cr2 '.$o,'WHODAS INTERPRETACIÓN - Inicial','whodas2',null,null,false,false,'','col-15');
-	$c[]=new cmp('cert_disca2','s','3',$j['cert_disca2'],$w.' '.$o,'Cuenta con Certificado de Discapacidad','aler',null,null,true,true,'','col-15',"enabMod(this,'mod');");
+	$c[]=new cmp('cert_disca2','s','3',$j['cert_disca2'],$w.' cr2 '.$o,'Cuenta con Certificado de Discapacidad','aler',null,null,true,true,'','col-15',"enabMod(this,'mod');");
 	
 	$c[]=new cmp('cuidado_3','s','18',$j['cuidado_3'],$w.' cr3 care '.$o,'Seleccione Tercer Usuario que requiere cuidado','rel_validacion13',null,null,false,false,'','col-25');
 	$c[]=new cmp('antecedentes_3','s','3',$j['antecedentes_3'],$w.' cr3 '.$o,'ANTECEDENTES PATOLOGICOS','antecedentes',null,null,false,false,'','col-25',"othePath(this,'ot2');");
 	$c[]=new cmp('otro_3','t','50',$j['otro_3'],$w.' ot2 '.$o,'Otro, Cual','otro_3',null,null,false,false,'','col-2');
 	$c[]=new cmp('whodas3','t','3',$j['whodas3'],$w.' cr3 '.$o,'WHODAS INTERPRETACIÓN - Inicial','whodas3',null,null,false,false,'','col-15');
-	$c[]=new cmp('cert_disca3','s','3',$j['cert_disca3'],$w.' '.$o,'Cuenta con Certificado de Discapacidad','aler',null,null,true,true,'','col-15',"enabMod(this,'mod');");
+	$c[]=new cmp('cert_disca3','s','3',$j['cert_disca3'],$w.' cr3 '.$o,'Cuenta con Certificado de Discapacidad','aler',null,null,true,true,'','col-15',"enabMod(this,'mod');");
 
 	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
 	//  $rta .="<div class='encabezado integrantes'>TABLA DE INTEGRANTES DE LA FAMILIA</div><div class='contenido' id='integrantes-lis' >".lis_integrantes1()."</div></div>";

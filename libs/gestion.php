e352cdab5c854c5ca2e06d9378af44947fd494a0
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-14 11:40:30 -0500

    11-14 Relev 11:40

--- libs/gestion.php
+++ libs/gestion.php
@@ -154,6 +154,11 @@ function fechas_app($modu){
       $info=datos_mysql($sql);
       $dias=$info['responseResult'][0]['valor'];
     break;
+    case 'relevo':
+      $sql="SELECT valor FROM `catadeta` WHERE idcatalogo='224' and estado='A' and idcatadeta=3;";
+      $info=datos_mysql($sql);
+      $dias=$info['responseResult'][0]['valor'];
+    break;
     default:
       $dias=-7;
       break;

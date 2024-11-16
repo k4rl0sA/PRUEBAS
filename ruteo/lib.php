c9993eab560bc833b5e41fcc82d96c9f18aeb3f4
Ferchokar <114622263+Ferchokar@users.noreply.github.com>
2024-11-16 13:20:34 -0500

    20241116_fer_ruteo

--- ruteo/lib.php
+++ ruteo/lib.php
@@ -79,9 +79,10 @@ function cmp_rute(){
  $t=['id'=>'','fecha_asig'=>'','fuente'=>'','priorizacion'=>'','tipo_prior'=>'','tipo_doc'=>'','documento'=>'','nombres'=>'','fecha_nac'=>'','sexo'=>'',
  'nacionalidad'=>'','etnia'=>'','regimen'=>'','eapb'=>'','tipo_doc_acu'=>'','documento_acu'=>'','nombres_acu'=>'','direccion'=>'','telefono1'=>'','telefono2'=>'','telefono3'=>'',
  'intervencion'=>'','caracteriza'=>'','per_consul'=>'','fecha_consulta'=>'','cod_cups'=>'',
- 'subred_report'=>'','localidad'=>'','upz'=>'','barrio'=>'','cordx'=>'','cordy'=>'','fecha_gestion'=>'','fecha_gest'=>'','estado_g'=>'',
- 'motivo_estado'=>'','direccion_nueva'=>'','sect'=>'', 'manz'=>'','pred'=>'', 'obse'=>'','dir_new'=>'','sector'=>'', 'manzana'=>'','predio'=>'','usu_creo'=>'', 'fecha_create'=>'', 'usu_update'=>'', 
- 'fecha_update'=>'', 'estado'=>''];
+ 'subred_report'=>'','localidad'=>'','upz'=>'','barrio'=>'','cordx'=>'','cordy'=>'',
+ 'fecha_gestion'=>'','dir_new'=>'','sector'=>'', 'manzana'=>'','predio'=>'','tel_1'=>'','tel_2'=>'','tel_3'=>'',
+ 'fecha_gest'=>'','estado_g'=>'', 'motivo_estado'=>'','direccion_nueva'=>'','sect'=>'', 'manz'=>'','pred'=>'', 'obse'=>'',
+ 'usu_creo'=>'', 'fecha_create'=>'', 'usu_update'=>'','fecha_update'=>'', 'estado'=>''];
  $w='rute';
  $d=get_rute(); 
  if ($d=="") {$d=$t;}

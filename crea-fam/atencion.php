0511337627f98ff46a8c2b38edeee6eea98640be
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-24 05:15:16 -0500

    11-24 Atencion 05:15

--- crea-fam/atencion.php
+++ crea-fam/atencion.php
@@ -29,7 +29,7 @@ function cmp_atencion(){
 	var_dump($d);
 	$x="";
 	if ($d==""){$d=$t;}
-	$key=(!isset($d['id']))?$_POST['id']:$d['id'];
+	$key=($d['id']=='')?$_POST['id']:$d['id'];
 	$u=($d['idpersona']=='')?true:false;
 	$w='atencion';		
 	$o='datos';
@@ -194,7 +194,7 @@ function get_atencion(){
 			LEFT JOIN person b ON a.idpeople=b.idpeople
 			LEFT JOIN eac_atencion c ON a.idpeople=c.idpeople 
 			WHERE c.id_factura ='{$id}' and a.id_factura='{$id}'";
-			echo $sql;
+			// echo $sql;
 			$info=datos_mysql($sql);
 			return json_encode($info['responseResult'][0]);
 			}else{
@@ -211,7 +211,7 @@ function get_atencion(){
 				LEFT JOIN person b ON a.idpeople=b.idpeople 
 				LEFT JOIN eac_atencion c ON a.idpeople=c.idpeople AND a.id_factura=c.id_factura
 				WHERE a.id_factura='{$id}'";
-			echo $sql;
+			// echo $sql;
 			/*  */
 			$info=datos_mysql($sql);
 			return json_encode($info['responseResult'][0]);

95150c3430c1a0e3700ad433ce46fb9f2727f122
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-25 14:38:10 -0500

    11-25 Atencion 14:38

--- crea-fam/atencion.php
+++ crea-fam/atencion.php
@@ -388,7 +388,7 @@ function opc_estrategia($id=''){
 }
 /****************FIN DESPLEGABLES*****************+*****/
 function gra_atencion(){
-	var_dump($_POST);
+	// var_dump($_POST);
 		$id=divide($_POST['ida']);
 	// print_r($_POST['ida']);
 	if(count($id)==1){

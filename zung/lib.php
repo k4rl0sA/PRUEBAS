71ca1d1e8c2521e3a12c728a8551f548680b1338
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-11-29 11:44:12 -0500

    11-29  Zung 11:44

--- zung/lib.php
+++ zung/lib.php
@@ -143,7 +143,7 @@ function cmp_tamzung(){
 		$sql="SELECT P.idpeople,P.idpersona zung_idpersona,P.tipo_doc zung_tipodoc,
         concat_ws(' ',P.nombre1,P.nombre2,P.apellido1,P.apellido2) zung_nombre,P.fecha_nacimiento zung_fechanacimiento,
         TIMESTAMPDIFF(YEAR, P.fecha_nacimiento, CURDATE()) AS zung_edad,
-		(select count(Z.momento) from hog_tam_zung Z  where Z.idpeople='{$id[0]}') as moment 
+		(select count(Z.momento) from hog_tam_zung Z  where Z.idpeople='{$id[0]}') as momento 
 		FROM person P
 		WHERE P.idpeople ='{$id[0]}'";
 		echo $sql; 

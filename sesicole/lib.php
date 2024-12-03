2dab3c676fc82c5d9473f2be51dc393346127d3d
0scarC1 <ingsoportesds2@gmail.com>
2024-12-03 15:21:50 -0500

    12-03  SessColect 15:21

--- sesicole/lib.php
+++ sesicole/lib.php
@@ -24,11 +24,11 @@ function lis_sesigcole(){
 	$total="SELECT COUNT(*) AS total FROM (
 		SELECT G.idgeo AS ACCIONES,G.idgeo AS Cod_Predio,H.direccion,H.sector_catastral Sector,H.nummanzana AS Manzana,H.predio_num AS predio,H.unidad_habit AS 'Unidad',FN_CATALOGODESC(2,H.localidad) AS 'Localidad',U1.nombre,G.fecha_create,FN_CATALOGODESC(44,G.estado_v) AS estado 
 		FROM geo_gest G	LEFT JOIN hog_geo H ON G.idgeo = H.idgeo LEFT JOIN usuarios U ON H.subred = U.subred	LEFT JOIN usuarios U1 ON H.usu_creo = U1.id_usuario
-			WHERE G.estado_v IN ('7') ".whe_homes()." AND U.id_usuario = '{$_SESSION['us_sds']}') AS Subquery";
+			WHERE G.estado_v IN ('7') ".whe_sesigcole()." AND U.id_usuario = '{$_SESSION['us_sds']}') AS Subquery";
 	$info=datos_mysql($total);
 	$total=$info['responseResult'][0]['total']; 
 	$regxPag=5;
-	$pag=(isset($_POST['pag-homes']))? ($_POST['pag-homes']-1)* $regxPag:0;
+	$pag=(isset($_POST['pag-sesigcole']))? ($_POST['pag-sesigcole']-1)* $regxPag:0;
 
 	
 $sql="SELECT G.idgeo AS ACCIONES,
@@ -46,13 +46,13 @@ $sql="SELECT G.idgeo AS ACCIONES,
 	LEFT JOIN hog_geo H ON G.idgeo = H.idgeo
 	LEFT JOIN usuarios U ON H.subred = U.subred
 	LEFT JOIN usuarios U1 ON H.usu_creo = U1.id_usuario 
-WHERE G.estado_v in('7') ".whe_homes()." 
+WHERE G.estado_v in('7') ".whe_sesigcole()." 
 	AND U.id_usuario = '{$_SESSION['us_sds']}'
 	ORDER BY nummanzana, predio_num
 	LIMIT $pag, $regxPag";
 //   echo $sql;
 		$datos=datos_mysql($sql);
-	return create_table($total,$datos["responseResult"],"homes",$regxPag);
+	return create_table($total,$datos["responseResult"],"sesigcole",$regxPag);
 }	
 
 function whe_gestuser() {

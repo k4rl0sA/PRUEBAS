66c0e66e5542caa36b876421d18383aef948bbff
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-03 14:13:51 -0500

    12-03 sesionesColectiv 15:13

---
+++ sesicole/lib.php
@@ -0,0 +1,93 @@
+<?php
+ require_once '../libs/gestion.php';
+ini_set('display_errors','1');
+if (!isset($_SESSION['us_sds'])) die("<script>window.top.location.href='/';</script>");
+else {
+  $rta="";
+  switch ($_POST['a']){
+  case 'csv': 
+    header_csv ($_REQUEST['tb'].'.csv');
+    $rs=array('','');    
+    echo csv($rs,'');
+    die;
+    break;
+  default:
+    eval('$rta='.$_POST['a'].'_'.$_POST['tb'].'();');
+    if (is_array($rta)) json_encode($rta);
+	else echo $rta;
+  }   
+}
+
+
+
+function lis_gestuser(){
+	
+}
+
+function whe_gestuser() {
+
+}
+
+
+function focus_gestuser(){
+ return 'gestuser';
+}
+
+function men_gestuser(){
+ $rta=cap_menus('gestuser','pro');
+ return $rta;
+} 
+
+
+function cap_menus($a,$b='cap',$con='con') {
+  $rta = ""; 
+  if ($a=='gestuser'){  
+	$rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
+  	$rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";
+  }
+  return $rta;
+}
+
+
+function cmp_gestuser(){
+	$rta="";
+	$hoy=date('Y-m-d');
+	$t=['gestion'=>'','perfil'=>'','usuario'=>''];
+	$d='';
+	if ($d==""){$d=$t;}
+	$w='adm_usuarios';
+	$o='creusu';
+	$c[]=new cmp($o,'e',null,'GESTIÓN DE USUARIOS',$w);
+	
+	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
+	return $rta;
+}
+
+function get_gestuser(){
+	
+}
+
+function gra_gestuser(){
+
+	return $rta;
+}
+
+
+function formato_dato($a,$b,$c,$d){
+ $b=strtolower($b);
+ $rta=$c[$d];
+// $rta=iconv('UTF-8','ISO-8859-1',$rta);
+// var_dump($rta);
+	if ($a=='gestuser' && $b=='acciones'){//a mnombre del modulo
+		$rta="<nav class='menu right'>";		
+		$rta.="<li class='icono asigna1' title='Asignar Usuario' id='".$c['ACCIONES']."' Onclick=\"mostrar('gestuser','pro',event,'','lib.php',7);\"></li>";
+	}
+	
+ return $rta;
+}
+
+function bgcolor($a,$c,$f='c'){
+ $rta="";
+ return $rta;
+}
+?>

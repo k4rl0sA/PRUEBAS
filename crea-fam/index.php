bf79d24b4910f4b72d64e413763db9ff57df9852
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-03 16:21:46 -0500

    12-03 AlertasAjustar 16:21

--- crea-fam/index.php
+++ crea-fam/index.php
@@ -41,7 +41,7 @@ function actualizar(){
 function fixRecord(a='') {
 	const fields= document.getElementById('fapp').querySelectorAll('select:not(.nFx), input:not(.nFx), textarea:not(.nFx)');
 
-	fiels.reset();
+	fields.reset();
 	var c = document.getElementById(a+'-pro-con');
 	var cmp=c.querySelectorAll('.captura,.bloqueo');
 	for (i=0;i<cmp.length;i++) {

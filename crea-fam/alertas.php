1e52ffdfa2bf85c70121c4dce7545820c6fcc411
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-04 11:05:37 -0500

    12-04 AlertasAjustar 11:05

--- crea-fam/alertas.php
+++ crea-fam/alertas.php
@@ -283,7 +283,7 @@ function get_alertas(){
 		concat_ws(' ',nombre1,nombre2,apellido1,apellido2) nombres,FN_CATALOGODESC(21,sexo) sexo,fecha_nacimiento,
 		'Años: '.$p['ano'].' Meses: '.$p['mes'].' Dias:'.$p['dia'];
 		CONCAT('Años: ',TIMESTAMPDIFF(YEAR,fecha_nacimiento, CURDATE() ),' Meses: ',TIMESTAMPDIFF(MONTH,fecha_nacimiento ,CURDATE() ) % 12 ,
-		' Dias: ',DATEDIFF(CURDATE(), DATE_ADD(fecha_nacimiento,INTERVAL TIMESTAMPDIFF(MONTH, fecha_nacimiento, CURDATE()) MONTH)),
+		' Dias: ',DATEDIFF(CURDATE(), DATE_ADD(fecha_nacimiento,INTERVAL TIMESTAMPDIFF(MONTH, fecha_nacimiento, CURDATE()) MONTH))),
 		cursovida,D.fecha, tipo,D.crit_epi, men_dnt, men_sinctrl, gestante, etapgest, ges_sinctrl, cronico, cro_hiper, cro_diabe, cro_epoc, cro_sinctrl, esq_vacun, alert1, selmul1, alert2, selmul2, alert3, selmul3, alert4, selmul4, alert5, selmul5, alert6, selmul6, agen_intra, servicio, fecha_cita, hora_cita, lugar_cita, deriva_pf, evento_pf";
 		$sql.=" FROM hog_alert D
 				LEFT JOIN person P ON D.idpeople=P.idpeople

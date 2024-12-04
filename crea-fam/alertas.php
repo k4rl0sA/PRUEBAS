ccd3ce375e5fee5fe8c6fc8f0c600fe17e1e2d07
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-04 15:35:37 -0500

    12-04 AlertasAjustar 15:35

--- crea-fam/alertas.php
+++ crea-fam/alertas.php
@@ -38,7 +38,7 @@ function men_alertas(){
 	 	$rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>"; //~ openModal();
 	 }
 	 if ($a=='alertas'  && isset($acc['ajustar']) && $acc['ajustar']=='SI'){
-		$id = intval($_POST['id']);
+		$id = $_POST['id'];
 		$rta .= "<li class='icono $a basura'      title='Ajustar'          OnClick=\"fixRecord('$a',$id);\"></li>"; //~ openModal();
 	}
 	 return $rta;

6cb424cfccda9f876b9442ffab44dc479839b10a
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-04 10:51:12 -0500

    12-04 AlertasAjustar 10:51

--- crea-fam/alertas.php
+++ crea-fam/alertas.php
@@ -269,7 +269,9 @@ function get_alertas(){
 	}else{
 		$id=divide($_POST['id']);
 		// print_r($id);
-		$sql1="SELECT TIMESTAMPDIFF(YEAR,fecha_nacimiento, fecha ) AS ano,TIMESTAMPDIFF(MONTH,fecha_nacimiento ,fecha ) % 12 AS mes 
+		$sql1="SELECT TIMESTAMPDIFF(YEAR,fecha_nacimiento, CURDATE() ) AS ano,
+  		TIMESTAMPDIFF(MONTH,fecha_nacimiento ,CURDATE() ) % 12 AS mes,
+		DATEDIFF(CURDATE(), DATE_ADD(fecha_nacimiento,INTERVAL TIMESTAMPDIFF(MONTH, fecha_nacimiento, CURDATE()) MONTH)) AS dia
 		from person P 
 		left join hog_alert D ON P.idpeople=D.idpeople WHERE id_alert='{$id[0]}'";
 		$data=datos_mysql($sql1);
@@ -279,7 +281,9 @@ function get_alertas(){
 
 		$sql="SELECT  concat_ws('_',D.idpeople,D.tipo) as id,P.idpersona,P.tipo_doc,
 		concat_ws(' ',nombre1,nombre2,apellido1,apellido2) nombres,FN_CATALOGODESC(21,sexo) sexo,fecha_nacimiento,
-		FN_EDAD(fecha_nacimiento,V.fecha),
+		TIMESTAMPDIFF(YEAR,fecha_nacimiento, CURDATE() ) AS ano,
+  		TIMESTAMPDIFF(MONTH,fecha_nacimiento ,CURDATE() ) % 12 AS mes,
+		DATEDIFF(CURDATE(), DATE_ADD(fecha_nacimiento,INTERVAL TIMESTAMPDIFF(MONTH, fecha_nacimiento, CURDATE()) MONTH)) AS dia,
 		cursovida,D.fecha, tipo,D.crit_epi, men_dnt, men_sinctrl, gestante, etapgest, ges_sinctrl, cronico, cro_hiper, cro_diabe, cro_epoc, cro_sinctrl, esq_vacun, alert1, selmul1, alert2, selmul2, alert3, selmul3, alert4, selmul4, alert5, selmul5, alert6, selmul6, agen_intra, servicio, fecha_cita, hora_cita, lugar_cita, deriva_pf, evento_pf";
 		$sql.=" FROM hog_alert D
 				LEFT JOIN person P ON D.idpeople=P.idpeople

13c673a9f9df68c860208731c642fb830ec9bb28
Ferchokar <114622263+Ferchokar@users.noreply.github.com>
2024-12-05 08:50:19 -0500

    20241205_fer_violreite

--- vsp/violreite.php
+++ vsp/violreite.php
@@ -46,7 +46,7 @@ function focus_violreite(){
 	// var_dump($_POST['id']);
 	$id = isset($_POST['id']) ? divide($_POST['id']) : (isset($_POST['id_violreite']) ? divide($_POST['id_violreite']) : null);
   $info=datos_mysql("SELECT COUNT(*) total FROM vsp_violreite A LEFT JOIN  usuarios U ON A.usu_creo=U.id_usuario 
-  WHERE idpeople='".$id[0]."'");
+  WHERE A.estado = 'A' AND A.idpeople='".$id[0]."'");
 	$total=$info['responseResult'][0]['total'];
 	$regxPag=4;
   $pag=(isset($_POST['pag-violreite']))? ($_POST['pag-violreite']-1)* $regxPag:0;
@@ -58,7 +58,7 @@ fecha_cierre 'Fecha de Cierre',nombre Creó
 FROM vsp_violreite A
 LEFT JOIN   person P ON A.idpeople=P.idpeople
 	LEFT JOIN  usuarios U ON A.usu_creo=U.id_usuario ";
-$sql.=" WHERE A.idpeople='".$id[0];
+$sql.=" WHERE A.estado = 'A' AND A.idpeople='".$id[0];
 $sql.="' ORDER BY A.fecha_create"; 
 	$sql.=' LIMIT '.$pag.','.$regxPag;
 	// echo $sql;

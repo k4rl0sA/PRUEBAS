c81f282948d1b9e38bd122b879e64bf3b394c503
0scarC1 <ingsoportesds2@gmail.com>
2024-12-06 13:32:33 -0500

    12-06  SessColect 13:32

--- sesicole/lib.php
+++ sesicole/lib.php
@@ -98,7 +98,7 @@ function cmp_sesigcole(){
 		
 	$o='alert';
 	$c[]=new cmp($o,'e',null,'ALERTAS',$w); 
-	$c[]=new cmp('alert1','s',15,$d,$w.' '.$o,'Alerta N° 1','alert',null,null,true,true,'','col-1');
+	$c[]=new cmp('alert1','s',15,$d,$w.' '.$o,'Alerta N° 1','rta',null,null,true,true,'','col-1');
 	
 	$c[]=new cmp('agen_intra','s',15,$d,$w.' '.$o,'Agendamiento Intramural','rta',null,null,true,true,'','col-1',"fieldsValue('agen_intra','aIM','1',true);");
 	$c[]=new cmp('servicio','t',15,$d,$w.' aIM '.$o,'Servicio Agendado','servicio',null,null,false,false,'','col-15');

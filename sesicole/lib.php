ab93344779ca994c87720dda19496fa9b0416102
0scarC1 <ingsoportesds2@gmail.com>
2024-12-06 12:21:09 -0500

    12-06  SessColect 12:21

--- sesicole/lib.php
+++ sesicole/lib.php
@@ -100,6 +100,7 @@ function cmp_sesigcole(){
 	$c[]=new cmp('abo2','s','3',$d,$w.' '.$o,'¿Cual tematica 2?','tipose',null,null,true,true,'','col-2');
 	$c[]=new cmp('tem3','o',2,$d,$w.' '.$o,'Tematica Abordada 3','sp1',null,null,false,true,'','col-1');
 	$c[]=new cmp('abo3','s','3',$d,$w.' '.$o,'¿Cual tematica 3?','tipose',null,null,true,true,'','col-2');
+	$c[]=new cmp('seg_pre3','o',2,$d,$w.' '.$o,'¿Hubo alguna vez en que usted haya comido poca variedad de alimentos por falta de dinero u otros recursos?','sp3',null,null,false,true,'','col-10');
 
 
 

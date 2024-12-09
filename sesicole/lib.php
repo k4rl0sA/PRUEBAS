144f4e3e5c75b235acfa81125a6061fddde896b6
0scarC1 <ingsoportesds2@gmail.com>
2024-12-09 09:37:31 -0500

    12-09  SessColect 09:29

--- sesicole/lib.php
+++ sesicole/lib.php
@@ -100,8 +100,8 @@ function cmp_sesigcole(){
 	$c[]=new cmp('fecha_int','d','10',$d['fecha_int'],$w.' '.$o,'fecha_Intervencion','fecha_int',null,null,true,true,'','col-15',"validDate(this,$days,0);"); 
 	$c[]=new cmp('activi','s','15',$d['activi'],$w.' '.$o,'Tipo de Actividad','fm1',null,null,false,true,'','col-25');
 	$c[]=new cmp('luga','t','15',$d['luga'],$w.' '.$o,'Lugar','rta',null,null,true,true,'','col-6',"fieldsValue('agen_intra','aIM','1',true);");
-	$c[]=new cmp('accion1','s','3',$d['accion1'],$w.' '.$o,'Accion 1','accion1',null,null,true,true,'','col-5',"selectDepend('accion1','desc_accion1','../crea-fam/plancui.php');");
-	$c[]=new cmp('desc_accion1','s','3',$d['desc_accion1'],$w.' '.$o,'Descripcion Accion 1','desc_accion1',null,null,true,true,'','col-5');
+	$c[]=new cmp('temati1','s','3',$d['temati1'],$w.' '.$o,'tematica 1','temati1',null,null,true,true,'','col-5',"selectDepend('accion1','desc_accion1','../crea-fam/plancui.php');");
+	$c[]=new cmp('desc_temati1','s','3',$d['desc_temati1'],$w.' '.$o,'Descripcion tematica 1','desc_temati1',null,null,true,true,'','col-5');
     $c[]=new cmp('accion2','s','3',$d['accion2'],$w.' '.$o,'Accion 2','accion2',null,null,false,true,'','col-5',"selectDepend('accion2','desc_accion2','../crea-fam/plancui.php');");
     $c[]=new cmp('desc_accion2','s','3',$d['desc_accion2'],$w.' '.$o,'Descripcion Accion 2','desc_accion2',null,null,false,true,'','col-5');
     $c[]=new cmp('accion3','s','3',$d['accion3'],$w.' '.$o,'Accion 3','accion3',null,null,false,true,'','col-5',"selectDepend('accion3','desc_accion3','../crea-fam/plancui.php');");

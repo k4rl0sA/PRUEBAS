92ba8e2e3bc9673f5f871c4654ea20d1adaac1ca
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-09 12:40:37 -0500

    12-09 SesionesPersonas 12:40

--- sesicole/index.php
+++ sesicole/index.php
@@ -8,6 +8,7 @@ include $_SERVER['DOCUMENT_ROOT'].'/libs/nav.php';
 <title>Sesiones || GTAPS</title>
 <link href="../libs/css/stylePop.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Cabin+Sketch&family=Chicle&family=Merienda&family=Rancho&family=Boogaloo&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 <script src="../libs/js/a.js"></script>
 <script src="../libs/js/x.js"></script>
 <script src="../libs/js/d.js"></script>

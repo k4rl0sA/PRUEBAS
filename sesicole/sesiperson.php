cbe19849227febfa8165d11418c5159cd7cad859
0scarC1 <ingsoportesds2@gmail.com>
2024-12-09 15:07:40 -0500

    12-09  Sesiperson 15:07

--- sesicole/sesiperson.php
+++ sesicole/sesiperson.php
@@ -54,13 +54,13 @@ function cmp_sespers(){
 	$c[]=new cmp($o,'e',null,'IDENTIFICACIÓN DE PERSONAS',$w);
 	$c[]=new cmp('idp','h',15,$_POST['id'],$w.' '.$o,'id','id',null,'####',false,false);
 	$c[]=new cmp('idpersona','n','18','',$w.' '.$key.' '.$o,'Identificación <a href="https://www.adres.gov.co/consulte-su-eps" target="_blank">     Abrir ADRES</a>','idpersona',null,null,true,$edit,'','col-4');
-	$c[]=new cmp('tipo_doc','s','3','',$w.' '.$key.' '.$o,'Tipo documento','tipo_doc',null,null,true,$edit,'','col-4',"getDatForm('pEr','personOld',['infgen'],this);");
+	$c[]=new cmp('tipo_doc','s','3','',$w.' '.$key.' '.$o,'Tipo documento','tipo_doc',null,null,true,true,'','col-4',"getDatForm('pEr','personOld',['infgen'],this);");
 	$c[]=new cmp('nombre1','t','30','',$w.' '.$o,'Primer Nombre','nombre1',null,null,true,true,'','col-2');
 	$c[]=new cmp('nombre2','t','30','',$w.' '.$o,'Segundo Nombre','nombre2',null,null,false,true,'','col-2');
 	$c[]=new cmp('apellido1','t','30','',$w.' '.$o,'Primer Apellido','apellido1',null,null,true,true,'','col-2');
 	$c[]=new cmp('apellido2','t','30','',$w.' '.$o,'Segundo Apellido','apellido2',null,null,false,true,'','col-2');
-	$c[]=new cmp('fecha_nacimiento','d','','',$w.' '.$o,'Fecha de nacimiento','fecha_nacimiento',null,null,true,$edit,'','col-2',"validDate(this,-43800,0);",[],"child14('fecha_nacimiento','osx');Ocup5('fecha_nacimiento','OcU');");
-	$c[]=new cmp('sexo','s','3','',$w.' '.$o,'Sexo','sexo',null,null,true,$edit,'','col-2');
+	$c[]=new cmp('fecha_nacimiento','d','','',$w.' '.$o,'Fecha de nacimiento','fecha_nacimiento',null,null,true,true,'','col-2',"validDate(this,-43800,0);",[],"child14('fecha_nacimiento','osx');Ocup5('fecha_nacimiento','OcU');");
+	$c[]=new cmp('sexo','s','3','',$w.' '.$o,'Sexo','sexo',null,null,true,true,'','col-2');
 	$c[]=new cmp('genero','s','3','',$w.' '.$o,'Genero','genero',null,null,true,true,'','col-2');
 	$c[]=new cmp('etnia','s','3','',$w.' '.$o,'Pertenencia Etnica','etnia',null,null,true,true,'','col-2',"enabEtni('etnia','ETn','idi');");
 	$c[]=new cmp('pueblo','s','50','',$w.' ETn cmhi '.$o,'pueblo','pueblo',null,null,false,true,'','col-2');

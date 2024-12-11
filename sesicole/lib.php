58c6077ba9f04afe90580e6264b523211cb08f8c
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-11 14:00:59 -0500

    12-11 SesionColect 14:00

--- sesicole/lib.php
+++ sesicole/lib.php
@@ -100,21 +100,21 @@ function cmp_sesigcole(){
 	$c[]=new cmp('fecha_int','d','10',$d['fecha_int'],$w.' '.$o,'fecha_Intervencion','fecha_int',null,null,true,true,'','col-15',"validDate(this,$days,0);"); 
 	$c[]=new cmp('activi','s','15',$d['activi'],$w.' '.$o,'Tipo de Actividad','fm1',null,null,false,true,'','col-25');
 	$c[]=new cmp('luga','t','15',$d['luga'],$w.' '.$o,'Lugar','rta',null,null,true,true,'','col-6',"fieldsValue('agen_intra','aIM','1',true);");
-	$c[]=new cmp('temati1','s','3',$d['temati1'],$w.' '.$o,'tematica 1','temati1',null,null,true,true,'','col-15',"selectDepend('accion1','desc_accion1','../crea-fam/plancui.php');");
+	$c[]=new cmp('temati1','s','3',$d['temati1'],$w.' '.$o,'tematica 1','temati1',null,null,true,true,'','col-15',"selectDepend('temati1','desc_temati1');");
 	$c[]=new cmp('desc_temati1','s','3',$d['desc_temati1'],$w.' '.$o,'Descripcion tematica 1','desc_temati1',null,null,true,true,'','col-35');
-    $c[]=new cmp('temati2','s','3',$d['temati2'],$w.' '.$o,'tematica 2','temati2',null,null,false,true,'','col-15',"selectDepend('accion2','desc_accion2','../crea-fam/plancui.php');");
+    $c[]=new cmp('temati2','s','3',$d['temati2'],$w.' '.$o,'tematica 2','temati2',null,null,false,true,'','col-15',"selectDepend('temati2','desc_temati2');");
     $c[]=new cmp('desc_temati2','s','3',$d['desc_temati2'],$w.' '.$o,'Descripcion tematica 2','desc_temati2',null,null,false,true,'','col-35');
-    $c[]=new cmp('temati3','s','3',$d['temati3'],$w.' '.$o,'tematica 3','temati3',null,null,false,true,'','col-15',"selectDepend('accion3','desc_accion3','../crea-fam/plancui.php');");
+    $c[]=new cmp('temati3','s','3',$d['temati3'],$w.' '.$o,'tematica 3','temati3',null,null,false,true,'','col-15',"selectDepend('temati3','desc_temati3');");
     $c[]=new cmp('desc_temati3','s','3',$d['desc_temati3'],$w.' '.$o,'Descripcion tematica 3','desc_temati3',null,null,false,true,'','col-35');
-    $c[]=new cmp('temati4','s','3',$d['temati4'],$w.' '.$o,'tematica 4','temati4',null,null,false,true,'','col-15',"selectDepend('accion4','desc_accion4','../crea-fam/plancui.php');");
+    $c[]=new cmp('temati4','s','3',$d['temati4'],$w.' '.$o,'tematica 4','temati4',null,null,false,true,'','col-15',"selectDepend('temati4','desc_temati4');");
     $c[]=new cmp('desc_temati4','s','3',$d['desc_temati4'],$w.' '.$o,'Descripcion tematica 4','desc_temati4',null,null,false,true,'','col-35');
-	$c[]=new cmp('temati5','s','3',$d['temati5'],$w.' '.$o,'tematica 5','temati5',null,null,false,true,'','col-15',"selectDepend('accion4','desc_accion4','../crea-fam/plancui.php');");
+	$c[]=new cmp('temati5','s','3',$d['temati5'],$w.' '.$o,'tematica 5','temati5',null,null,false,true,'','col-15',"selectDepend('temati5','desc_temati5');");
     $c[]=new cmp('desc_temati5','s','3',$d['desc_temati5'],$w.' '.$o,'Descripcion tematica 5','desc_temati5',null,null,false,true,'','col-35');
-	$c[]=new cmp('temati6','s','3',$d['temati6'],$w.' '.$o,'tematica 6','temati6',null,null,false,true,'','col-15',"selectDepend('accion4','desc_accion4','../crea-fam/plancui.php');");
+	$c[]=new cmp('temati6','s','3',$d['temati6'],$w.' '.$o,'tematica 6','temati6',null,null,false,true,'','col-15',"selectDepend('temati6','desc_temati6');");
     $c[]=new cmp('desc_temati6','s','3',$d['desc_temati6'],$w.' '.$o,'Descripcion tematica 6','desc_temati6',null,null,false,true,'','col-35');
-	$c[]=new cmp('temati7','s','3',$d['temati7'],$w.' '.$o,'tematica 7','temati7',null,null,false,true,'','col-15',"selectDepend('accion4','desc_accion4','../crea-fam/plancui.php');");
+	$c[]=new cmp('temati7','s','3',$d['temati7'],$w.' '.$o,'tematica 7','temati7',null,null,false,true,'','col-15',"selectDepend('temati7','desc_temati7');");
     $c[]=new cmp('desc_temati7','s','3',$d['desc_temati7'],$w.' '.$o,'Descripcion tematica 7','desc_temati7',null,null,false,true,'','col-35');
-	$c[]=new cmp('temati8','s','3',$d['temati8'],$w.' '.$o,'tematica 8','temati8',null,null,false,true,'','col-15',"selectDepend('accion4','desc_accion4','../crea-fam/plancui.php');");
+	$c[]=new cmp('temati8','s','3',$d['temati8'],$w.' '.$o,'tematica 8','temati8',null,null,false,true,'','col-15',"selectDepend('temati8','desc_temati8');");
     $c[]=new cmp('desc_temati8','s','3',$d['desc_temati8'],$w.' '.$o,'Descripcion tematica 8','desc_temati8',null,null,false,true,'','col-35');
 
 

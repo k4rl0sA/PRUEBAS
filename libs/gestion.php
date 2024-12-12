604a97ed5cbaa378587cc8a77906d845ab03d6a1
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-12 11:40:26 -0500

    12-12 SesionColect 11:40

--- libs/gestion.php
+++ libs/gestion.php
@@ -10,7 +10,7 @@ setlocale(LC_TIME, 'es_CO');
 ini_set('memory_limit','1024M');
 date_default_timezone_set('America/Bogota');
 setlocale(LC_ALL,'es_CO');
-$GLOBALS['APP']='GTAPS';
+$APP='GTAPS';
 if (!isset($_SESSION["us_sds"])) {
   header("Location: /index.php"); 
   exit;

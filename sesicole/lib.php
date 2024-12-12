08859042472f8ad19b81d8316e8deac3aa146c0c
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-12 11:10:48 -0500

    12-12 SesionColect 11:10

--- sesicole/lib.php
+++ sesicole/lib.php
@@ -23,15 +23,16 @@ else {
 function lis_sesigcole(){
 	if (!empty($_POST['fpred'])) {
 	$total="SELECT COUNT(*) AS total FROM (
-		SELECT G.idgeo AS ACCIONES,G.idgeo AS Cod_Predio,H.direccion,H.sector_catastral Sector,H.nummanzana AS Manzana,H.predio_num AS predio,H.unidad_habit AS 'Unidad',FN_CATALOGODESC(2,H.localidad) AS 'Localidad', H.upz AS PRUEBA ,U1.nombre,G.fecha_create,FN_CATALOGODESC(44,G.estado_v) AS estado 
-		FROM geo_gest G	LEFT JOIN hog_geo H ON G.idgeo = H.idgeo LEFT JOIN usuarios U ON H.subred = U.subred	LEFT JOIN usuarios U1 ON H.usu_creo = U1.id_usuario
-			WHERE G.estado_v IN ('7') ".whe_sesigcole()." AND U.id_usuario = '{$_SESSION['us_sds']}') AS Subquery";
-			// echo $total;
+		SELECT sc.id_cole AS 'ACCIONES',sc.fecha,FN_CATALOGODESC(239, sc.tipo_activ),sc.lugar,FN_CATALOGODESC(237, sc.tematica1),FN_CATALOGODESC(238, sc.des_temati1),u.nombre AS Creo,
+		sc.fecha_create AS Creado,sc.estado FROM hog_sescole sc LEFT JOIN usuarios u ON sc.usu_create = u.id_usuario LEFT JOIN geo_gest gg ON sc.idpre = gg.idgeo LEFT JOIN hog_geo hg ON gg.idgeo = hg.idgeo 
+		WHERE gg.estado_v IN ('7') AND hg.subred = (SELECT subred FROM usuarios WHERE id_usuario ='{$_SESSION['us_sds']}') ".whe_sesigcole()." AND U.id_usuario = '{$_SESSION['us_sds']}') AS Subquery";
+			echo $total;
 	$info=datos_mysql($total);
 	$total=$info['responseResult'][0]['total'];
 	$regxPag=5;
 	$pag=(isset($_POST['pag-sesigcole']))? ($_POST['pag-sesigcole']-1)* $regxPag:0;
- $sql="SELECT 
+
+ /* $sql="SELECT 
    sc.id_cole AS 'ACCIONES',
     sc.fecha,
     FN_CATALOGODESC(239, sc.tipo_activ),
@@ -49,7 +50,7 @@ WHERE gg.estado_v IN ('7')
   AND hg.subred = (SELECT subred FROM usuarios WHERE id_usuario ='{$_SESSION['us_sds']}')
    ".whe_sesigcole()." 
 	GROUP BY sc.id_cole, sc.fecha, sc.tipo_activ, sc.lugar,sc.tematica1, sc.des_temati1, u.nombre, sc.fecha_create, sc.estado 
-	LIMIT $pag, $regxPag";
+	LIMIT $pag, $regxPag"; */
   echo $sql;
 		$datos=datos_mysql($sql);
 	return create_table($total,$datos["responseResult"],"sesigcole",$regxPag); 

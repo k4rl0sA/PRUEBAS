c079c3cea9184603775dcbf8a46dca12d7232396
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-14 01:00:58 -0500

    12-14 SesionColect 01:00

--- sesicole/sesiperson.php
+++ sesicole/sesiperson.php
@@ -44,7 +44,7 @@ function cmp_sespers(){
 	if ($d==""){$d=$t;}
 	// var_dump($_POST);
 	$id=divide($_POST['id']);
-    $w="sesions";
+    $w="alertas";
 	$o='infbas';
 	$key='pEr';
 	// var_dump($p);
@@ -54,7 +54,7 @@ function cmp_sespers(){
 	$c[]=new cmp($o,'e',null,'IDENTIFICACIÓN DE PERSONAS',$w);
 	$c[]=new cmp('idp','h',15,$_POST['id'],$w.' '.$o,'id','id',null,'####',true,false);
 	$c[]=new cmp('idpersona','n','18','',$w.' '.$key.' '.$o,'Identificación <a href="https://www.adres.gov.co/consulte-su-eps" target="_blank">     Abrir ADRES</a>','idpersona',null,null,true,true,'','col-4');
-	$c[]=new cmp('tipo_doc','s','3','',$w.' '.$key.' '.$o,'Tipo documento','tipo_doc',null,null,true,true,'','col-4',"getDatForm('pEr','personOld',['sesions'],this);");
+	$c[]=new cmp('tipo_doc','s','3','',$w.' '.$key.' '.$o,'Tipo documento','tipo_doc',null,null,true,true,'','col-4',"getDatForm('pEr','personOld',['infbas'],this);");
 	$c[]=new cmp('nombre1','t','30','',$w.' '.$o,'Primer Nombre','nombre1',null,null,true,true,'','col-2');
 	$c[]=new cmp('nombre2','t','30','',$w.' '.$o,'Segundo Nombre','nombre2',null,null,false,true,'','col-2');
 	$c[]=new cmp('apellido1','t','30','',$w.' '.$o,'Primer Apellido','apellido1',null,null,true,true,'','col-2');

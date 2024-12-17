16f4d03afed98da448c00e841de0fb2636781423
0scarC1 <ingsoportesds2@gmail.com>
2024-12-17 06:35:22 -0500

    12-17  etniasfor 06:35

--- crea-fam/etnias.php
+++ crea-fam/etnias.php
@@ -62,7 +62,7 @@ function cmp_etnias(){
 	$c[]=new cmp($o,'e',null,'SESIONES ETNIAS',$w);
 	$c[]=new cmp('idsesetn','h',15,$_POST['id'],$w.' '.$o,'id','idg',null,'####',false,false);
 	$c[]=new cmp('fecha','d','10',$d,$w.' '.$o,'Fecha Sesion','fecha',null,null,true,true,'','col-15',"validDate(this,$days,0);");
-	$c[]=new cmp('tipo_activi','s','3',$d,$w.' '.$o,'','tipo_activi',null,null,true,true,'','col-5');
+	$c[]=new cmp('tipo_activi','s','3',$d,$w.' '.$o,'H','tipo_activi',null,null,true,true,'','col-5');
 	$c[]=new cmp('tipo_activi','s','3',$d,$w.' '.$o,'','tipo_activi',null,null,true,true,'','col-5');
 
 	$o='espvit';

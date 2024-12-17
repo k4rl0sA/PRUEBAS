58ba2318648d23f606a62d46264152c235bd8b79
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-17 14:26:33 -0500

    12-17 Etnias 14:26

--- crea-fam/etnias.php
+++ crea-fam/etnias.php
@@ -96,7 +96,7 @@ function cmp_etnias(){
 function get_person(){
 	// var_dump($_POST);
   $id=divide($_POST['id']);
-    $sql="SELECT CONCAT_WS(' ',p.nombre1, p.apellido1) nombre,FN_CATALOGODESC(21,p.sexo) sexo,
+    $sql="SELECT CONCAT_WS(' ',p.nombre1, p.apellido1) nombre,p.sexo,
 	CONCAT('AÑOS: ',TIMESTAMPDIFF(YEAR, fecha_nacimiento, CURDATE()),' MESES: ',
     TIMESTAMPDIFF(MONTH, fecha_nacimiento, CURDATE())- (TIMESTAMPDIFF(YEAR, fecha_nacimiento, CURDATE()) *12),' DIAS: ',
     DATEDIFF(CURDATE(),DATE_ADD(fecha_nacimiento, INTERVAL TIMESTAMPDIFF(YEAR, fecha_nacimiento, CURDATE()) YEAR)) %30) edad,p.fecha_nacimiento 

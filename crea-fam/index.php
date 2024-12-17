de09c839d183bad26dc01d1a32881292ed613b0b
0scarC1 <ingsoportesds2@gmail.com>
2024-12-17 12:57:02 -0500

    12-17  etniasfor 12:56

--- crea-fam/index.php
+++ crea-fam/index.php
@@ -155,7 +155,8 @@ function grabar(tb='',ev){
 	'tamhamilton':'../hamilton/lib.php',
 	'tamWhodas':'../whodas/lib.php',
 	'tamBarthel':'../barthel/lib.php',
-	'tamzarit':'../zarit/lib.php'
+	'tamzarit':'../zarit/lib.php',
+  'etnias':'etnias.php'
  	};
    let ruta_app = rutaMap[tb] || 'lib.php';
   if(tb=='sesion2'){

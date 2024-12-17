e0e4db543ec748a43c79add351985bfc5f220d31
Jcurrea91 <ingsoportesds1@gmail.com>
2024-12-17 11:31:12 -0500

    12-17 Agendamiento 11:31

--- agendamiento/lib.php
+++ agendamiento/lib.php
@@ -138,13 +138,13 @@ function get_agendamiento(){
     return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=18 and estado='A' ORDER BY 1",$id);
   }
   function opc_tconsulta($id=''){
-    return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=240 and estado='A' ORDER BY 1",$id);
+    return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=1 and estado='A' ORDER BY 1",$id);
   }
   function opc_punto_atenc($id=''){
     return opc_sql("SELECT `idcatadeta`,concat(idcatadeta,' - ',descripcion) FROM `catadeta` WHERE idcatalogo=241 and estado='A' ORDER BY LENGTH(idcatadeta), idcatadeta",$id);
   }
   function opc_tipo_cita($id=''){
-    return opc_sql("SELECT `idcatadeta`,concat(idcatadeta,' - ',descripcion) FROM `catadeta` WHERE idcatalogo=39 and estado='A' ORDER BY LENGTH(idcatadeta), idcatadeta",$id);	
+    return opc_sql("SELECT `idcatadeta`,concat(idcatadeta,' - ',descripcion) FROM `catadeta` WHERE idcatalogo=240 and estado='A' ORDER BY LENGTH(idcatadeta), idcatadeta",$id);	
   }
 
 function gra_gestuser(){

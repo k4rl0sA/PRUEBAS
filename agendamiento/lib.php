b20fbf237fe06f76b4e4b2b32dfb65fa5c3f1a34
Jcurrea91 <ingsoportesds1@gmail.com>
2024-12-17 09:40:31 -0500

    12-17 Agendamiento 09:40

--- agendamiento/lib.php
+++ agendamiento/lib.php
@@ -78,6 +78,39 @@ function cap_menus($a,$b='cap',$con='con') {
 
 function cmp_agendamiento(){
 	$rta="";
+  $t=['id_persona'=>'','tipodoc'=>'','nombre1'=>'','nombre2'=>'','apellido1'=>'','apellido2'=>'','tipo_doc'=>'',
+ 'fecha_nacimiento'=>'','edad'=>'','genero'=>'','eapb'=>'','telefono1'=>'','telefono2'=>'','nombre_atendio'=>'','observac_cita'=>''];
+ $w='agendamiento';
+ $d=get_agendamiento(); 
+  //~ echo(json_encode($d));
+ if ($d=="") {$d=$t;}
+ $u=($d['id_persona']=='')?true:false;
+ $o='percit';
+ $c[]=new cmp($o,'e',null,'AGENDAMIENTO DE USUARIOS',$w);
+ $c[]=new cmp('ipe','h',50,$_POST['id'],$w,'','idp',null,'','','');  
+ //~ $c[]=new cmp('fcr','h',18,$d['fecha_create'],$w.' '.$o,'',0,'','','',false,'','col-4');
+ $c[]=new cmp('idp','n',18,$d['id_persona'],$w.' '.$o,'N° Identificación',0,'rgxdfnum','#################',true,$u,'','col-3');
+ $c[]=new cmp('tdo','s',3,$d['tipodoc'],$w.' '.$o,'Tipo Documento','tipo_doc',null,null,true,$u,'','col-4','getPerson');
+ $c[]=new cmp('no1','t',50,$d['nombre1'],$w.' '.$o,'Primer Nombre','nombre1',null,null,false,false,'','col-3');
+ $c[]=new cmp('no2','t',50,$d['nombre2'],$w.' '.$o,'Segundo Nombre','nombre2',null,null,false,false,'','col-3');
+ $c[]=new cmp('ap1','t',50,$d['apellido1'],$w.' '.$o,'Primer Apellido','apellido1',null,null,false,false,'','col-4');
+ $c[]=new cmp('ap2','t',50,$d['apellido2'],$w.' '.$o,'Segundo Apellido','apellido2',null,null,false,false,'','col-3');
+ $c[]=new cmp('fen','d',10,$d['fecha_nacimiento'],$w.' '.$o,'Fecha de Nacimiento','fecha_nacimiento',null,null,false,false,'','col-3');
+ $c[]=new cmp('eda','t',50,$d['edad'],$w.' '.$o,'Edad','edad',null,null,false,false,'','col-4');
+ $c[]=new cmp('gen','s',3,$d['genero'],$w.' '.$o,'Sexo','genero',null,null,false,false,'','col-3');
+ $c[]=new cmp('eap','s',3,$d['eapb'],$w.' '.$o,'Eapb','eapb',null,null,false,false,'','col-3');
+ $c[]=new cmp('te1','t',10,$d['telefono1'],$w.' '.$o,'Telefono 1','telefono1',null,null,false,false,'','col-2');
+ $c[]=new cmp('te2','t',10,$d['telefono2'],$w.' '.$o,'Telefono 2','telefono2',null,null,false,false,'','col-2'); 
+ $c[]=new cmp('con','s',3,$d['tipo_consulta'],$w.' '.$o,'Tipo de Consulta','tconsulta',null,null,true,true,'','col-3'); 
+ $c[]=new cmp('pun','s',3,$d['punto_atencion'],$w.' '.$o,'Punto de Atención','punto_atenc',null,null,true,true,'','col-5'); 
+ $c[]=new cmp('cit','s',3,$d['tipo_cita'],$w.' '.$o,'Tipo de Cita','tipo_cita',null,null,true,$u,'','col-5'); 
+ $c[]=new cmp('fci','d',10,$d['fecha_cita'],$w.' '.$o,'Fecha','fecha',null,null,true,true,'','col-3'); 
+ $c[]=new cmp('hci','c',10,$d['hora_cita'],$w.' '.$o,'Hora','hora',null,null,true,true,'','col-2','validTime');
+ $c[]=new cmp('nom','t',100,$d['nombre_atendio'],$w,'Persona que Atendio','nombre_atendio',null,null,true,true,'','col-5');
+ $c[]=new cmp('obc','a',1000,$d['observac_cita'],$w.' '.$o,'Observaciones','observacion',null,null,false,true,'','col-10s'); 
+ for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
+ $rta.="<br>";
+ $rta.="</div>";
 	return $rta;
 }
 

2b2b853141cb842cc83899286eeb827863cceb71
Jcurrea91 <ingsoportesds1@gmail.com>
2024-12-17 09:40:44 -0500

    12-17 Agendamiento 09:41

--- agendamiento/lib.php
+++ agendamiento/lib.php
@@ -110,7 +110,7 @@ function cmp_agendamiento(){
  $c[]=new cmp('obc','a',1000,$d['observac_cita'],$w.' '.$o,'Observaciones','observacion',null,null,false,true,'','col-10s'); 
  for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
  $rta.="<br>";
- $rta.="</div>";
+$rta.="</div>";   
 	return $rta;
 }
 

0b4358988c279cbcda8cd6478c057d0544554e23
Jcurrea91 <ingsoportesds1@gmail.com>
2024-12-18 11:08:39 -0500

    12-18 Sesicole 11:08

--- sesicole/lib.php
+++ sesicole/lib.php
@@ -109,7 +109,8 @@ function cmp_sesigcole(){
 	$c[]=new cmp('activi','s','15',$d['tipo_activ'],$w.' '.$o,'Tipo de Actividad','fm1',null,null,true,true,'','col-25');
 	$c[]=new cmp('luga','t','100',$d['lugar'],$w.' '.$o,'Lugar','rta',null,null,true,true,'','col-6',"fieldsValue('agen_intra','aIM','1',true);");
 	$c[]=new cmp('jorna','s','3',$d['jorna'],$w.' '.$o,'jornada','rta',null,null,true,true,'','col-5',"fieldsValue('agen_intra','aIM','1',true);");
-	$c[]=new cmp('equi','sm','3',$d['equi'],$w.' '.$o,'Equipo','equi',null,null,true,true,'','col-5',"selectDepend('temati1','des_temati1');");
+	$c[]=new cmp('equi','s','3',$d['equi'],$w.' '.$o,'Equipo','rta',null,null,true,true,'','col-5',"fieldsValue('agen_intra','aIM','1',true);");
+	$c[]=new cmp('temati1','s','3',$d['temati1'],$w.' '.$o,'tematica 1','temati1',null,null,true,true,'','col-15',"selectDepend('temati1','des_temati1');");
 	$c[]=new cmp('desc_temati1','s','3',$d['des_temati1'],$w.' '.$o,'Descripcion tematica 1','desc_temati1',null,null,true,true,'','col-35');
     $c[]=new cmp('temati2','s','3',$d['tematica2'],$w.' '.$o,'tematica 2','temati2',null,null,false,true,'','col-15',"selectDepend('temati2','desc_temati2');");
     $c[]=new cmp('desc_temati2','s','3',$d['des_temati2'],$w.' '.$o,'Descripcion tematica 2','desc_temati2',null,null,false,true,'','col-35');

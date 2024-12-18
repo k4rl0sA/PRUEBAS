04336e92dc89c7d7cca924693e0d8eb310b56f68
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-18 15:20:44 -0500

    12-18 SesionColect 15:20

--- sesicole/lib.php
+++ sesicole/lib.php
@@ -243,7 +243,7 @@ function get_personOld(){
  */
 
  function opc_equi($id=''){
-	return opc_sql("SELECT id_usuario, nombre FROM usuarios WHERE equipo=242 AND estado='A' ORDER BY LPAD(idcatadeta, 2, '0')", $id);
+	return opc_sql("SELECT id_usuario, nombre FROM usuarios WHERE equipo=(select equipo from usuarios where id_usuario=$_SESSION['us_sds']) AND estado='A' ORDER BY LPAD(idcatadeta, 2, '0')", $id);
 }
 
 function opc_jorna($id=''){

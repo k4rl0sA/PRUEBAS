0d225c0afa13d8f6da50c8084e05fef3ca926948
Jcurrea91 <ingsoportesds1@gmail.com>
2024-12-26 12:34:26 -0500

    12-26 MME 12:34

--- vsp/mme.php
+++ vsp/mme.php
@@ -142,6 +142,7 @@ function cmp_mme(){
     $c[]=new cmp('ctrl_postpar_espe','s','3',$d,$w.' PuE '.$o,'¿Tiene control post parto con especialista?','rta',null,null,false,$x,'','col-2',"enabOthSi('ctrl_postpar_espe','Esp');");
     $c[]=new cmp('fecha_postpar_espe','d','10',$d,$w.' PuE Esp '.$o,'Fecha de control post parto con especialista','fecha_postpar_espe',null,null,false,$x,'','col-2');
     $c[]=new cmp('asis_ctrl_postpar_espe','s','3',$d,$w.' PuE '.$o,'¿Asistió a control post parto?','rta',null,null,false,$x,'','col-2',"enabOthSi('ctrl_postpar_espe','Esp');");
+    $c[]=new cmp('porque_no_postpar','t','500',$d,$w.' PuE MFe '.$o,'¿Por que?','porque_no_postpar',null,null,false,$x,'','col-2');
     $c[]=new cmp('consul_apoy_lacmater','s','3',$d,$w.' PuE '.$o,'¿Tiene consulta apoyo lactancia materna?','rta',null,null,false,$x,'','col-2',"enabOthSi('consul_apoy_lacmater','aLM');");
     $c[]=new cmp('fecha_apoy_lacmater','d','10',$d,$w.' PuE aLM '.$o,'Fecha de consulta apoyo lactancia materna','fecha_apoy_lacmater',null,null,false,$x,'','col-2');
     

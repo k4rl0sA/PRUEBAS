c63d75ef0e0d29a8fa1fe27b4301852ba27bc086
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-27 11:16:19 -0500

    12-27 MME  11:16

--- vsp/mme.php
+++ vsp/mme.php
@@ -114,9 +114,9 @@ function cmp_mme(){
     $c[]=new cmp('esqu_vacuna','s','3',$d,$w.' pRe '.$o,'¿Tiene Esquema De Vacunación Completo?','rta',null,null,false,$x,'','col-3');
     $c[]=new cmp('cons_micronutr','s','2',$d,$w.' pRe '.$o,'¿Consume Micronutrientes?','rta',null,null,false,$x,'','col-2');
     $c[]=new cmp('trata_farma','s','3',$d,$w.' pRe '.$o,'Trata de Farma','rta',null,null,false,$x,'','col-2',"enabOthSi('trata_farma','FaR');");
-    $c[]=new cmp('tipo_tratafarma','s','3',$d,$w.' pRe FaR '.$o,'Tipo de Tratamiento','tipo_tratafarma',null,null,false,$x,'','col-2');
+    $c[]=new cmp('tipo_tratafarma','s','3',$d,$w.' pRe '.$o,'Tipo de Tratamiento','tipo_tratafarma',null,null,false,$x,'','col-2');
     $c[]=new cmp('cual','t','100',$d,$w.' pRe SiA '.$o,'Cuál','cual',null,null,false,$x,'','col-4');
-    $c[]=new cmp('adhe_tratafarma','s','3',$d,$w.' pRe FaR '.$o,'Adhe de Tratafarma','rta',null,null,false,$x,'','col-2');
+    $c[]=new cmp('adhe_tratafarma','s','3',$d,$w.' pRe '.$o,'Adhe de Tratafarma','rta',null,null,false,$x,'','col-2');
     $c[]=new cmp('porque_noadh','t','500',$d,$w.' pRe SiA '.$o,'¿Por Qué?','porque_noadh',null,null,false,$x,'','col-4');
     $c[]=new cmp('peso','sd',6,$d,$w.' pRe '.$o,'Peso (Kg) Mín=0.50 - Máx=150.00','fpe','rgxpeso','##.#',false,$x,'','col-2');
     $c[]=new cmp('talla','sd',5,$d,$w.' pRe '.$o,'Talla (Cm) Mín=40 - Máx=210','fta','rgxtalla','###.#',false,$x,'','col-2',"calImc('peso','talla','imc');");

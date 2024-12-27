2b9473f111148a741dfeb2f3a3eceeadd0169327
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-27 10:51:39 -0500

    12-27 ajusteCaracte  10:51

--- crea-caract/lib.php
+++ crea-caract/lib.php
@@ -23,7 +23,6 @@ function cmp_caract(){
     $rta="";
 	$rta .="<div class='encabezado vivienda'>TABLA DE CARACTERIZACIONES</div>
 	<div class='contenido' id='caracteriza-lis' >".lis_caracterizaciones()."</div></div>";
-
 	// $hoy=date('Y-m-d');
 	/* $t=['idviv'=>'','tipo_familia'=>'','vinculos'=>'','ingreso'=>'','seg_pre1'=>'','seg_pre2'=>'','seg_pre3'=>'','seg_pre4'=>'','seg_pre5'=>'','seg_pre6'=>'',
 	'seg_pre7'=>'','seg_pre8'=>'','subsidio_1'=>'','subsidio_2'=>'','subsidio_3'=>'','subsidio_4'=>'','subsidio_5'=>'','subsidio_6'=>'','subsidio_7'=>'',
@@ -42,7 +41,7 @@ function cmp_caract(){
 	$c[]=new cmp($o,'e',null,'INFORMACIÓN DE LA VIVIENDA',$w);
 	$c[]=new cmp('idg','h',15,$_POST['id'],$w.' '.$o,'id','idg',null,'####',false,false);
 	$c[]=new cmp('fecha','d','10',$d,$w.' '.$o,'fecha Caracterización','fecha',null,'',true,true,'','col-2',"validDate(this,$days,0);");
-	$c[]=new cmp('motivoupd','s','3',$d,$w.' '.$o,'Tipo de Caracterización','motivoupd',null,'',false,true,'','col-3');
+	$c[]=new cmp('motivoupd','s','3',$d,$w.' '.$o,'Tipo de Caracterización','motivoupd',null,'',true,true,'','col-3');
 	$c[]=new cmp('eventoupd','s','3',$d,$w.' '.$o,'Evento Notificado','evenupd',null,'',false,true,'','col-3');
 	$c[]=new cmp('fechanot','d','10',$d,$w.' '.$o,'fecha Notificación','fechanot',null,'',false,true,'','col-2',"validDate(this,$days,0);");
 	

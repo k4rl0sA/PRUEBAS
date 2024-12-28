7cb9c55d92f451900cb9f49c0c20f86c71817591
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2024-12-28 01:56:38 -0500

    12-28 MME 01:56

--- Catalogo/lib.php
+++ Catalogo/lib.php
@@ -20,39 +20,45 @@ if (!isset($_SESSION['us_sds'])) {
 	}
 }
 
-function whe_catalogo() {
-	$sql = "";
-	if ($_POST['fidcata'])
-		$sql .= " AND idcatalogo='".$_POST['fidcata']."' ";
-	if ($_POST['fcatalogo'])
-		$sql .= " AND descripcion like '%".$_POST['fcatalogo']."%' ";
-	if ($_POST['festado'])
-		$sql .= " AND estado = '".$_POST['festado']."' ";
+function whe_deriva() {
+		$sql = "";
+	if ($_POST['fidp'])
+		$sql .= " AND documento='".$_POST['fidp']."' ";
+	if ($_POST['fest'])
+		$sql .= " AND realizado IN (".$_POST['fest'].")";
+	/* if ($_POST['fdes']) {
+		$fefin=date('Y-m-d');
+		$feini = date("Y-m-d",strtotime($fefin."- 2 days"));
+		if ($_POST['fhas']) {
+		      $sql .= " AND fecha_create BETWEEN '$feini 00:00:00' and '$fefin 23:59:59' ";
+		} else {
+		    $sql .= " AND fecha_create BETWEEN '$feini 00:00:00' and '$feini 23:59:59' ";
+		}
+	} */
 	return $sql;
 }
 
-function lis_catalogo(){	
-	$info=datos_mysql("SELECT COUNT(*) total FROM catadeta	WHERE 1 ".whe_catalogo());
+function lis_deriva(){	
+	$info=datos_mysql("SELECT COUNT(*) total FROM derivaciones WHERE 1 ".whe_deriva());
 	$total=$info['responseResult'][0]['total'];
 	$regxPag=17;
-	$pag=(isset($_POST['pag-catalogo']))? ($_POST['pag-catalogo']-1)* $regxPag:0; 
+	$pag=(isset($_POST['pag-deriva']))? ($_POST['pag-deriva']-1)* $regxPag:0; 
 
-	$sql="SELECT idcatalogo ACCIONES,idcatadeta ID,descripcion,estado,valor from catadeta
-	 WHERE 1  ";
-	$sql.=whe_catalogo();
-	$sql.=" ORDER BY 1,2,CAST(idcatadeta AS UNSIGNED), idcatadeta";
+	$sql="SELECT  `id_deriva` ACCIONES,`documento`, `tipo_doc` , `derivado_colaborador` Colaborador, `realizado`, `observacion` from derivaciones
+	 WHERE 1 ".whe_deriva()." 
+	 ORDER BY fecha_create";
 	$sql.=' LIMIT '.$pag.','.$regxPag;
 	// echo $sql;
 		$datos=datos_mysql($sql);
-	return create_table($total,$datos["responseResult"],"catalogo",$regxPag);
+	return create_table($total,$datos["responseResult"],"deriva",$regxPag,"lib.php");
 }
 
 function focus_catalogo(){
-	return 'catalogo';
+	return 'deriva';
    }
    
-   function men_catalogo(){
-	$rta=cap_menus('catalogo','pro');
+   function men_deriva(){
+	$rta=cap_menus('deriva','pro');
 	return $rta;
    }
    
@@ -66,50 +72,79 @@ function focus_catalogo(){
 	return $rta;
   }
 
-  function cmp_catalogo(){
+  function cmp_deriva(){
 	$rta="";
-	$t=['idcatalogo'=>'','idcatadeta'=>'','descripcion'=>'','valor'=>'','estado'=>'A'];
-	$w='catalogo';
-	//~ $id=explode('-',$_REQUEST['id']);
-	$d=get_catalogo(); 
-	//~ var_dump($d);
+	$t=['id_deriva'=>'','documento'=>'','tipo_doc'=>'','predio'=>'','derivado_colaborador'=>'','realizado'=>'','observacion'=>''];
+	$w='deriva';
+	$uPd = $_REQUEST['id']=='0' ? true : false;
+	$d=get_deriva(); 
+	//print_r($d);
 	if ($d=="") {$d=$t;}
-	$d['estado']=($d['estado']=='A')?'SI':'NO';
-	if($d['idcatalogo']){$ids=$d['idcatalogo'].'_'.$d['idcatadeta'];}else{$ids='';}
-	$o='infcat';
-	$c[]=new cmp('id','h',100,$ids,$w,'',0,'','','',false,'','col-1');
-	$c[]=new cmp('cat','s',3,$d['idcatalogo'],$w.' '.$o,'Nombre Catalogo','catalogo',null,'',false,false,'','col-2');
-	$c[]=new cmp('cod','t',12,$d['idcatadeta'],$w.' '.$o,'Codigo Item','',null,'',false,false,'','col-2');
-	$c[]=new cmp('des','t',40,$d['descripcion'],$w.' '.$o,'Texto Descriptivo Item',null,null,'',false,false,'','col-2');
-	$c[]=new cmp('est','o',1,$d['estado'],$w.' '.$o,'Item Activo',null,null,'',false,false,'','col-2');
-	$c[]=new cmp('val','n',10,$d['valor'],$w.' '.$o,'Valor',0,'rgxdfnum','NNNN',false,true,'Número de 4 a 10 Digitos','col-1');
+	$o='docder';
+	$c[]=new cmp('id','h',100,$d['id_deriva'],$w,'',0,'','','',false,'','col-1');
+	$c[]=new cmp('doc','t',20,$d['documento'],$w.' '.$o,'Documento','',NULL,'',true,$uPd,'','col-2');
+	$c[]=new cmp('tip','s',3,$d['tipo_doc'],$w.' '.$o,'tipo_doc','tipo_doc',null,'',true,$uPd,'','col-2');
+	$c[]=new cmp('pre','h',15,$d['predio'],$w.' '.$o,'Predio',null,null,'',false,false,'','col-2');
+	$c[]=new cmp('asi','s',12,$d['derivado_colaborador'],$w.' '.$o,'Asignado A','asignado',null,'',true,$uPd,'','col-2');
+	$c[]=new cmp('rea','o',1,$d['realizado'],$w.' '.$o,'Realizado',null,null,'',true,true,'','col-2');
+	$c[]=new cmp('obs','t',10,$d['observacion'],$w.' '.$o,'Observación',null,null,'',true,true,'Número de 4 a 10 Digitos','col-1');
 	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
 	$rta.="</div>";
 	return $rta;
 	}
 
-	function opc_catalogo(){
-	 return opc_sql("SELECT `idcatalogo`,concat(idcatalogo,' - ',nombre) FROM `catalogo` ORDER BY 1",$id = ($_POST['id'] == '') ? '' : divide($_POST['id'])[0]);
+
+	function opc_tipo_doc($id=''){
+		return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=1 and estado='A' ORDER BY 1",$id);
+	}
+	function opc_asignado($id=''){
+		return opc_sql("SELECT id_usuario,nombre FROM usuarios WHERE perfil in('PROFAM','AUXHOG') and estado='A' ORDER BY 1",$id);
 	}
 
-	function get_catalogo(){
+
+	function get_deriva(){
 		if($_POST['id']=='0'){
 			return "";
 		}else{
 			$id=divide($_POST['id']);
-			$sql="SELECT * FROM catadeta WHERE idcatalogo='".$id[0]."' AND idcatadeta='".$id[1]."'";
+			$sql="SELECT * FROM derivaciones WHERE id_deriva='".$id[0]."'";
 			$info=datos_mysql($sql);
 			return $info['responseResult'][0];		
 		} 
 	}
 
+/* 	function imp_deriva(){
+		$id=$_POST['id'];
+		$info['responseResult'][['id_deriva'] => 1,
+			['documento'] => '80811594',
+		  	['tipo_doc'] => 'CC',
+			['predio'] => 452147];
+		// $info['responseResult'];
+		/* if ($_POST['id']=== 0 ) {
+			$sql="SELECT idpersona,tipo_doc,concat_ws(' ',nombre1,nombre2,apellido1,apellido2) nombres,fecha_nacimiento,YEAR(CURDATE())-YEAR(fecha_nacimiento) Edad
+				from personas
+				WHERE idpersona='".$id[0]."' AND tipo_doc=upper('".$id[1]."')";
+		$info=datos_mysql($sql);
+		if (!$info['responseResult']) {
+			return json_encode (new stdClass);
+		}	
+		}else{
+
+		} 
+	return json_encode($info['responseResult'][0]);
+	} */
+
+	function btn_deriva(){
+		print_r(acceBtns('derivacion'));
+		echo json_encode(acceBtns('derivacion'));
+	}
+
    function formato_dato($a,$b,$c,$d){
 	$b=strtolower($b);
 	$rta=$c[$d];
-	 if ($a=='catalogo'&& $b=='id'){$rta= "<div class='txt-center'>".$c['ID']."</div>";}
-	if (($a=='catalogo') && ($b=='acciones'))    {
+	if (($a=='deriva') && ($b=='acciones')){
 		   $rta="<nav class='menu right'>";
-		   $rta.="<li class='fa-solid fa-pen-to-square' title='Editar $a' id='".$c['ACCIONES']."_".$c['ID']."' Onclick=\"mostrar('catalogo','pro',event,'','lib.php',4);\"></li>";
+		   $rta.="<li class='fa-solid fa-pen-to-square' title='Editar Derivación' id='".$c['ACCIONES']."' Onclick=\"mostrar('deriva','pro',event,'','lib.php',4);\"></li>";
 		   $rta.="</nav>";
 	   }    
 	return $rta;
@@ -120,5 +155,4 @@ function bgcolor($a,$c,$f='c'){
 	//~ if ($a=='transacciones'&&$c['ESTADO']=='A') $rta='green';
 	return $rta;
    }
-
 ?>

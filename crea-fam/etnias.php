4bc6f2fe05387b883e01946b549568759a8063c4
Jcurrea91 <ingsoportesds1@gmail.com>
2025-01-02 18:08:32 -0500

    02-01 Etnia 18:08

--- crea-fam/etnias.php
+++ crea-fam/etnias.php
@@ -97,6 +97,7 @@ function cmp_etnias(){
 	$c[]=new cmp('edad_gesta','s','3',$d,$w.'  '.$o,'Edad Gestacional Al Momento De Identificacion En Semanas','edad_gesta',null,null,true,true,'','col-3');
 	$c[]=new cmp('resul_gest','s','3',$d,$w.' '.$o,'Resultado De La Gestación','resul_gest',null,null,true,true,'','col-3',"enabClasValu('resul_gest',['ncvmor','mOr','NOm']);");
 	$c[]=new cmp('peso_nacer','sd','4',$d,$w.' '.$o,'Peso Al Nacer (Gr)','peso_nacer','rgxpeso','##.#',true,true,'','col-2');
+	$c[]=new cmp('asis_ctrpre','s','2',$d,$w.' pRe '.$o,'¿Asiste A Controles Prenatales?','rta',null,null,true,true,'','col-2',"enabOthNo('asis_ctrpre','CtP');disaOthNo('asis_ctrpre','CPn');");
 	//$c[]=new cmp('des_sin','t','100',$d,$w.' '.$o,'Descripcion Sintoma','des_sin',null,null,true,true,'','col-10');
 
 

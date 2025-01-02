376ebac7e445729e6c6a2d0cc019dbd9745b2a4a
Jcurrea91 <ingsoportesds1@gmail.com>
2025-01-02 18:21:35 -0500

    02-01 Etnia 18:21

--- crea-fam/etnias.php
+++ crea-fam/etnias.php
@@ -104,6 +104,9 @@ function cmp_etnias(){
 	$c[]=new cmp('signos_alarma','s','3',$d,$w.'  '.$o,'¿Presenta Signos De Alarma?','rta',null,null,true,true,'','col-2');
 	$c[]=new cmp('diag_sifilis','s','3',$d,$w.'  '.$o,'¿Diagnosticada Con Sifilis Gestacional ?','rta',null,null,true,true,'','col-2');
 	$c[]=new cmp('adhe_tto','s','3',$d,$w.'  '.$o,'¿Adherencia A Tratamiento?','rta',null,null,true,true,'','col-2');
+	$c[]=new cmp('sifilis_cong','s','3',$d,$w.'  '.$o,'¿Sifilis Congenita?','rta',null,null,true,true,'','col-2');
+	$c[]=new cmp('seg_partera','s','3',$d,$w.'  '.$o,'¿Le Ha Realizado Seguimiento Partera? ','rta',null,null,true,true,'','col-2');
+	$c[]=new cmp('seg_medanc','s','3',$d,$w.'  '.$o,'¿Le Ha Realizado Seguimiento El Médico Ancestral?','rta',null,null,true,true,'','col-2');
 	//$c[]=new cmp('des_sin','t','100',$d,$w.' '.$o,'Descripcion Sintoma','des_sin',null,null,true,true,'','col-10');
 
 

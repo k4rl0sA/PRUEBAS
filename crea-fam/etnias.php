3758bb00f28a375dfb7a85f117c846b9b902a7de
Jcurrea91 <ingsoportesds1@gmail.com>
2025-01-02 15:41:56 -0500

    02-01 Etnia 15:41

--- crea-fam/etnias.php
+++ crea-fam/etnias.php
@@ -80,10 +80,11 @@ function cmp_etnias(){
 	$c[]=new cmp($o,'e',null,'IDENTIFICACIÓN DEL RIESGO',$w);
 	$c[]=new cmp('idsesetn','h',15,$_POST['id'],$w.' '.$o,'id','idg',null,'####',false,false);
 	$c[]=new cmp('des_sin','t','100',$d,$w.' '.$o,'Lider Con El Que Se Identifica La Familia','des_sin',null,null,true,true,'','col-35');
-	$c[]=new cmp('ser_edu','s','3',$d,$w.' '.$o,'Esta Vinculado (a) A Servicios De Educación','rta',null,null,true,true,'','col-2');
-	$c[]=new cmp('ent_esp','s','3',$d,$w.' '.$o,'¿Entiende Castellano - Español?','rta',null,null,true,true,'','col-2');
-	$c[]=new cmp('saberpro','s','3',$d,$w.' '.$o,'Saberes Propios','saberpro',null,null,true,true,'','col-25');
+	$c[]=new cmp('ser_edu','s','3',$d,$w.' '.$o,'Esta Vinculado (a) A Servicios De Educación','rta',null,null,true,true,'','col-3');
+	$c[]=new cmp('ent_esp','s','3',$d,$w.' '.$o,'¿Entiende Castellano - Español?','rta',null,null,true,true,'','col-3');
+	$c[]=new cmp('saberpro','s','3',$d,$w.' '.$o,'Saberes Propios','saberpro',null,null,true,true,'','col-3');
 	$c[]=new cmp('enf_dif','s','3',$d,$w.' '.$o,'Enfoque Diferencial','enf_dif',null,null,true,true,'','col-2');
+	$c[]=new cmp('tip_inv','s','3',$d,$w.' '.$o,'Tipo De Intervención','tip_inv',null,null,true,true,'','col-2');
 	//$c[]=new cmp('des_sin','t','100',$d,$w.' '.$o,'Descripcion Sintoma','des_sin',null,null,true,true,'','col-10');
 
 	$o='espvit';

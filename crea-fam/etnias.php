7cf7ccd0b5fc7e879579d190d3c815fe2c4ecb6b
Jcurrea91 <ingsoportesds1@gmail.com>
2025-01-03 13:16:10 -0500

    03-01 Etnia 13:15

--- crea-fam/etnias.php
+++ crea-fam/etnias.php
@@ -386,6 +386,6 @@ $sql="SELECT (POWER(($id[0] / (SELECT M FROM tabla_zscore WHERE indicador = '$in
 	function opc_ubi_acom($id=''){
 		return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=253 and estado='A' ORDER BY 1",$id);
 	}
-	function opc_res_get($id=''){
+	function opc_res_gest($id=''){
 		return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=254 and estado='A' ORDER BY 1",$id);
 	}

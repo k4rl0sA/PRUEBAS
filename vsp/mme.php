bcfeedbb2fb6af82ec408c7a9117cdce677df9f5
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-01-07 13:47:17 -0500

    01-07 MME  13:47

--- vsp/mme.php
+++ vsp/mme.php
@@ -143,8 +143,8 @@ function cmp_mme(){
     $c[]=new cmp('asis_ctrl_postpar_espe','s','3',$d,$w.' PuE aST '.$o,'¿Asistió a control post parto?','rta2',null,null,false,$x,'','col-2',"disaOthNo('asis_ctrl_postpar_espe','NPp');validDate(this,-400,0);");
     $c[]=new cmp('porque_no_postpar','t','500',$d,$w.' PuE MFe NPp '.$o,'¿Por que?','porque_no_postpar',null,null,false,$x,'','col-4');
     $c[]=new cmp('consul_apoy_lacmater','s','3',$d,$w.' PuE '.$o,'¿Tiene consulta apoyo lactancia materna?','rta',null,null,false,$x,'','col-2',"enabOthSi('consul_apoy_lacmater','aLM');");
-    $c[]=new cmp('signos_alarma','s','3',$d,$w.' PuE '.$o,'Identifica signos de alarma al momento del seguimiento','rta',null,null,false,$x,'','col-2',"enabOthSi('signos_alarma','MFe');");
-    $c[]=new cmp('desc_sigala','t','500',$d,$w.' PuE MFe '.$o,'Descripcion del signo de alarma','desc_sigala',null,null,false,$x,'','col-4');
+    $c[]=new cmp('signos_alarma','s','3',$d,$w.' PuE '.$o,'Identifica signos de alarma al momento del seguimiento','rta',null,null,false,$x,'','col-2',"enabOthSi('signos_alarma','dsA');");
+    $c[]=new cmp('desc_sigala','t','500',$d,$w.' PuE dsA '.$o,'Descripcion del signo de alarma','desc_sigala',null,null,false,$x,'','col-4');
     $c[]=new cmp('disc_ges','s','3',$d,$w.' PuE '.$o,'¿La gestante presenta alguna discapacidad o secuela posterior al evento obstetrico?','rta',null,null,false,$x,'','col-3',"enabOthSi('disc_ges','dGp');");
     $c[]=new cmp('cual_disc_ges','t','500',$d,$w.' PuE dGp '.$o,'¿Cúal?','cual_disc_ges',null,null,false,$x,'','col-4');
     $c[]=new cmp('fecha_apoy_lacmater','d','10',$d,$w.' PuE aLM '.$o,'Fecha de consulta apoyo lactancia materna','fecha_apoy_lacmater',null,null,false,$x,'','col-2');

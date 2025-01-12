9677bf991821678423cfa80eff0c9d6c0a61571e
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-01-12 10:47:56 -0500

    01-12 etnias 10:47

--- 1/02src/gestion.php
+++ 1/02src/gestion.php
@@ -32,3 +32,36 @@ switch ($req) {
 		}
 		break;
 }
+
+function datos_mysql($sql,$resulttype = MYSQLI_ASSOC, $pdbs = false){
+    $arr = ['code' => 0, 'message' => '', 'responseResult' => []];
+    $con = $GLOBALS['con'];
+  if (!$con) {
+    $arr['code'] = 30;
+    $arr['message'] = 'No hay conexión activa a la base de datos.';
+    log_error($_SESSION["us_sds"] . ' = Connection error');
+    return $arr;
+  }
+  try {
+    $con->set_charset('utf8');
+    $rs = $con->query($sql);
+    if (!$rs) {
+      log_error($_SESSION["us_sds"] . ' Error en la consulta: ' . $con->error, $con->errno);
+      throw new mysqli_sql_exception("Error en la consulta: " . $con->error, $con->errno);
+    }
+    fetch($con, $rs, $resulttype, $arr);
+  } catch (mysqli_sql_exception $e) {
+    echo json_encode(['code' => 30, 'message' => 'Error BD', 'errors' => ['code' => $e->getCode(), 'message' => $e->getMessage()]]);
+    log_error($_SESSION["us_sds"].'=>'.$e->getCode().'='.$e->getMessage());
+  }finally {
+    // $GLOBALS['con']->close();
+  }
+  return $arr;
+  }
+
+  function log_error($message) {
+    if (!is_dir('../logs')) {
+      mkdir('../logs', 0777, true);
+  }
+    file_put_contents('../logs/file.log', "[" . date('Y-m-d H:i:s') . "] " . $message . PHP_EOL, FILE_APPEND);
+  }

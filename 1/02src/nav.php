8609150217654f2aa7954f590e3581e1eca06b7f
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-01-12 10:42:12 -0500

    01-12 etnias 10:42

--- 1/02src/nav.php
+++ 1/02src/nav.php
@@ -9,7 +9,8 @@
 ini_set('display_errors','1');
 $vers='1.03.29.1';
 // if (!isset($_SESSION["us_riesgo"])){ die("<script>window.top.location.href = '/';</script>");}
-require_once __DIR__ . '/gestion.php';
+require_once __DIR__ . '../../02src/gestion.php';
+// require_once $_SERVER['DOCUMENT_ROOT'].'/libs/php/gestion.php';
   $sql="SELECT *
   FROM adm_menu
   WHERE id IN (

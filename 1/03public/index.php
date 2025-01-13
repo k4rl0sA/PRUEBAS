0034933e42a50cc7bda3cdac8a3aa01ee3ada0b1
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-01-12 23:11:03 -0500

    01-12 etnias 23:10

--- 1/03public/index.php
+++ 1/03public/index.php
@@ -1,8 +1,8 @@
 <?php
 // Incluir el archivo de configuración
 require_once __DIR__ . '/../01config/config.php';
-session_start();
 require_once __DIR__ . '/login/login.php';
+var_dump(session_status());
 
 /* // claves.php o config.php
 if (session_status() === PHP_SESSION_NONE) {

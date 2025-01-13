c2da7599ac0a7d51d70459cc3ddebab288b8566b
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-01-12 23:43:19 -0500

    01-12 etnias 23:43

--- 1/03public/Ejemplo/lib.php
+++ 1/03public/Ejemplo/lib.php
@@ -3,40 +3,20 @@ require_once __DIR__ . '/../02src/gestion.php';
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-$_POST['a'].'-'.$_POST['tb'];
-$perf = perfil($_POST['tb']);
-if (!isset($_SESSION[SESSION_NAME])) {
-    http_response_code(401);
-    echo json_encode(['redirect' => '/01/03public/']);
-    exit();
-} else {
-	$rta = "";
-	switch ($_POST['a']) {
-		case 'csv':
-			header_csv($_REQUEST['tb'] . '.csv');
-			$rs = array('', '');
-			echo csv($rs, '');
-			break;
-		default:
-			if (isset($_REQUEST['t']) && $_REQUEST['t'] == 'json') {
-				header('Content-Type: application/json');
-			} else {
-				header('Content-Type: text/html; charset=UTF-8');
-			}
-			$func = $_POST['a'] . '_' . $_POST['tb'];
-			echo $func;
-			if (function_exists($func)) {
-				$rta = $func();
-			} else {
-				http_response_code(400);
-				echo json_encode(['error' => 'Función no encontrada']);
-				exit();
-			}
-			// Si $rta es un arreglo, devolverlo como JSON
-			if (is_array($rta)) {
-				echo json_encode($rta);
-			} else {
-				echo $rta; // Si no es un arreglo, devolver la respuesta directamente
-			}
-	}
+if ($_POST['a']!='opc') $perf=perfil($_POST['tb']);
+if (!isset($_SESSION['us_sds'])) die("<script>window.top.location.href='/';</script>");
+else {
+  $rta="";
+  switch ($_POST['a']){
+  case 'csv': 
+    header_csv ($_REQUEST['tb'].'.csv');
+    $rs=array('','');    
+    echo csv($rs,'');
+    die;
+    break;
+  default:
+    eval('$rta='.$_POST['a'].'_'.$_POST['tb'].'();');
+    if (is_array($rta)) json_encode($rta);
+	else echo $rta;
+  }   
 }

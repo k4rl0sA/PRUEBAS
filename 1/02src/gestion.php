78e2d0019aa66a6cc1b6135383b9db6e5c7003d8
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-01-12 20:01:19 -0500

    01-12 etnias 20:01

--- 1/02src/gestion.php
+++ 1/02src/gestion.php
@@ -2,11 +2,11 @@
 require_once __DIR__ . '/../01config/config.php';
 ini_set('memory_limit','1024M');
 // Verificar si la sesión está activa
-/* if (!isset($_SESSION["us_subred"])) {
+if (!isset($_SESSION[SESSION_NAME])) {
     header("Location: /1/03public/index.php");
     exit;
-} */
-$sesion = $_SESSION["us_subred"];
+}
+$sesion = $_SESSION[SESSION_NAME];
 
 function db_connect() {
     $con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);

2ac413d6bd8f7a5b2f3d33de8de0d405d14d977a
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-01-21 12:39:11 -0500

    01-21 perfilapoyo  12:39

--- zung/lib.php
+++ zung/lib.php
@@ -172,7 +172,7 @@ function men_tamzung(){
    function cap_menus($a,$b='cap',$con='con') {
 	$rta = ""; 
 	$acc=rol($a);
-	/*if ($a=='homes' && isset($acc['crear']) && $acc['crear']=='SI') {  
+	if ($a=='homes' && isset($acc['crear']) && $acc['crear']=='SI') {  
 		$rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>"; //~ openModal();
 	  //   $rta .= "<li class='icono $a exportar'       title='Exportar'    Onclick=\"csv('$a');\"></li>"; 
 		$rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";  
@@ -203,7 +203,7 @@ function men_tamzung(){
 			  $rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
 			  $rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";
 	  
-		  }*/
+		  }
 	return $rta;
   }
    

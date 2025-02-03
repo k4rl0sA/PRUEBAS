94d14e7123e9a3432c477b750eae413569aaec17
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-03 10:47:11 -0500

    02-03 whodas 10:47

--- whodas/lib.php
+++ whodas/lib.php
@@ -246,40 +246,12 @@ function men_tamWhodas(){
    }
 
    function cap_menus($a,$b='cap',$con='con') {
-	$rta = ""; 
+	$rta = "";
 	$acc=rol($a);
-	if ($a=='homes' && isset($acc['crear']) && $acc['crear']=='SI') {  
-		$rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>"; //~ openModal();
-	  //   $rta .= "<li class='icono $a exportar'       title='Exportar'    Onclick=\"csv('$a');\"></li>"; 
-		$rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";  
-		 }
-		 if ($a=='person' && isset($acc['crear']) && $acc['crear']=='SI') {  
-	  
-		  $rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>"; //~ openModal();
-		  // $rta .= "<li class='icono $a exportar'       title='Exportar'    Onclick=\"csv('$a');\"></li>"; 
-		  $rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";
-	  
-		  }
-		  if($a=='atencion' && isset($acc['crear']) && $acc['crear']=='SI'){
-			  $rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
-			  $rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";
-	  
-		  }
-			  if($a=='eac_juventud' && isset($acc['crear']) && $acc['crear']=='SI'){
-			  $rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
-			  $rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";
-	  
-		  }
-		  if($a=='eac_adultez' && isset($acc['crear']) && $acc['crear']=='SI'){
-			  $rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
-			  $rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";
-	  
-		  }
-		  if($a=='eac_vejez' && isset($acc['crear']) && $acc['crear']=='SI'){
-			  $rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
-			  $rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";
-	  
-		  }
+	if ($a=='whodas' && isset($acc['crear']) && $acc['crear']=='SI') {  
+	 $rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>"; //~ openModal();
+	  }
+	$rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";  
 	return $rta;
   }
    

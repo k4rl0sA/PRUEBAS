942826ab1906f72360ba16ac13c17b67d7d053f4
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-04 15:47:14 -0500

    02-04 Etnicos embera seg 15:47

--- etnias/embseg.php
+++ etnias/embseg.php
@@ -98,8 +98,8 @@ function cmp_segnoreg(){
   $c[]=new cmp('diag_sifigest','s',3,$d['diag_sifigest'],$w.' '.$o,'¿Diagnosticada Con Sifilis Gestacional?','rta',null,null,false,true,'','col-3');
   $c[]=new cmp('adhe_tto','s',3,$d['adhe_tto'],$w.' '.$o,'¿Adherencia A Tratamiento?','rta',null,null,false,true,'','col-2');
   $c[]=new cmp('diag_sificong','s',3,$d['diag_sificong'],$w.' '.$o,'¿Sifilis Congenita?','rta',null,null,false,true,'','col-3');
-  $c[]=new cmp('seg_partera','s',3,$d['seg_partera'],$w.' '.$o,'¿Le Ha Realizado Seguimiento Partera?','rta',null,null,false,true,'','col-2');
-  $c[]=new cmp('seg_med_ancestral1','s',3,$d['seg_med_ancestral1'],$w.' '.$o,'¿Le Ha Realizado Seguimiento El Médico Ancestral?','rta',null,null,false,true,'','col-35');
+  $c[]=new cmp('seg_partera','s',3,$d['seg_partera'],$w.' '.$o,'¿Le Ha Realizado Seguimiento Partera?','rta',null,null,false,true,'','col-3');
+  $c[]=new cmp('seg_med_ancestral1','s',3,$d['seg_med_ancestral1'],$w.' '.$o,'¿Le Ha Realizado Seguimiento El Médico Ancestral?','rta',null,null,false,true,'','col-4');
   
   $o='cronicos';
   $c[]=new cmp($o,'e',null,'CONDICIONES CRONICAS',$w);

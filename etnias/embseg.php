7bb7f4b48d0c0e10f130e25911a63849df97d2e6
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-04 13:58:20 -0500

    02-04 Etnicos embera seg 13:58

--- etnias/embseg.php
+++ etnias/embseg.php
@@ -213,7 +213,7 @@ function opc_clasi_nutri($id=''){
  }
 
  function opc_ser_remigesti($id=''){
-       return opc_sql('SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=261 and estado=""A"" ORDER BY 1',$id);
+       return opc_sql('SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=261 and estado="A" ORDER BY 1',$id);
  }
 
 	function formato_dato($a,$b,$c,$d){

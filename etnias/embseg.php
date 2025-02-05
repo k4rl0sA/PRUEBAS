b44287ba2a7250f459a844508ff7b9de36918d57
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-02-05 14:13:36 -0500

    02-05 Etnias 14:13

--- etnias/embseg.php
+++ etnias/embseg.php
@@ -159,8 +159,7 @@ echo 'Vacio';
   }else{
     echo 'NO Vacio';
   }
-  $pn = ($_POST['peso_nacer'] === '') ? null : $_POST['peso_nacer'];
-  $docma=$_POST['doc_madre']?? 0 ;
+  $docma=($_POST['doc_madre']=== '')? null: $_POST['doc_madre'];
   $sis=$_POST['sistolica'] ?? 0;
   $diast=$_POST['diastolica'] ?? 0;
   $fcar=$_POST['frec_cardiaca'] ?? 0;

aebe6d343fd73e1206d7836435dd1c3dab6263f3
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-02-05 14:39:49 -0500

    02-05 Etnias 14:39

--- etnias/embseg.php
+++ etnias/embseg.php
@@ -153,13 +153,8 @@ function cmp_segnoreg(){
 
 function gra_segnoreg(){
 	$id=divide($_POST['id']);
-  if($_POST['doc_madre']=== ''){
-echo 'Vacio';
-  }else{
-    echo 'NO Vacio';
-  }
-  $pn=($_POST['peso_nacer']=== '')? null: $_POST['peso_nacer'];
-  $docma=($_POST['doc_madre']=== '')? 0: $_POST['doc_madre'];
+  $pn=$_POST['peso_nacer']?? 0;
+  $docma=$_POST['doc_madre']??0;
   $sis=$_POST['sistolica'] ?? 0;
   $diast=$_POST['diastolica'] ?? 0;
   $fcar=$_POST['frec_cardiaca'] ?? 0;

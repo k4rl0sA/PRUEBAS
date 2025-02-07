c461766b02bea77d95634dd31626040b516c78b1
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-07 13:50:06 -0500

    02-07 Etnicos seguimiento  13:49

--- etnias/embsegui.php
+++ etnias/embsegui.php
@@ -142,6 +142,9 @@ return $rta;
       } 
 }*/
 
+function opc_segui($id=''){
+  return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=76 and estado='A' ORDER BY 1",$id);
+  }
 
 function opc_motivo_estado($id=''){
   return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=74 and estado='A' ORDER BY 1",$id);

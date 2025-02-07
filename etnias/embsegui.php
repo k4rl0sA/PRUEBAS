d905568cb974ba65f3dc07b074fe139c5903c1d8
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-07 16:01:24 -0500

    02-07 Etnicos seguimiento  15:59

--- etnias/embsegui.php
+++ etnias/embsegui.php
@@ -113,7 +113,7 @@ function cmp_seguim(){
 
     $o='aspfin';
     $c[]=new cmp($o,'e',null,'ASPECTOS FINALES',$w);
-    $c[]=new cmp('observaciones','t',7000,$d,$w.' '.$o,'Observaciones','observaciones',null,null,false,true,'','col-65');
+    $c[]=new cmp('observaciones','a',7000,$d,$w.' '.$o,'Observaciones','observaciones',null,null,false,true,'','col-65');
     $c[]=new cmp('equi','m',3,'',$w.' '.$o,'Equipo','equi',null,null,true,true,'','col-35',"fieldsValue('agen_intra','aIM','1',true);");
     for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
 	return $rta;

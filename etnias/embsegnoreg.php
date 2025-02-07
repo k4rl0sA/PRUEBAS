f3f2a886de6a06ae967dd3f775bc2c0771140557
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-02-07 01:55:26 -0500

    02-07 etnias 01:55

--- etnias/embsegnoreg.php
+++ etnias/embsegnoreg.php
@@ -76,13 +76,12 @@ function cmp_segnoreg(){
   $cro='CrO';
   $me5='mE5';
   $class=[
-    1 => ['PuE'],
+    1 => ['mE5'],
     2 => ['pRe'],
     3 => ['pRe', 'OtraClase'],
     4 => ['ClaseNueva']
 ];
-$title=json_encode($class);
-
+$title = json_encode($class, JSON_HEX_APOS | JSON_HEX_QUOT);
   var_dump($_POST);
 	$c[]=new cmp($o,'e',null,'MODULO INICIAL',$w);
   $c[]=new cmp('id','h',15,$_POST['id'],$w.' '.$o,'id','id',null,'####',false,false);

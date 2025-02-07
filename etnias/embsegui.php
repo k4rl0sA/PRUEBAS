f3dcd6211d5eada2637de3146e08505315c0a8cd
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-07 15:32:56 -0500

    02-07 Etnicos seguimiento  15:32

--- etnias/embsegui.php
+++ etnias/embsegui.php
@@ -60,7 +60,7 @@ $pag=(isset($_POST['pag-seguim']))? ($_POST['pag-seguim']-1)* $regxPag:0;
 
 function cmp_seguim(){
   $rta="<div class='encabezado placuifam'>TABLA SEGUIMIENTOS</div><div class='contenido' id='seguim-lis'>".lis_seguim()."</div></div>";
-  $rta='';
+ // $rta='';
   $w="placuifam";
 	$o='seguim';
   $d='';

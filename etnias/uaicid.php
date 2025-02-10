d3b57153d4a00f124b726fcb1b5aa378be580a0c
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-10 14:36:22 -0500

    02-10 Etnicos seguimiento  14:36

--- etnias/uaicid.php
+++ etnias/uaicid.php
@@ -58,7 +58,7 @@ $pag=(isset($_POST['pag-cambiar']))? ($_POST['pag-cambiar']-1)* $regxPag:0;
 
 
 function cmp_uaic_id(){
-  // $rta="<div class='encabezado'>TABLA SEGUIMIENTOS</div><div class='contenido' id='uaic_id-lis'>".lis_uaic_id()."</div></div>";
+  $rta="<div class='encabezado'>TABLA SEGUIMIENTOS</div><div class='contenido' id='uaic_id-lis'>".lis_uaic_id()."</div></div>";
   $rta='';
   $w="iden";
 	$t=['id'=>'']; 

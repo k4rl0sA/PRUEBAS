0039f1e1c661a30e5d2b434cda489475ed78ca5a
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-10 14:37:29 -0500

    02-10 Etnicos seguimiento  14:37

--- etnias/uaicid.php
+++ etnias/uaicid.php
@@ -61,7 +61,7 @@ function cmp_uaic_id(){
   $rta="<div class='encabezado'>TABLA SEGUIMIENTOS</div><div class='contenido' id='uaic_id-lis'>".lis_uaic_id()."</div></div>";
   $rta='';
   $w="iden";
-	$t=['id'=>'']; 
+	$t=['iduaic'=>'','idpeople'=>'','fecha_seg'=>'','parentesco'=>'','nombre_cui'=>'','tipo_doc'=>'','num_doc'=>'','telefono'=>'','era'=>'','eda'=>'','dnt'=>'','des_sinto'=>'','peri_cef'=>'','peri_bra'=>'','peso'=>'','talla'=>'','zcore'=>'','clasi_nut'=>'','tempe'=>'','frec_res'=>'','frec_car'=>'','satu'=>'','sales_reh'=>'','aceta'=>'','traslados_uss'=>'','educa'=>'','menor_hos'=>'','tempe2'=>'','frec_res2'=>'','frec_car2'=>'','satu2'=>'','seg_entmed'=>'','observacion'=>'','usu_creo'=>'','fecha_create'=>'','usu_update'=>'','fecha_update'=>'','estado']; 
 	$e="";
 	$key='pln';
 	$o='uaic_id';

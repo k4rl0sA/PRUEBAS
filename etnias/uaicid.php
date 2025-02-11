88317f66b242431b8eba57d5e428b0338324a43a
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-11 16:50:11 -0500

    02-11 Etnias uaic iden  16:49

--- etnias/uaicid.php
+++ etnias/uaicid.php
@@ -120,7 +120,7 @@ function cmp_uaic_id(){
 
     $o='aspe';
     $c[]=new cmp($o,'e',null,'ASPECTOS FINALES',$w);
-    $c[]=new cmp('observacion','t',7,$d['observacion'],$w.' '.$o,'Observacion','observacion',null,null,true,true,'','col-10');
+    $c[]=new cmp('observacion','a',7,$d['observacion'],$w.' '.$o,'Observacion','observacion',null,null,true,true,'','col-10');
 	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
 	return $rta;
 }

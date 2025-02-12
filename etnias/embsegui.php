665fa178ebd0ec35ed50a8c2886f85e6627d8977
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-12 12:34:58 -0500

    02-12 Etnias uaic iden  12:34

--- etnias/embsegui.php
+++ etnias/embsegui.php
@@ -60,7 +60,7 @@ $pag=(isset($_POST['pag-seguim']))? ($_POST['pag-seguim']-1)* $regxPag:0;
 function cmp_seguim(){
    $rta="<div class='encabezado seguiEmbera'>TABLA SEGUIMIENTOS</div><div class='contenido' id='seguim-lis'>".lis_seguim()."</div></div>";
 // $rta='';
-  $w="modini";
+  $w='modini';
 	$o='seguim';
   $d='';
   $days=fechas_app('ETNIAS');

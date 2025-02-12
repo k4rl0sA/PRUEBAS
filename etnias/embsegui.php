3567a7c708fe28b5a33c0bc674363dbd5ed91571
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-12 15:15:58 -0500

    02-12 Etnias uaic iden  15:15

--- etnias/embsegui.php
+++ etnias/embsegui.php
@@ -160,8 +160,8 @@ function gra_seguim() {
 ['type' => 's', 'value' => NULL],
 ['type' => 's', 'value' => NULL],
 ];
-//$rta=show_sql($sql, $params);
-$rta = mysql_prepd($sql, $params);
+$rta=show_sql($sql, $params);
+//$rta = mysql_prepd($sql, $params);
 return $rta;
 }
 

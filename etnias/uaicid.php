d9e68684d93753b117fed7b644c28315e41d090c
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-14 11:48:21 -0500

    02-14 Etnias segnoreg 11:48

--- etnias/uaicid.php
+++ etnias/uaicid.php
@@ -63,11 +63,11 @@ $pag=(isset($_POST['pag-uaic_id']))? ($_POST['pag-uaic_id']-1)* $regxPag:0;
 function cmp_uaic_id(){
   $rta="";
   //$rta='';
-  $w="iden";
+  $w='uaic_id';
 	$t=['iduaic'=>'','idpeople'=>'','fecha_seg'=>'','parentesco'=>'','nombre_cui'=>'','tipo_doc'=>'','num_doc'=>'','telefono'=>'','era'=>'','eda'=>'','dnt'=>'','des_sinto'=>'','peri_cef'=>'','peri_bra'=>'','peso'=>'','talla'=>'','zcore'=>'','clasi_nut'=>'','tempe'=>'','frec_res'=>'','frec_car'=>'','satu'=>'','sales_reh'=>'','aceta'=>'','traslados_uss'=>'','educa'=>'','menor_hos'=>'','tempe2'=>'','frec_res2'=>'','frec_car2'=>'','satu2'=>'','seg_entmed'=>'','observacion'=>'','usu_creo'=>'','fecha_create'=>'','usu_update'=>'','fecha_update'=>'','estado']; 
 	$e="";
 	$key='pln';
-	$o='uaic_id';
+	$o='modini';
   $d=get_uaic_id();
   $d=($d=="")?$d=$t:$d;
   $days=fechas_app('ETNIAS');

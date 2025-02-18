db4c3806cb309a29267b5d498375fd9f267f657e
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-18 11:33:23 -0500

    02-18 Etnias uaicidseg 11:31

--- etnias/uaicseg.php
+++ etnias/uaicseg.php
@@ -87,8 +87,8 @@ function cmp_uaic_seg(){
     $c[]=new cmp('ftlc_apme','s',3,$d['ftlc_apme'],$w.' '.$o,'Tiene Ftlc U Otro Apme (Cual)','rta',null,null,false,true,'','col-2');
     $c[]=new cmp('cual','T',50,$d['cual'],$w.' '.$o,'Cual','cual',null,null,false,true,'','col-3');
     $c[]=new cmp('cita_nutri7','s',3,$d['cita_nutri7'],$w.' '.$o,'Cita Con Nutricion O Pediatria A Los 7 Dias','rta',null,null,false,true,'','col-2');
-    $c[]=new cmp('cita_nutri15','s',3,$d['cita_nutri15'],$w.' '.$o,'Cita Con Nutricion O Pediatria A Los 15 Dias','rta',null,null,false,true,'','col-35');
-    $c[]=new cmp('cita_nutri30','s',5,$d['cita_nutri30'],$w.' '.$o,'Cita Con Nutricion O Pediatria A Los 30 Dias','rta',null,null,false,true,'','col-35');
+    $c[]=new cmp('cita_nutri15','s',3,$d['cita_nutri15'],$w.' '.$o,'Cita Con Nutricion O Pediatria A Los 15 Dias','rta',null,null,false,true,'','col-25');
+    $c[]=new cmp('cita_nutri30','s',5,$d['cita_nutri30'],$w.' '.$o,'Cita Con Nutricion O Pediatria A Los 30 Dias','rta',null,null,false,true,'','col-25');
     
     $o='aspe';
     $c[]=new cmp($o,'e',null,'ASPECTOS FINALES',$w);

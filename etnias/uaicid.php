8b1cfe685e5f3293fceddc8cbd61c78bb65b6c01
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-18 16:19:02 -0500

    02-18 Etnias emberaseg 16:18

--- etnias/uaicid.php
+++ etnias/uaicid.php
@@ -173,11 +173,11 @@ return $rta;
 }
 
 function get_uaic_id(){
-  if($_REQUEST['id']==''){
+  if($_POST['id']==''){
     return "";
   }else{
     // print_r($_POST);
-    $id=divide($_REQUEST['id']);
+    $id=divide($_POST['id']);
     // print_r($id);
     $sql="SELECT fecha_seg,parentesco,nombre_cui,tipo_doc,num_doc,telefono,era,eda,dnt,des_sinto,peri_cef,peri_bra,peso,talla,zcore,clasi_nut,tempe,frec_res,frec_car,satu,sales_reh,aceta,traslados_uss,educa,menor_hos,tempe2,frec_res2,frec_car2,satu2,seg_entmed,observacion
           FROM `uaic_ide` 

6edd87b6848597efc3294180d62504768885434d
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-18 12:39:38 -0500

    02-18 Etnias uaicidseg 12:39

--- etnias/uaicseg.php
+++ etnias/uaicseg.php
@@ -126,8 +126,8 @@ function gra_uaic_seg(){
 ['type' => 's', 'value' => NULL]
       ];
 // var_dump($sql);
-      //$rta = show_sql($sql, $params);
-    $rta = mysql_prepd($sql, $params);
+      $rta = show_sql($sql, $params);
+    // $rta = mysql_prepd($sql, $params);
     }else{
    /*$sql="UPDATE hog_planconc SET cumple=?,fecha_update=?,usu_update=? WHERE idcon=?"; //  compromiso=?, equipo=?, 
     $params = [

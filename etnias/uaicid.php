e229187da2b54a8d3d6f7941e5848287ca078bd8
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-21 15:48:02 -0500

    02-21 UAICID 15:47

--- etnias/uaicid.php
+++ etnias/uaicid.php
@@ -161,7 +161,7 @@ function gra_uaic_id(){
       // $rta = show_sql($sql, $params);
      $rta = mysql_prepd($sql, $params);
     }else{
-   $sql="UPDATE TABLA SET campo1=?,campo2=?,fecha_update=?,usu_update=? WHERE IDDELATABLA=?"; //  compromiso=?, equipo=?, 
+   $sql="UPDATE uaic_ide SET fecha_seg=?,parentesco=?,nombre_cui=?,tipo_doc=?,num_doc=?,telefono=?,era=?,eda=?,dnt=?,des_sinto=?,aten_medi=?,aten_part=?,peri_cef=?,peri_bra=?,peso=?,talla=?,zcore=?,clasi_nut=?,tempe=?,frec_res=?,frec_car=?,satu=?,sales_reh=?,aceta=?,traslados_uss=?,educa=?,menor_hos=?,tempe2=?,frec_res2=?,frec_car2=?,satu2=?,seg_entmed=?,observacion=?, usu_update=?, fecha_update=DATE_SUB(NOW(), INTERVAL 5 HOUR) WHERE iduaic=?";
     $params = [
         ['type' => 's', 'value' => $_POST['CAMPO1']],
         ['type' => 's', 'value' => date("Y-m-d H:i:s")],

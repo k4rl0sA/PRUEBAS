dc8bd9d5489a086d663759fe13117efb8d555a7b
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-26 10:42:28 -0500

    02-26 UAICid 10:42

--- etnias/uaicid.php
+++ etnias/uaicid.php
@@ -81,11 +81,11 @@ function cmp_uaic_id(){
     $c[]=new cmp('zcore','t',50,$d['zcore'],$w.' '.$o,'Zcore','zcore',null,null,false,true,'','col-2');
     $c[]=new cmp('clasi_nutri','s','3',$d['clasi_nut'],$w.' '.$o,'Clasificación Nutricional','clasi_nutri',null,null,false,false,'','col-2');
     $c[]=new cmp('tempe','sd',3,$d['tempe'],$w.' '.$o,'Temperatura','tempe',null,null,false,true,'','col-2');
-    $c[]=new cmp('frec_res','sd',3,$d['frec_res'],$w.' '.$o,'Frecuencia Respiratoria','frec_res','rgxfresp',null,false,true,'','col-2');
+    $c[]=new cmp('frec_res','sd',3,$d,$w.' '.$o,'Frecuencia Respiratoria','frec_res','rgxfresp',null,false,true,'','col-2');
     $c[]=new cmp('frec_car','sd',3,$d['frec_car'],$w.' '.$o,'Frecuencia Cardiaca ','frec_car','rgxfcard',null,false,true,'','col-2');
     $c[]=new cmp('satu','sd',3,$d['satu'],$w.' '.$o,'Saturacion','satu','rgxsatu',null,false,true,'','col-2');
 
-    
+
     $o='mane';
     $c[]=new cmp($o,'e',null,'MANEJO',$w);
     $c[]=new cmp('sales_reh','s',3,$d['sales_reh'],$w.' '.$o,'Sales De Rehidratación','rta',null,null,false,true,'','col-2');

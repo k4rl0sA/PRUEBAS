822e8b55c739551e858bd67623d090905a34bfed
Jcurrea91 <ingsoportesds1@gmail.com>
2025-02-25 21:19:15 -0500

    02-25 libs a 21:19

--- etnias/embsegnoreg.php
+++ etnias/embsegnoreg.php
@@ -136,7 +136,7 @@ function cmp_segnoreg(){
   $c[]=new cmp('frec_cardiaca','sd',3,$d,$w.' '.$o,'Frecuencia Cardiaca (50 a 150 lt)','frec_cardiaca','rgxfcard',null,false,true,'','col-25');
   $c[]=new cmp('frec_respiratoria','sd',3,$d,$w.' '.$o,'Frecuencia Respiratoria (15 a 60 x min)','frec_respiratoria','rgxfresp',null,false,true,'','col-25');
   $c[]=new cmp('saturacion','sd',3,$d,$w.' '.$o,'Saturación (40 a 99 %)','saturacion','rgxsatu',null,false,true,'','col-25');
-  $c[]=new cmp('gluco','sd',3,$d,$w.' '.$o,'Glucometria (40 a 500 mg/dl)','gluco',null,null,false,true,'','col-25');
+  $c[]=new cmp('gluco','sd',3,$d,$w.' '.$o,'Glucometria (40 a 500 mg/dl)','gluco','rgxgluco',null,false,true,'','col-25');
   
   $o='antrop';
   $c[]=new cmp($o,'e',null,'VALORACIÓN ANTROPOMETRICA',$w);  

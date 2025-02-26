77932d09d31dfb255c2b5da6b3f75fdf31ec863c
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-02-25 21:12:58 -0500

    02-25 Etnias 21:12

--- etnias/embsegnoreg.php
+++ etnias/embsegnoreg.php
@@ -134,7 +134,7 @@ function cmp_segnoreg(){
   $c[]=new cmp('sistolica','sd',3,$d,$w.' '.$o,'Valor Sistolica (50 mmhg y 200 mmhg)','sistolica','rgxsisto',null,false,true,'','col-25');
   $c[]=new cmp('diastolica','sd',3,$d,$w.' '.$o,'Valor Diastolica (30 mmhg y 150 mmhg)','diastolica','rgxdiast',null,false,true,'','col-25');
   $c[]=new cmp('frec_cardiaca','sd',3,$d,$w.' '.$o,'Frecuencia Cardiaca (50 a 150 lt)','frec_cardiaca','rgxfcard',null,false,true,'','col-25');
-  $c[]=new cmp('frec_respiratoria','sd',3,$d,$w.' '.$o,'Frecuencia Respiratoria (15 a 60 x min)','frec_respiratoria',null,null,false,true,'','col-25');
+  $c[]=new cmp('frec_respiratoria','sd',3,$d,$w.' '.$o,'Frecuencia Respiratoria (15 a 60 x min)','frec_respiratoria','rgxfresp',null,false,true,'','col-25');
   $c[]=new cmp('saturacion','sd',3,$d,$w.' '.$o,'Saturación (40 a 99 %)','saturacion',null,null,false,true,'','col-25');
   $c[]=new cmp('gluco','sd',3,$d,$w.' '.$o,'Glucometria (40 a 500 mg/dl)','gluco',null,null,false,true,'','col-25');
   

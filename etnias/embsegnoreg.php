2b402d89294c8dca81cf411aefe692b2935ad498
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-02-26 19:27:34 -0500

    02-26 Etnias 19:27

--- etnias/embsegnoreg.php
+++ etnias/embsegnoreg.php
@@ -153,12 +153,6 @@ function cmp_segnoreg(){
   if($person['ano']<5){
     $c[]=new cmp('zcore','t',50,$d,$w.' '.$o,'Zcore','zcore',null,null,false,true,'','col-35');
   }
-  $c[]=new cmp('peri_cefalico','sd',2,$d,$w.' '.$o,'Perimetro Cefalico (Cm)','peri_cefalico',null,null,false,true,'','col-25');
-  $c[]=new cmp('peri_braqueal','sd',2,$d,$w.' '.$o,'Perimetro Braquial  (Cm)','peri_braqueal',null,null,false,true,'','col-25');
-  $c[]=new cmp('peso','sd',5,$d,$w.' '.$o,'Peso (Kg)','peso','rgxpeso',null,false,true,'','col-25',"calImc('peso','talla','imc');");
-  $c[]=new cmp('talla','sd',4,$d,$w.' '.$o,'Talla (Cm)','talla','rgxtalla',null,false,true,'','col-25',"calImc('peso','talla','imc');");
-  $c[]=new cmp('imc','sd',5,$d,$w.' '.$o,'Imc','imc',null,null,false,false,'','col-3');
-  $c[]=new cmp('zcore','t',50,$d,$w.' '.$o,'Zcore','zcore',null,null,false,false,'','col-35');
   $c[]=new cmp('clasi_nutri','s',3,$d,$w.' '.$o,'Clasificación Nutricional','clasi_nutri',null,null,false,true,'','col-35');
   
   $o='aspfin';

cd88369ad20c482deb3d0b8d39410146c6f16d4a
Jcurrea91 <ingsoportesds1@gmail.com>
2025-03-01 07:55:21 -0500

    03-01 uaicseg 07:55

--- etnias/uaicseg.php
+++ etnias/uaicseg.php
@@ -63,9 +63,9 @@ left join usuarios u ON us.usu_creo = u.id_usuario
 
 function cmp_uaic_seg(){
   $rta="<div class='encabezado'>TABLA SEGUIMIENTOS</div><div class='contenido' id='uaic_seg-lis'>".lis_uaic_seg()."</div></div>";
-  $w='uaic_seg';
+  $w='modini';
 	$t=['iduaicseg'=>'','idpeople'=>'','fecha_seg'=>'','segui'=>'','estado_seg'=>'','motivo_seg'=>'','at_medi'=>'','at_part'=>'','peso'=>'','talla'=>'','zcore'=>'','clasi_nutri'=>'','ftlc_apme'=>'','cual'=>'','cita_nutri7'=>'','cita_nutri15'=>'','cita_nutri30'=>'','observaciones'=>''];
-	$o='modini';
+	$o='uaic_seg';
   $ob='Ob';
   $no='nO';
   $bl='bL';

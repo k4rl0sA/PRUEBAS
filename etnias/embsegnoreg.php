6770819b0d83d13a6176be508863a39a4537921a
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-03-01 17:47:55 -0500

    03-01 etnias 17:47

--- etnias/embsegnoreg.php
+++ etnias/embsegnoreg.php
@@ -344,7 +344,7 @@ function get_segnoreg(){
     $sql="SELECT null,null,idsegnoreg,fecha_seg, segui, estado_seg, motivo, prioridad, gestaciones, partos, abortos, cesareas, vivos, muertos, fum, edad_gest, resul_gest, peso_nacer, asist_controles, exa_labo, cons_micronutri, esq_vacu, signos_alarma1, diag_sifigest, adhe_tto, diag_sificong, seg_partera, seg_med_ancestral1, diag_cronico, cual, tto_enf, ctrl_cronico, signos_alarma2, seg_med_ancestral2, doc_madre, ctrl_cyd, lactancia_mat, esq_vacunacion, sig_alarma_seg, seg_med_ancestral3, at_med, at_partera, sistolica, diastolica, frec_cardiaca, frec_respiratoria, saturacion, gluco, peri_cefalico, peri_braqueal, peso, talla, imc, zcore, clasi_nutri, ser_remigesti, observaciones
           FROM `emb_segreg` 
           WHERE idsegnoreg='{$id[0]}'";
-          return json_encode($sql);
+          // return json_encode($sql);
           $info=datos_mysql($sql);
           return json_encode($info['responseResult'][0]);
         } 

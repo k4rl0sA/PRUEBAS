0268c38b1acdadd9a754ad3c3a547ff2e58423f3
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-03-03 10:30:33 -0500

    03-3 etnias 10:30

--- etnias/embsegnoreg.php
+++ etnias/embsegnoreg.php
@@ -400,7 +400,7 @@ function opc_clasi_nutri($id=''){
         // var_dump($a);
 		if ($a=='segnoreg' && $b=='acciones'){
 			$rta="<nav class='menu right'>";
-      $rta.="<li class='icono editar' title='Editar' id='".$c['ACCIONES']."' Onclick=\"setTimeout(getData,500,'segnoreg',event,this,['fecha_seg','segui','estado_seg','prioridad'],'../etnias/embsegnoreg.php');setTimeout(enabPrioEtn,700);enbValue('id','segnoreg','".$c['ACCIONES']."');enaFie(document.getElementById('observaciones'),false);\"></li>";
+      $rta.="<li class='icono editar' title='Editar' id='".$c['ACCIONES']."' Onclick=\"setTimeout(getData,500,'segnoreg',event,this,['fecha_seg','segui','estado_seg','prioridad','aten_med'],'../etnias/embsegnoreg.php');setTimeout(enabPrioEtn,700);enbValue('id','segnoreg','".$c['ACCIONES']."');enaFie(document.getElementById('observaciones'),false);\"></li>";
 			}
 		return $rta;
 	}

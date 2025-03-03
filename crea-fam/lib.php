8ec553e6083612e42db5857ad5e9a496ffbdcc88
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-03-03 11:15:59 -0500

    03-3 etnias 11:15

--- crea-fam/lib.php
+++ crea-fam/lib.php
@@ -966,11 +966,11 @@ function formato_dato($a,$b,$c,$d){
 			if (ember($c['ACCIONES'])) {
 				$rta .= acceso('ember') ? "<li title='Identificación Embera' onclick=\"mostrar('emb_Id','pro',event,'','../etnias/embid.php',7,'Identificación Embera');Color('datos-lis');\"><i class=' fa-solid fa-users-line ico' id='{$c['ACCIONES']}'></i></li>":"";
 				$rta .= acceso('ember') ? "<li title='Embera Seguimiento Rutinario' onclick=\"mostrar('segnoreg','pro',event,'','../etnias/embsegnoreg.php',7,'Seguimiento No Regular');Color('datos-lis');\"><i class=' fa-solid fa-list-check ico' id='{$c['ACCIONES']}'></i></li>":"";
-				$rta .= acceso('ember') ? "<li title='Embera Seguimientos' onclick=\"mostrar('seguim','pro',event,'','../etnias/embsegui.php',7,'Seguimientos');Color('datos-lis');\"><i class=' fa-solid fa-person-circle-question ico' id='{$c['ACCIONES']}'></i></li>":"";
+				$rta .= acceso('ember') ? "<li title='Embera Seguimientos' onclick=\"mostrar('seguim','pro',event,'','../etnias/embsegui.php',7,'Seguimientos');Color('datos-lis');\"><i class=' fa-solid fa-rectangle-list ico' id='{$c['ACCIONES']}'></i></li>":"";
 			}
 			if (uaic($c['ACCIONES'])) {
 				$rta .= acceso('uaic') ? "<li title='Identificación UAIC' onclick=\"mostrar('uaic_id','pro',event,'','../etnias/uaicid.php',7,'Identificación');Color('datos-lis');\"><i class=' fa-solid fa-addres-book ico' id='{$c['ACCIONES']}'></i></li>":"";
-				$rta .= acceso('uaic') ? "<li title='Seguimientos UAIC' onclick=\"mostrar('uaic_seg','pro',event,'','../etnias/uaicseg.php',7,'Seguimientos');Color('datos-lis');\"><i class=' fa-solid fa-person-circle-question ico' id='{$c['ACCIONES']}'></i></li>":"";
+				$rta .= acceso('uaic') ? "<li title='Seguimientos UAIC' onclick=\"mostrar('uaic_seg','pro',event,'','../etnias/uaicseg.php',7,'Seguimientos');Color('datos-lis');\"><i class=' fa-solid fa-list-ol ico' id='{$c['ACCIONES']}'></i></li>":"";
 			}
 	}
 		if($a=='atencion' && $b=='acciones'){

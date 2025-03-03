ef723c307e875f9d0ef98cd50675f4ced6e19824
Jcurrea91 <ingsoportesds1@gmail.com>
2025-03-03 13:44:13 -0500

    03-03 uaicid 13:43

--- etnias/embsegui.php
+++ etnias/embsegui.php
@@ -1,4 +1,4 @@
-<?php
+  <?php
 ini_set('display_errors','1');
 require_once "../libs/gestion.php";
 if ($_POST['a']!='opc') $perf=perfil($_POST['tb']);
@@ -260,7 +260,7 @@ function formato_dato($a,$b,$c,$d){
   // var_dump($a);
   if ($a=='seguim' && $b=='acciones'){
     $rta="<nav class='menu right'>";
-    $rta.="<li class='icono editar' title='Editar' id='".$c['ACCIONES']."' Onclick=\"setTimeout(getData,500,'seguim',event,this,[],'../etnias/embsegui.php');enbValue('idseg','seguim','".$c['ACCIONES']."');enaFie(document.getElementById('observaciones'),false);\"></li>";
+    $rta.="<li class='icono editar' title='Editar' id='".$c['ACCIONES']."' Onclick=\"setTimeout(getData,500,'seguim',event,this,['fecha_seg'],'../etnias/embsegui.php');enbValue('idseg','seguim','".$c['ACCIONES']."');enaFie(document.getElementById('observaciones'),false);\"></li>";
     }
 		return $rta;
 }

14dd2b4566c06b671775da02ad1095cc3e697e2d
Jcurrea91 <ingsoportesds1@gmail.com>
2025-03-04 12:30:40 -0500

    03-04 uaicid 12:309

--- etnias/uaicid.php
+++ etnias/uaicid.php
@@ -198,6 +198,7 @@ function get_persona(){
 function gra_uaic_id(){
 	$id = divide($_POST['iduaic']);
   $usu = $_SESSION['us_sds'];
+  return validFecha('ETNIAS',$_POST['fecha_seg']);
     if(COUNT($id)==2){
       $equ=datos_mysql("select equipo from usuarios where id_usuario='".$_SESSION['us_sds']."'");
       $bina = isset($_POST['fequi'])?(is_array($_POST['fequi'])?implode("-", $_POST['fequi']):implode("-",array_map('trim',explode(",",str_replace("'","",$_POST['fequi']))))):'';

13fc464cc0a53ebacb521978bd9cc4c61029c327
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-03-06 10:43:11 -0500

    03-06 ETNIAS 10:43

--- etnias/embsegnoreg.php
+++ etnias/embsegnoreg.php
@@ -140,7 +140,6 @@ function cmp_segnoreg(){
   
   $o='antrop';
   $c[]=new cmp($o,'e',null,'VALORACIÓN ANTROPOMETRICA',$w);
-  var_dump($p['ano'],'=año - ',$p['mes'],'=mes');
   $men5 = ($p['ano']<5) ? true : false ;
   $c[]=new cmp('peri_cefalico','sd',4,$d,$w.' '.' men5 ','Perimetro Cefalico (Cm)','peri_cefalico',null,null,true,$men5,'','col-25');
 

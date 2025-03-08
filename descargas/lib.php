c9fe1eaecaf494e1368cd6a5a1c108b47628afd8
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-03-08 09:17:59 -0500

    03-06 AGENDAMIENTO 09:17

--- descargas/lib.php
+++ descargas/lib.php
@@ -217,7 +217,7 @@ echo "data: " . json_encode(['status' => 'completed', 'filename' => $filename]) 
 ob_flush();
 flush();
 
-// Enviar el archivo al cliente para descargar
+/* // Enviar el archivo al cliente para descargar
 if (file_exists($filename)) {
     // Configurar las cabeceras para la descarga
     header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
@@ -230,6 +230,6 @@ if (file_exists($filename)) {
     exit; // Terminar la ejecución del script
 } else {
     echo "data: " . json_encode(['status' => 'error', 'message' => 'El archivo no existe']) . "\n\n";
-}
+} */
 
 session_write_close(); // Liberar la sesión

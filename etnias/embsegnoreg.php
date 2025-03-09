2a4524e46affe9996e6847b068cb0b2db693cc05
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-03-08 21:37:34 -0500

    03-08 AjusteEtnias  21:37

--- etnias/embsegnoreg.php
+++ etnias/embsegnoreg.php
@@ -128,17 +128,18 @@ function cmp_segnoreg(){
   
   
   $o='signosV';
+  $p='sigVit';
   $c[]=new cmp($o,'e',null,'SIGNOS VITALES',$w);
   $c[]=new cmp('aten_med','s',3,$d,$w.' '.$o,'Recibio Atención por Medico Ancestral','rta',null,null,true,true,'','col-25');
   $c[]=new cmp('aten_par','s',3,$d,$w.' '.$o,'Recibio Atención por Partera','rta',null,null,true,true,'','col-25');
   var_dump($p['mes']);
   $men = ($p['mes']<216) ? false : true ;
-  $c[]=new cmp('sistolica','sd',3,$d,$w.' '.$o,'Valor Sistolica (50 mmhg y 200 mmhg)','sistolica','rgxsisto',null,true,$men,'','col-25');
-  $c[]=new cmp('diastolica','sd',3,$d,$w.' '.$o,'Valor Diastolica (30 mmhg y 150 mmhg)','diastolica','rgxdiast',null,true,$men,'','col-25');
+  $c[]=new cmp('sistolica','sd',3,$d,$w.' '.$p,'Valor Sistolica (50 mmhg y 200 mmhg)','sistolica','rgxsisto',null,true,$men,'','col-25');
+  $c[]=new cmp('diastolica','sd',3,$d,$w.' '.$p,'Valor Diastolica (30 mmhg y 150 mmhg)','diastolica','rgxdiast',null,true,$men,'','col-25');
   $c[]=new cmp('frec_cardiaca','sd',3,$d,$w.' '.$o,'Frecuencia Cardiaca (50 a 150 lt)','frec_cardiaca','rgxfcard',null,true,true,'','col-25');
   $c[]=new cmp('frec_respiratoria','sd',3,$d,$w.' '.$o,'Frecuencia Respiratoria (15 a 60 x min)','frec_respiratoria','rgxfresp',null,true,true,'','col-25');
   $c[]=new cmp('saturacion','sd',3,$d,$w.' '.$o,'Saturación (40 a 99 %)','saturacion','rgxsatu',null,true,true,'','col-25');
-  $c[]=new cmp('gluco','sd',3,$d,$w.' '.$o,'Glucometria (40 a 500 mg/dl)','gluco','rgxgluco',null,true,$men,'','col-25');
+  $c[]=new cmp('gluco','sd',3,$d,$w.' '.$p,'Glucometria (40 a 500 mg/dl)','gluco','rgxgluco',null,true,$men,'','col-25');
   
   $o='antrop';
   $c[]=new cmp($o,'e',null,'VALORACIÓN ANTROPOMETRICA',$w);

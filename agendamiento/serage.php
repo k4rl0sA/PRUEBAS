5fe7f9168f2aa0bc311c46310a0015bd40a8dad7
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-03-10 13:02:05 -0500

    03-10 Agendamiento 13:02

--- agendamiento/serage.php
+++ agendamiento/serage.php
@@ -32,6 +32,7 @@ function focus_servagen(){
  function cap_menus($a,$b='cap',$con='con') {
   $rta = "";
   $acc=rol($a);
+  var_dump($acc);
   if ($a=='servagen' && isset($acc['crear']) && $acc['crear']=='SI') {  
    $rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>"; //~ openModal();
     }

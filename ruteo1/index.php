a8dffcf4bec75f2130d2911848247166b56c17b9
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-03-13 15:48:51 -0500

    03-13 Ruteo 15:48

--- ruteo1/index.php
+++ ruteo1/index.php
@@ -136,13 +136,14 @@ $localidad=opc_sql("select idcatadeta,descripcion from catadeta where idcatalogo
 			<?php echo $grupos; ?>
 		</select>
 	</div>
-		<div class="campo"><div>Estados</div>
+		
+	<div class="campo"><div>Codigo del Predio</div><input class="captura" type="number" size=10 id="fpred" name="fpred" OnChange="actualizar();"></div>
+	<div class="campo"><div>Documento Usuario</div><input class="captura" type="number" size=20 id="fusu" name="fusu" OnChange="actualizar();"></div>
+	<div class="campo"><div>Estados</div>
 		<select class="captura" id="festados" name="festados" OnChange="actualizar();">
 			<?php echo $localidad; ?>
 		</select>
 	</div>
-	<div class="campo"><div>Codigo del Predio</div><input class="captura" type="number" size=10 id="fpred" name="fpred" OnChange="actualizar();"></div>
-	<div class="campo"><div>Documento Usuario</div><input class="captura" type="number" size=20 id="fusu" name="fusu" OnChange="actualizar();"></div>
 </div>
 <div class='col-8 panel' id='<?php echo $mod; ?>'>
       <div class='titulo' > RUTEO

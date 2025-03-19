16c5852bd1e0ae8ed3836fe052a9b21916365674
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-03-19 15:49:27 -0500

    03-19 Ruteo 15:4

--- ruteo1/lib.php
+++ ruteo1/lib.php
@@ -137,8 +137,8 @@ $o='gesefc';
  $c[]=new cmp('predio_num_v','n','3',$d['predio'],$w.' '.$o,'Predio de Num (3)','predio_num_v',null,null,true,$u,'','col-25');
 
  $o='gesefc';
- $c[]=new cmp($o,'e',null,'PROCESO GESTIÓN',$w);
- $c[]=new cmp('docu_confirm','nu','999999999999999999',$d['docu_confirm'],$w.' pRe '.$o,'Fecha de Gestion','docu_confirm',null,null,true,$x,'','col-2','validDate(this,-2,0);');
+ $c[]=new cmp($o,'e',null,'PROCESO DE ASIGNACIÓN',$w);
+ $c[]=new cmp('docu_confirm','nu','999999999999999999',$d['docu_confirm'],$w.' pRe '.$o,'Documento Confirmado','docu_confirm',null,null,true,$x,'','col-2','validDate(this,-2,0);');
  $c[]=new cmp('perfil','s','90','',$w.' dir '.$o,'Perfil','perfil',null,null,false,$u,'','col-25');
  $c[]=new cmp('nombre','s','6',$d['doc_asignado'],$w.' dir '.$o,'Asignado A','doc_asignado',null,null,false,$u,'','col-25');
  

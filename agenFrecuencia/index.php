670c2be8fb059894fa91c512c998e55ca8275df1
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-03-19 11:16:56 -0500

    03-19 Frecuencia 11:16

--- agenFrecuencia/index.php
+++ agenFrecuencia/index.php
@@ -306,9 +306,6 @@ $digitadores=opc_sql("SELECT `id_usuario`,nombre FROM `usuarios` WHERE`perfil`='
       <div class='titulo' >FRECUENCIA DE USO
 		<nav class='menu left' >
 			<li class='icono listado' title='Ver Listado' onclick="desplegar(mod+'-lis');" ></li>
-			<?php if ($info['responseResult'][0]['perfil']=='ADM' || $info['responseResult'][0]['perfil']=='TEC' ){
-				echo "<li class='icono exportar'      title='Exportar CSV'    Onclick=\"csv(mod);\"></li>";	
-			}?>
 			<li class='icono actualizar'    title='Actualizar'      Onclick="actualizar();">
 			<li class='icono filtros'    title='Filtros'      Onclick="showFil(mod);">
 			<li class='icono crear'       title='Crear frecuencia de Uso'     Onclick="mostrar(mod,'pro');hideMotiv();"></li> <!--hideMotiv();-->

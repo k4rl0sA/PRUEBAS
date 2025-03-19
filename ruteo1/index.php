ba38ca01805226982254c1d9d50b641b3133f940
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-03-19 14:02:26 -0500

    03-19 Frecuencia 14:02

--- ruteo1/index.php
+++ ruteo1/index.php
@@ -132,23 +132,9 @@ $prioridad=opc_sql("select idcatadeta,descripcion from catadeta where idcatalogo
 			<?php /* echo $fuentes; */ ?>
 		</select>
 	</div> -->
-	<div class="campo"><div>Grupo Priorizado</div>
-		<select class="captura" id="fgrupo" name="fgrupo" OnChange="actualizar();">
-			<?php echo $grupos; ?>
-		</select>
-	</div>
-	<div class="campo"><div>Prioridad</div>
-		<select class="captura" id="fgrupo" name="fgrupo" OnChange="actualizar();">
-			<?php echo $prioridad; ?>
-		</select>
-	</div>
+	
 	<div class="campo"><div>Codigo del Predio</div><input class="captura" type="number" size=10 id="fpred" name="fpred" OnChange="actualizar();"></div>
 	<div class="campo"><div>Documento Usuario</div><input class="captura" type="number" size=20 id="fusu" name="fusu" OnChange="actualizar();"></div>
-	<div class="campo"><div>Estado</div>
-		<select class="captura" id="festados" name="festados" OnChange="actualizar();">
-			<?php echo $localidad; ?>
-		</select>
-	</div>
 </div>
 <div class='col-8 panel' id='<?php echo $mod; ?>'>
       <div class='titulo' > RUTEO

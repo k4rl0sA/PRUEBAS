56aaa84ea4d3188b20613ec19319855641dc309b
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-03-24 13:33:42 -0500

    03-24 13:33

--- ruteo1/clasifica.php
+++ ruteo1/clasifica.php
@@ -90,6 +90,9 @@ function opc_clasificacion($id=''){
 function opc_pre_clasif($id=''){
 	return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=191 and estado='A' ORDER BY 1",$id);
 }
+function opc_perfil($id=''){
+  return opc_sql('SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=218 and estado="A" ORDER BY 1',$id);
+  }
 function formato_dato($a,$b,$c,$d){
  $b=strtolower($b);
  $rta=$c[$d];

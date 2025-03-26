5baf06c1c38b1bd4efe7aeee5ab327c6f6e5dd01
Jcurrea91 <ingsoportesds1@gmail.com>
2025-03-26 12:12:01 -0500

    03-26 Etnias 12:11

--- etnias/embsegnoreg.php
+++ etnias/embsegnoreg.php
@@ -101,9 +101,9 @@ function cmp_segnoreg(){
   $c[]=new cmp('cons_micronutri','s',3,$d,$w.' PrG '.$bl.' '.$no,'¿Consume Micronutrientes?','rta',null,null,true,true,'','col-4');
   $c[]=new cmp('esq_vacu','s',3,$d,$w.' PrG '.$bl.' '.$no,'¿Tiene Esquema De Vacunacion Completo Para La Edad Gestacional?','rta',null,null,true,true,'','col-3');
   $c[]=new cmp('signos_alarma1','s',3,$d,$w.' '.$bl.' '.$ge.' '.$o,'¿Presenta Signos De Alarma?','rta',null,null,true,true,'','col-2');
-  $c[]=new cmp('diag_sifigest','s',3,$d,$w.' '.$bl.' '.$ge.' '.$o,'¿Diagnosticada Con Sifilis Gestacional?','rta',null,null,true,true,'','col-3');
-  $c[]=new cmp('adhe_tto','s',3,$d,$w.' '.$bl.' '.$ge.' '.$o,'¿Adherencia A Tratamiento?','rta',null,null,true,true,'','col-2');
-  $c[]=new cmp('diag_sificong','s',3,$d,$w.' '.$bl.' '.$ge.' '.$o,'¿Sifilis Congenita?','rta',null,null,true,true,'','col-3');
+  $c[]=new cmp('diag_sifigest','s',3,$d,$w.' '.$bl.' '.$ge.' '.$o,'¿Diagnosticada Con Sifilis Gestacional?','rta',null,null,true,true,'','col-3', 'enabEmbSif();');
+  $c[]=new cmp('adhe_tto','s',3,$d,$w.' '.$bl.' SiF '.$no,'¿Adherencia A Tratamiento?','rta',null,null,true,true,'','col-2');
+  $c[]=new cmp('diag_sificong','s',3,$d,$w.' '.$bl.' Sif '.$no,'¿Sifilis Congenita?','rta',null,null,true,true,'','col-3');
   $c[]=new cmp('seg_partera','s',3,$d,$w.' '.$bl.' '.$ge.' '.$o,'¿Le Ha Realizado Seguimiento Partera?','rta',null,null,true,true,'','col-3');
   $c[]=new cmp('seg_med_ancestral1','s',3,$d,$w.' '.$bl.' '.$ge.' '.$o,'¿Le Ha Realizado Seguimiento El Médico Ancestral?','rta',null,null,true,true,'','col-4');
 

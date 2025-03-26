80c156ff85042a28535ace8d6b4dfe6073895eb7
0scarC1 <ingsoportesds2@gmail.com>
2025-03-26 09:55:15 -0500

    03-26  Ruteo 09:55

--- ruteo1/lib.php
+++ ruteo1/lib.php
@@ -199,7 +199,7 @@ function opc_gestion($id=''){
 	return opc_sql("SELECT `idcatadeta`, descripcion FROM `catadeta` WHERE idcatalogo=222 AND estado='A' ORDER BY 1", $id);
 }
 function opc_estado_agenda($id=''){
-	return opc_sql("SELECT `idcatadeta`, descripcion FROM `catadeta` WHERE idcatalogo=1 AND estado='A' ORDER BY 1", $id);
+	return opc_sql("SELECT `idcatadeta`, descripcion FROM `catadeta` WHERE idcatalogo=271 AND estado='A' ORDER BY 1", $id);
 }
 function opc_idgeo($a){
 	$id=divide($a);
@@ -309,7 +309,7 @@ function opc_barrio($id=''){
 	return opc_sql("SELECT `idcatadeta`,CONCAT(idcatadeta,'-',descripcion) FROM `catadeta` WHERE idcatalogo=20 and estado='A' ORDER BY 1",$id);
 }
 function opc_estado_g($id=''){
-	return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=35 and estado='A' ORDER BY 1",$id);
+	return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=270 and estado='A' ORDER BY 1",$id);
 }
 function opc_motivo_estado($id=''){
 	return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=36 and estado='A' ORDER BY 1",$id);

75c9d537be7b66af9a842883c674bd38b086149d
Jcurrea91 <ingsoportesds1@gmail.com>
2025-03-28 06:43:41 -0500

    3-28 ruteo1 06:43

--- ruteo1/lib.php
+++ ruteo1/lib.php
@@ -356,7 +356,7 @@ function get_gest(){
 }
 
 function gra_rute(){
-	$id=divide($_POST['id_ruteo'] ?? '');
+	$id=divide($_POST['id'] ?? '');
 	$usu = $_SESSION['us_sds'];
 		// $equ=datos_mysql("select equipo from usuarios where id_usuario=".$_SESSION['us_sds']);
 		$bina = isset($_POST['fequi'])?(is_array($_POST['fequi'])?implode("-", $_POST['fequi']):implode("-",array_map('trim',explode(",",str_replace("'","",$_POST['fequi']))))):'';

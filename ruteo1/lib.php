5aeb6d3c5733e06f4b9047be262d87e571adbecf
Jcurrea91 <ingsoportesds1@gmail.com>
2025-03-28 10:22:39 -0500

    03-28 libs crea fam 10:21

--- ruteo1/lib.php
+++ ruteo1/lib.php
@@ -359,7 +359,7 @@ function gra_rute(){
 	$id=divide($_POST['id'] ?? '');
 	$usu = $_SESSION['us_sds'];
 		// $equ=datos_mysql("select equipo from usuarios where id_usuario=".$_SESSION['us_sds']);
-		$bina = isset($_POST['fequi'])?(is_array($_POST['fequi'])?implode("-", $_POST['fequi']):implode("-",array_map('trim',explode(",",str_replace("'","",$_POST['fequi']))))):'';
+		// $bina = isset($_POST['fequi'])?(is_array($_POST['fequi'])?implode("-", $_POST['fequi']):implode("-",array_map('trim',explode(",",str_replace("'","",$_POST['fequi']))))):'';
 		$sql = "INSERT INTO eac_ruteo_ges VALUES(null,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,DATE_SUB(NOW(),INTERVAL 5 HOUR),?,?,'A')";
 		$params = [
 	['type' => 'i', 'value' => $id[0]],

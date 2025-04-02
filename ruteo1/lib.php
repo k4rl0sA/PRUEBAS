06fa86cafbd389698b14ba86252a51d6d6a27665
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-02 09:19:26 -0500

    04-02 Ruteo 09:19

--- ruteo1/lib.php
+++ ruteo1/lib.php
@@ -470,6 +470,7 @@ function gra_rute(){
 		];
 		// var_dump($rta);
 	//  $rta = show_sql($sql, $params);
+	var_dump($_POST);
 if($_POST['fecha_gestion']!='' && $_POST['usuario_gest']!=''){
 	return "NO inserta";
 }else{

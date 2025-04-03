88e9316b0c47300e87a195ddce9d689cc9b58156
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-03 10:21:40 -0500

    03-30 Agendamiento 10:21

--- agenFrecuencia/lib.php
+++ agenFrecuencia/lib.php
@@ -137,12 +137,13 @@ function cmp_frecuenciauso(){
  if ($d=="") {$d=$t;}
  $u=($d['id_persona']=='')?true:false;
  $o='percit';
+ $key='find';
  $rta=" <span class='mensaje' id='".$w."-msj' ></span>";
  $c[]=new cmp($o,'e',null,'FRECUENCIA DE USO DE USUARIOS',$w);
  $c[]=new cmp('key','h',50,$_POST['id'],$w.' '.$o,'',0,'','','',false,'','col-4');
   //~ $c[]=new cmp('ipe','h',10,$_POST['id'],$w,'','idp',null,'','',''); 
- $c[]=new cmp('idp','n',18,$d['id_persona'],$w.' '.$o,'N° Identificación',0,'rgxdfnum','#################',true,$u,'','col-4');
-  $c[]=new cmp('tdo','s',3,$d['tipo_doc'],$w.' '.$o,'Tipo Documento','tipo_doc',null,null,true,$u,'','col-3','getPerson');
+ $c[]=new cmp('idp','n',18,$d['id_persona'],$w.' '.$key.' '.$o,'N° Identificación',0,'rgxdfnum','#################',true,$u,'','col-4');
+  $c[]=new cmp('tdo','s',3,$d['tipo_doc'],$w.' '.$key.' '.$o,'Tipo Documento','tipo_doc',null,null,true,$u,'','col-3',"getDatForm('find','persona','percit');");
  $c[]=new cmp('no1','t',20,$d['nombre1'],$w.' '.$o,'Primer Nombre','nombre1',null,null,false,false,'','col-3');
  $c[]=new cmp('no2','t',20,$d['nombre2'],$w.' '.$o,'Segundo Nombre','nombre2',null,null,false,false,'','col-4');
  $c[]=new cmp('ap1','t',20,$d['apellido1'],$w.' '.$o,'Primer Apellido','apellido1',null,null,false,false,'','col-3');

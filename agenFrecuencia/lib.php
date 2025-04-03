14f973886af4f7fa12b6c0539955f7e1ec03f3ff
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-03 11:53:10 -0500

    04-03 Agendamiento 11:53

--- agenFrecuencia/lib.php
+++ agenFrecuencia/lib.php
@@ -143,7 +143,7 @@ function lis_citasUsuario(){
 	$sql="SELECT idfrecuencia,p.idpersona,p.tipo_doc,FN_CATALOGODESC(275,tipo_cita) `tipo de cita`, 
 	`observaciones` 
 	FROM `frecuenciauso` f left join person p ON f.idpeople=p.idpeople 
-	WHERE `id_people`='{$id[0]}' AND `realizada`='NO'";
+	WHERE f.id_people='{$id[0]}' AND `realizada`='NO'";
 //~ echo $sql;
 	$datos=datos_mysql($sql);
 return panel_content($datos["responseResult"],"citasUsuario",5);

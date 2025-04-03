b3efe8f97fc4425ab91a75297c4d0d94581bba0c
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-03 14:33:22 -0500

    04-03 Agenda 14:33

--- agenFrecuencia/lib.php
+++ agenFrecuencia/lib.php
@@ -139,7 +139,7 @@ function cmp_frecuenciauso(){
 function lis_citasUsuario(){
 	 $id=divide($_POST['id']);
 	$sql="SELECT idfrecuencia,p.idpersona,p.tipo_doc,FN_CATALOGODESC(275,tipo_cita) `tipo de cita`, 
-	FN_CATALOGODESC(278,`observaciones`) observaciones 
+	FN_CATALOGODESC(278,`observaciones`) observaciones,motivo 
 	FROM `frecuenciauso` f left join person p ON f.idpeople=p.idpeople 
 	WHERE f.idfrecuencia='{$id[0]}' AND `realizada`='NO'";
 // echo $sql;

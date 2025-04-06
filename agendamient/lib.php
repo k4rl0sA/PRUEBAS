fa4296160fe6d1d997fed3b72847f7b69e7eee60
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-06 18:02:43 -0500

    04-06 Agendamiento 18:02

--- agendamient/lib.php
+++ agendamient/lib.php
@@ -90,7 +90,8 @@ $id=divide($_POST['id']);
   $sql="SELECT T2.idpersona,T2.tipo_doc,T2.nombre1 nombre1,T2.nombre2 nombre2,T2.apellido1 apellido1,T2.apellido2 apellido2,
   T2.fecha_nacimiento fecha_nacimiento, concat('Años= ',timestampdiff(YEAR,T2.fecha_nacimiento,curdate()),
    ' Meses= ',MONTH(CURDATE()) - MONTH(T2.fecha_nacimiento) + 12 * IF( MONTH(CURDATE()) < MONTH(T2.fecha_nacimiento),1, IF(MONTH(CURDATE())=MONTH(T2.fecha_nacimiento),IF (DAY(CURDATE()) < DAY(T2.fecha_nacimiento),1,0),0)) - IF(MONTH(CURDATE())<>MONTH(T2.fecha_nacimiento), (DAY(CURDATE()) < DAY(T2.fecha_nacimiento)), IF (DAY(CURDATE()) < DAY(T2.fecha_nacimiento),1,0 ) ), ' Días= ',DAY(CURDATE())-DAY(T2.fecha_nacimiento)+30*(DAY(CURDATE()) < DAY(T2.fecha_nacimiento))) edad,
-   T2.genero genero, T2.eapb eapb,T3.telefono1 telefono1,T3.telefono2 telefono2,tipo_consulta,punto_atencion,tipo_cita,fecha_cita,hora_cita,nombre_atendio,observac_cita 
+   T2.genero genero, T2.eapb eapb,telefono1,telefono2,tipo_consulta,
+   punto_atencion,tipo_cita,fecha_cita,hora_cita,nombre_atendio,observac_cita 
 			FROM agendamiento T1 
 			left join person T2 ON T1.idpeople=T2.idpeople
 	WHERE T1.idagendamiento='".$id[0]."'";//AND T1.tipodoc=upper('".$id[2]."') AND fecha_cita='".$id[3]."' AND hora_cita='".$id[4]."'

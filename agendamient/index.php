689bfb2e1f22305bb5d15d5b4b59949c4fef2d10
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-06 15:55:14 -0500

    04-06 Agendamiento 15:55

--- agendamient/index.php
+++ agendamient/index.php
@@ -1,23 +1,20 @@
 <?php
-session_start();
 ini_set('display_errors','1');
 include $_SERVER['DOCUMENT_ROOT'].'/libs/nav.php';
-?>
+?>	
 <head>
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Agendamiento || Sigrev</title>
-<link href="https://fonts.googleapis.com/css2?family=Economica&family=Spicy+Rice&family=Trade+Winds&display=swap" rel="stylesheet">
-<link href="../libs/css/s.css" rel="stylesheet">
-<script src="../libs/js/c18082020.js"></script>
+<title>Agendamiento || <?php echo $APP; ?></title>
+<link href="../libs/css/stylePop.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Cabin+Sketch&family=Chicle&family=Merienda&family=Rancho&family=Boogaloo&display=swap" rel="stylesheet">
+<script src="../libs/js/a.js?v=1.5"></script>
 <script src="../libs/js/d.js"></script>
+<script src="../libs/js/popup.js"></script>
 <script>
 var mod='agendamiento';	
 var ruta_app='lib.php';
 var hoy = new Date().toISOString().slice(0,10);
-function csv(b){
-		var myWindow = window.open("../libs/gestion.php?a=exportar&b="+b,"Descargar archivo");
-}
 
 document.onkeyup=function(ev) {
  ev=ev||window.event;
@@ -29,165 +26,6 @@ document.onkeyup=function(ev) {
 function actualizar(){
 	act_lista(mod);
 }
-
-
-function changeSelect(a,b,c){		
-		const x = document.getElementById(a);
-		const y = document.getElementById(b);
-		const z = document.getElementById(c);
-		if(a!=null && b!=null && c!=null){
-		z.innerHTML="";
-		loader=document.getElementById('loader');
-		if (loader != undefined) loader.style.display = 'block';
-		if (window.XMLHttpRequest)
-			xmlhttp = new XMLHttpRequest();
-		else
-			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-			
-			xmlhttp.onreadystatechange = function () {
-			if ((xmlhttp.readyState == 4) && (xmlhttp.status == 200)){
-				data =JSON.parse(xmlhttp.responseText);
-				console.log(data);
-			}}
-				xmlhttp.open("POST", ruta_app,false);
-				xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-				xmlhttp.send('a=opc&tb='+a+b+'&id='+x.value+'_'+y.value);
-			//~ //	var rta =data;
-				var data=Object.values(data);
-				var opt = document.createElement('option');
-				opt.text ='SELECCIONE';
-				opt.classList.add('alerta');
-				opt.value='';
-				z.add(opt);
-				for(i=0;i<data.length;i++){
-					var obj=Object.keys(data[i]);					
-					var opt = document.createElement('option');
-					opt.text =data[i][obj[1]];
-					opt.value=data[i][obj[0]];;
-					z.add(opt);
-				}
-				if (loader != undefined) loader.style.display = 'none';
-	}
-}
-
-
-function getPerson() {
-	var id = document.getElementById('idp');
-	var tp= document.getElementById('tdo');
-	if (id.value!='' && tp.value!=''){
-		if (loader != undefined) loader.style.display = 'block';
-			if (window.XMLHttpRequest)
-				xmlhttp = new XMLHttpRequest();
-			else
-				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-				xmlhttp.onreadystatechange = function () {
-					if ((xmlhttp.readyState == 4) && (xmlhttp.status == 200)){
-						var cmp=['idp','tdo','no1','no2','ap1','ap2','fen','gen','eap','te1','te2','eda'];
-						for(i=2;i<cmp.length;i++){							
-								document.getElementById(cmp[i]).value='';
-						}
-								
-						try {
-							var rta=JSON.parse(xmlhttp.responseText);
-							if(rta==null){
-								//~ if (loader != undefined) loader.style.display = 'none';
-								//~ alert('No se encontro el Tipo y Documento ingresado, por favor valide');
-								//~ return;
-								
-								rta1=getPersonExt();
-								if(rta1==null){
-									if (loader != undefined) loader.style.display = 'none';
-									//~ //alert('No se encontro el Tipo y Documento ingresado, por favor valide');
-									return;
-								}else{
-									data =rta1;
-									console.log(data);
-									var data=Object.values(data);
-									for(i=0;i<cmp.length;i++){
-										if(i==11){
-										//~ //getEdad(cmp[6]);
-										}else{
-											document.getElementById(cmp[i]).value=data[i];
-										}
-									}
-									changeSelect('idp','tdo','cit');
-								if (loader != undefined) loader.style.display = 'none';
-								}
-								
-								
-								
-							}else{
-								data =rta;
-								console.log(data);
-								var data=Object.values(data);
-								for(i=0;i<cmp.length;i++){
-									if(i==11){
-										//~ getEdad(cmp[6]);
-									}else{
-										document.getElementById(cmp[i]).value=data[i];
-									}
-								}
-								changeSelect('idp','tdo','cit');
-								if (loader != undefined) loader.style.display = 'none';
-							}	
-						} catch (e) {
-							console.log(e);
-							return;
-						}							
-					}
-				}
-				xmlhttp.open("POST", ruta_app,true);
-				xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-				xmlhttp.send('a=get&tb=persona&id='+id.value+'_'+tp.value);
-	}
-}
-function getPersonExt() {
-	var id = document.getElementById('idp');
-	var tp= document.getElementById('tdo');
-	if (id.value!='' && tp.value!=''){
-		if (loader != undefined) loader.style.display = 'block';
-			if (window.XMLHttpRequest)
-				xmlhttp = new XMLHttpRequest();
-			else
-				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-				xmlhttp.onreadystatechange = function () {
-					if ((xmlhttp.readyState == 4) && (xmlhttp.status == 200)){
-						var cmp=['idp','tdo','no1','no2','ap1','ap2','fen','gen','eap','te1','te2','eda'];
-						for(i=2;i<cmp.length;i++){							
-								document.getElementById(cmp[i]).value='';
-						}
-						try {
-							var rta=JSON.parse(xmlhttp.responseText);
-							if(rta==null){
-								if (loader != undefined) loader.style.display = 'none';
-								alert('No se encontro el Tipo y Documento ingresado, por favor valide');
-								return;
-							}else{
-								data =rta;
-								console.log(data);
-								var data=Object.values(data);
-								for(i=0;i<cmp.length;i++){
-									if(i==11){
-										//~ //getEdad(cmp[6]);
-									}else{
-										document.getElementById(cmp[i]).value=data[i];
-									}
-								}
-								changeSelect('idp','tdo','cit');
-								if (loader != undefined) loader.style.display = 'none';
-							}	
-						} catch (e) {
-							console.log(e);
-							return;
-						}							
-					}
-				}
-				xmlhttp.open("POST", ruta_app,true);
-				xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-				xmlhttp.send('a=get&tb=persona_ext&id='+id.value+'_'+tp.value);
-	}
-}
-
 
 function getEdad(a){
 		var f = new Date();
@@ -209,14 +47,12 @@ function grabar(tb='',ev){
 	  if ((cit.value==1 || cit.value==2 || cit.value==3 ||cit.value==5 ||cit.value==7 ||cit.value==8 ||cit.value==12 ||cit.value==14 || cit.value==15) && con.value!=2){
 		alert('El Tipo Cita NO corresponde con el tipo de Consulta,por favor valide e intente nuevamente');
 	  }else{
-		  document.getElementById(tb+'-pro-msj').innerHTML=ajax(ruta_app,"a=gra&tb="+tb,false);
-			if (document.getElementById(tb+'-pro-msj') != undefined)
-				act_lista(tb);
-				act_lista('agendamiento');
+        myFetch(ruta_app,"a=gra&tb="+tb,mod);
+		act_lista(tb);
+		act_lista('agendamiento');
 	  }
   }else{
-	document.getElementById(tb+'-pro-msj').innerHTML=ajax(ruta_app,"a=gra&tb="+tb,false);
-	if (document.getElementById(tb+'-pro-msj') != undefined)
+    myFetch(ruta_app,"a=gra&tb="+tb,mod);
 		act_lista(tb);
 		act_lista('agendamiento');
   }
@@ -233,23 +69,19 @@ function valDate(a){
 	}
 }
 
-function validDate(a){
+/* function validDate(a){
 		let Ini=dateAdd();
 		//26-05-2023
 		let Fin=dateAdd(18);
-	
 	let min=`${Ini.a}-${Ini.m}-${Ini.d}`;
 	//26-05-2023
 	//let min=`2023-05-01`; //26-05-2023
-	let max=`${Fin.a}-${Fin.m}-${Fin.d}`;
-	
+	let max=`${Fin.a}-${Fin.m}-${Fin.d}`;	
 	//~ var max=dayjs(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`).format('YYYY-MM-DD');
 	RangeDateTime(a.id,min,max);
 	validTime('hci');
 }
-
-
-
+ */
 
 function asist(a){
 	//~ var asiste=document.getElementById(a.id);
@@ -272,10 +104,6 @@ function asist(a){
 function validTime(a){
 	RangeDateTime(a,'06:00','19:00');
 }
-
-
-
-
 </script>
 </head>
 <body Onload="actualizar();">
@@ -327,10 +155,10 @@ $digitadores=opc_sql("SELECT `id_usuario`,nombre FROM `usuarios` WHERE`perfil`='
 			<li class='icono filtros'    title='Filtros'      Onclick="showFil(mod);">
 			<li class='icono crear'       title='Crear Cita'     Onclick="mostrar(mod,'pro');"></li>	
 			<li class='icono comentarios'       title='Crear Observaciones'     Onclick="mostrar('observaciones','pro');"></li>
-			<?php $info=datos_mysql("SELECT perfil FROM usuarios WHERE id_usuario='".$_SESSION["us_riesgo"]."'");
+			<?php /* $info=datos_mysql("SELECT perfil FROM usuarios WHERE id_usuario='".$_SESSION["us_riesgo"]."'");
 	if ($info['responseResult'][0]['perfil']=='ADM' || $info['responseResult'][0]['perfil']=='TEC' ){	
 			echo "<li class='icono exportar'      title='Exportar CSV Observaciones'    Onclick=\"ajax(ruta_app, 'a=lis&tb=observaciones', false);csv('observaciones');\"></li>";
-		}?>
+		} */?>
 		</nav>
 		<nav class='menu right' >
 			<li class='icono ayuda'      title='Necesitas Ayuda'            Onclick=" window.open('https://sites.google.com/', '_blank');"></li>

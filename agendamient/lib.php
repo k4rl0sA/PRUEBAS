222a0696aec9184cbf0f97d1e923d41e00ec7563
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-06 17:41:38 -0500

    04-06 Agendamiento 17:41

--- agendamient/lib.php
+++ agendamient/lib.php
@@ -45,7 +45,7 @@ function lis_agendamiento(){
 
 
 	$sql="SELECT idagendamiento ACCIONES,
-idpersona ID,FN_CATALOGODESC(1,tipodoc) Tipo_Documento,
+idpersona ID,FN_CATALOGODESC(1,tipo_doc) Tipo_Documento,
 FN_CATALOGODESC(275,tipo_cita) 'Tipo Cita',`fecha_cita`,`hora_cita`,fecha_llamada 'Recordación Cita',FN_CATALOGODESC(40,`estado`) Estado
 from agendamiento LEFT JOIN person P ON A.idpeople=P.idpeople left JOIN usuarios U ON A.usu_creo = U.id_usuario WHERE estado not in (1,2,3,5) ";
 	$sql.=whe_agendamiento();

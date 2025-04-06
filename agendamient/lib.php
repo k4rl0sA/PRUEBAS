0aa031fc72466542ac7bf36d31c027108c223fbc
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-06 18:43:56 -0500

    04-06 Agendamiento 18:43

--- agendamient/lib.php
+++ agendamient/lib.php
@@ -95,12 +95,28 @@ if($_POST['id']){
 }
 } 
 
+/* function get_persona(){
+		$id=divide($_REQUEST['id']);
+		$sql="SELECT 
+T1.idpersona,T1.tipo_doc,T1.nombre1,T1.nombre2,T1.apellido1,T1.apellido2,T1.fecha_nacimiento,T1.genero,T1.eapb,T2.telefono1,T2.telefono2,T3.tipo_consulta,T3.punto_atencion,T3.tipo_cita,T3.fecha_cita,T3.hora_cita,T3.nombre_atendio,T3.observac_cita
+FROM personas T1
+RIGHT join caracterizacion T2 ON T1.ficha=T2.idficha
+LEFT join agendamiento T3 ON T1.idpersona=T3.id_persona
+WHERE T1.idpersona='".$id[0]."' AND T1.tipo_doc=upper('".$id[1]."')";
+		$info=datos_mysql($sql);
+		return json_encode($info['responseResult'][0]); 
+} */
 function get_persona(){
     if ($_REQUEST['id']){
 		$id=divide($_REQUEST['id']);
-		$sql="SELECT T1.idpersona,T1.tipo_doc,T1.nombre1,T1.nombre2,T1.apellido1,T1.apellido2,T1.fecha_nacimiento,T1.sexo
+		$sql="SELECT T1.idpersona,T1.tipo_doc,T1.nombre1,T1.nombre2,T1.apellido1,T1.apellido2,T1.fecha_nacimiento
+        ,concat('Años= ',timestampdiff(YEAR,T2.fecha_nacimiento,curdate()),
+    ' Meses= ',MONTH(CURDATE()) - MONTH(T2.fecha_nacimiento) + 12 * IF( MONTH(CURDATE()) < MONTH(T2.fecha_nacimiento),1, IF(MONTH(CURDATE())=MONTH(T2.fecha_nacimiento),IF (DAY(CURDATE()) < DAY(T2.fecha_nacimiento),1,0),0)) - IF(MONTH(CURDATE())<>MONTH(T2.fecha_nacimiento), (DAY(CURDATE()) < DAY(T2.fecha_nacimiento)), IF (DAY(CURDATE()) < DAY(T2.fecha_nacimiento),1,0 ) ), ' Días= ',DAY(CURDATE())-DAY(T2.fecha_nacimiento)+30*(DAY(CURDATE()) < DAY(T2.fecha_nacimiento))) edad,
+    T1.sexo,T1.eapb,telefono1,telefono2,telefono3,T4.punto_atencion,T4.tipo_cita,T4.fecha_cita,T4.hora_cita,T4.nombre_atendio,T4.observac_cita
 	 FROM person T1
 	 RIGHT join hog_agen T2 ON T1.idpeople=T2.idpeople
+     LEFT JOIN hog_fam T3 ON T1.vivipersona=T3.id_fam
+     RIGHT JOIN agendamiento T4 ON T1.idpeople=T4.idpeople
 	 WHERE T1.idpersona='".$id[0]."' AND T1.tipo_doc=upper('".$id[1]."')";
 		$info=datos_mysql($sql);
 		if (!$info['responseResult']) {

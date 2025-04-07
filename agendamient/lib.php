b7daa88d5655112a729c2a87929664c2bc295b5a
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-06 22:13:16 -0500

    04-06 Agendamiento 22:13

--- agendamient/lib.php
+++ agendamient/lib.php
@@ -324,7 +324,7 @@ function gra_agendamiento(){
 			$upfr=', Sin embargo, No se pudo realizar la actualización de la cita, en el campo realizado en la tabla frecuencia de uso.';
 		}else{$upfr='';}
 	}
-	return $rta." ".$upfr;
+	return $rta1." ".$upfr;
  }
 }
 

692e7977ec0b455ce4f10f59c9d2ff000087d650
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-06 19:46:56 -0500

    04-06 Agendamiento 19:46

--- agendamient/index.php
+++ agendamient/index.php
@@ -38,7 +38,7 @@ function getPerson() {
 				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 				xmlhttp.onreadystatechange = function () {
 					if ((xmlhttp.readyState == 4) && (xmlhttp.status == 200)){
-						var cmp=['idp','tdo','no1','no2','ap1','ap2','fen','gen','eap','te1','te2','eda'];
+						var cmp=['idp','tdo','no1','no2','ap1','ap2','fen','eda','gen','eap','te1','te2','eda'];
 						for(i=2;i<cmp.length;i++){							
 								document.getElementById(cmp[i]).value='';
 						}

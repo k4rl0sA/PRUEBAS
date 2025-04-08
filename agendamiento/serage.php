45f86184c964c2ce91cabfeea6f5f9529f1c68ec
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-08 11:18:43 -0500

    08-04 Agenda 11:18

--- agendamiento/serage.php
+++ agendamiento/serage.php
@@ -86,7 +86,7 @@ function cmp_servagen(){
 	$o='prufin';
     $c[]=new cmp($o,'e',null,'SERVICIO AGENDAMIENTO',$w);
     $c[]=new cmp('fecha_sol','d',10,$e,$w.' '.$o,'Fecha Solicitud','fecha_even',null,null,true,true,'','col-15',"validDate(this,$days,0);");
-    $c[]=new cmp('tipo_cons','s',3, $e,$w,'Tipo de Consulta','evento',null,null,true,true,'','col-25');
+    $c[]=new cmp('tipo_cons','s',3, $e,$w,'Tipo de Consulta','consulta',null,null,true,true,'','col-25');
     $c[]=new cmp('servicio','s',3, $e,$w,'Servicio','evento',null,null,true,true,'','col-3');
   for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
 	return $rta;
@@ -100,7 +100,9 @@ function opc_tipodoc($id=''){
 function opc_sexo($id=''){
   return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=21 and estado='A' ORDER BY 1",$id);
 }
-
+function opc_consulta($id=''){
+  return opc_sql('SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=281 and estado="A" ORDER BY 1',$id);
+}
 function opc_evento($id=''){
   $d=get_persona();
   if($d['sexo']=='M'){

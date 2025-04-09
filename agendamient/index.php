9f37cc76b7938c72e88e2799450f64933d8a41f8
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-09 13:46:10 -0500

    04-09 Agenda 13:46

--- agendamient/index.php
+++ agendamient/index.php
@@ -131,7 +131,7 @@ function grabar(tb='',ev){
   if (tb=='agendamiento'){
 	  var con=document.getElementById('con'),
 	  cit=document.getElementById('cit');
-	  if ((cit.value==1 || cit.value==2 || cit.value==3 ||cit.value==5 ||cit.value==7 ||cit.value==8 ||cit.value==12 ||cit.value==14 || cit.value==15) && con.value!=2){
+	  if ((cit.value==1 || cit.value==2 || cit.value==3 ||cit.value==5 ||cit.value==7 ||cit.value==8 ||cit.value==12 ||cit.value==14 || cit.value==15)){
 		alert('El Tipo Cita NO corresponde con el tipo de Consulta,por favor valide e intente nuevamente');
 	  }else{
         myFetch(ruta_app,"a=gra&tb="+tb,mod);

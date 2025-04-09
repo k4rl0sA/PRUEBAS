be6e5d22950c6cb1b04d6f2f2f7231103029fc19
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-09 13:10:05 -0500

    04-09 Agenda 13:10

--- agenFrecuencia/lib.php
+++ agenFrecuencia/lib.php
@@ -40,14 +40,14 @@ function whe_frecuenciauso() {
 }
 
 function lis_frecuenciauso(){
-	$info=datos_mysql("SELECT COUNT(*) total FROM `frecuenciauso` A LEFT JOIN person P ON A.idpeople=P.idpeople left JOIN usuarios U ON A.usu_creo = U.id_usuario WHERE U.subred IN (select subred from usuarios where id_usuario='{$_SESSION['us_sds']}') ".whe_frecuenciauso());
+	$info=datos_mysql("SELECT COUNT(*) total FROM `frecuenciauso` A LEFT JOIN person P ON A.idpeople=P.idpeople left JOIN usuarios U ON A.usu_creo = U.id_usuario ".whe_frecuenciauso());
 	$total=$info['responseResult'][0]['total'];
 	$regxPag=5;
 	$pag=(isset($_POST['pag-frecuenciauso']))? ($_POST['pag-frecuenciauso']-1)* $regxPag:0;
 //~ echo $sql;
 $sql="SELECT idfrecuencia ACCIONES,
 `idpersona` ID,FN_CATALOGODESC(1,tipo_doc) Tipo_Documento,FN_CATALOGODESC(274,`punto_atencion`) 'Punto de Control',FN_CATALOGODESC(275,tipo_cita) 'Tipo Cita',`realizada`,A.fecha_create,A.estado
-from frecuenciauso A LEFT JOIN person P ON A.idpeople=P.idpeople left JOIN usuarios U ON A.usu_creo = U.id_usuario WHERE U.subred IN (select subred from usuarios where id_usuario='{$_SESSION['us_sds']}') ";
+from frecuenciauso A LEFT JOIN person P ON A.idpeople=P.idpeople left JOIN usuarios U ON A.usu_creo = U.id_usuario ";
 	$sql.=whe_frecuenciauso();
 	$sql.="  ORDER BY 8 DESC";
 	$sql.=' LIMIT '.$pag.','.$regxPag;

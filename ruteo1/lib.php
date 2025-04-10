0291f2a1592143bc43858b5df6ba9eee1e46b98e
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-10 09:42:01 -0500

    04-10 Agendamiento 09:41

--- ruteo1/lib.php
+++ ruteo1/lib.php
@@ -30,6 +30,7 @@ LEFT JOIN apro_terr A ON G.idgeo = A.idgeo  ".whe_rute();
 	$sql.=" ORDER BY er.fecha_create";
 	$sql.=' LIMIT '.$pag.','.$regxPag;
 	// echo($sql);
+	$_SESSION['sql_rute']="select 1 +1;";
 		$datos=datos_mysql($sql);
 	return create_table($total,$datos["responseResult"],"rute",$regxPag);
 } 

488ba75b57cd7ef9b1e0f94e30d44b7b5bc98464
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-11 12:26:04 -0500

    04-11 Agendamiento 12:26

--- agendamient/serage.php
+++ agendamient/serage.php
@@ -196,7 +196,7 @@ function get_persona(){
     DATEDIFF(CURDATE(),DATE_ADD(fecha_nacimiento, INTERVAL TIMESTAMPDIFF(YEAR, fecha_nacimiento, CURDATE()) YEAR)) % 30 AS dias
 		FROM person P
     WHERE P.idpeople='{$id[0]}'"; 
-    echo $sql;
+    // echo $sql;
     print_r($_REQUEST);
     $info=datos_mysql($sql);
     return $info['responseResult'][0];

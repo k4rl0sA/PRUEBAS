51554eb49571d82f84985fa4272dfa139b44595f
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-13 01:55:08 -0500

    04-13 Agendamiento 01:55

--- riskFam/3/lib.php
+++ riskFam/3/lib.php
@@ -10,38 +10,6 @@ $data = [
         "nationality" => "Colombiana",
         "birthDate" => "1990-05-15",
         "age" => 33
-    ],
-    [
-        "document" => "987654321",
-        "sex" => "Femenino",
-        "gender" => "Mujer",
-        "nationality" => "Mexicana",
-        "birthDate" => "1985-12-01",
-        "age" => 37
-    ],
-    [
-        "document" => "456789123",
-        "sex" => "Masculino",
-        "gender" => "Otro",
-        "nationality" => "Argentina",
-        "birthDate" => "2000-07-20",
-        "age" => 22
-    ],
-    [
-        "document" => "321654987",
-        "sex" => "Femenino",
-        "gender" => "Mujer",
-        "nationality" => "Española",
-        "birthDate" => "1978-03-10",
-        "age" => 45
-    ],
-    [
-        "document" => "789123456",
-        "sex" => "Masculino",
-        "gender" => "Hombre",
-        "nationality" => "Venezolana",
-        "birthDate" => "1995-11-05",
-        "age" => 27
     ]
 ];
 

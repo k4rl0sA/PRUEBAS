527a2d88facd80476ab7c47a766a33250a6d11b7
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-19 11:19:38 -0500

    04-19 Agendamiento 11:19

--- riskFam/3/lib.php
+++ riskFam/3/lib.php
@@ -1,6 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 header('Content-Type: application/json');
-require_once "gestion.php";
+require_once "../../gestion.php";
+
 
 // Conexión a la base de datos
 $conn = conDB(); // Asumiendo que `conDB()` está en gestion.php

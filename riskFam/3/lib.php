2e1d20017bf2d3d0b249168941ed0f8359403b85
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-19 00:41:26 -0500

    04-19 Agendamiento 12:41

--- riskFam/3/lib.php
+++ riskFam/3/lib.php
@@ -3,6 +3,7 @@ ini_set('session.cookie_httponly', 1);
 ini_set('session.cookie_secure', 1);
 ini_set('session.use_strict_mode', 1);
 session_start();
+require_once '../libs/gestion.php';
 ini_set('display_errors','1');
 setlocale(LC_TIME, 'es_CO');
 date_default_timezone_set('America/Bogota');

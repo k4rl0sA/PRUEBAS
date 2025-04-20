28b6b606b6f0b52a9981d69ee0c09f87940f9f14
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-20 02:45:57 -0500

    04-20 Agendamiento 02:45

--- afrecuencia/lib.php
+++ afrecuencia/lib.php
@@ -157,6 +157,10 @@ function opc_punto_atenc($id=''){
 	return opc_sql("SELECT `idcatadeta`,concat(idcatadeta,' - ',descripcion) FROM `catadeta` WHERE idcatalogo=274 and estado='A'  AND valor=(select subred from usuarios where id_usuario='{$_SESSION['us_sds']}') ORDER BY LENGTH(idcatadeta), idcatadeta",$id);
 }
 function opc_tipo_cita($id=''){
+	return opc_sql("SELECT `idcatadeta`,concat(idcatadeta,' - ',descripcion) FROM `catadeta` WHERE idcatalogo=275 and estado='A' ORDER BY LENGTH(idcatadeta), idcatadeta",$id);	
+}
+
+function opc_tipcit($id=''){
 	$id = divide($_REQUEST['id']);
 	$persona = get_persona();
 	$edad = $persona['anos'];

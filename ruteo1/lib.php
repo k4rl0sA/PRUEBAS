ab7458d39ee6e7d04f2d895e5a72ca5073bd3f99
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-24 22:00:07 -0500

    04-24 Ruteo 22:00

--- ruteo1/lib.php
+++ ruteo1/lib.php
@@ -28,7 +28,7 @@ function lis_rute(){
 	$total=$info['responseResult'][0]['total'];
 	$regxPag=10;
 	$pag=(isset($_POST['pag-rute']))? ($_POST['pag-rute']-1)* $regxPag:0;
-	$sql="SELECT er.id_ruteo AS ACCIONES, er.idgeo AS Cod_Predio, FN_CATALOGODESC(235,tipo_prior) AS Grupo_Poblacion_Priorizada, er.documento AS Documento_Usuario,er.nombres AS Nombre_Usuario,FN_CATALOGODESC(218,er.perfil1) AS Interviene, FN_CATALOGODESC(269,er.actividad1) AS Realizar ,er.estado
+	$sql="SELECT er.id_ruteo AS ACCIONES, er.idgeo AS Cod_Predio, FN_CATALOGODESC(235,tipo_prior) AS Grupo_Poblacion_Priorizada, er.documento AS Documento_Usuario,er.nombres AS Nombre_Usuario,FN_CATALOGODESC(218,er.perfil1) AS Interviene,er.estado
   FROM eac_ruteo  er  
   LEFT JOIN hog_geo G ON er.idgeo = G.idgeo ";
   if (perfil1()!='ADM') $sql. = " LEFT JOIN apro_terr A ON G.territorio = A.territorio  "; 

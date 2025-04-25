8912729007dbe2adae054553ae8ee8dcc5a7a0cf
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-24 21:53:01 -0500

    04-24 Ruteo 21:52

--- ruteo1/lib.php
+++ ruteo1/lib.php
@@ -23,7 +23,7 @@ function lis_rute(){
 	if (perfil1()!='ADM') $tot. = " LEFT JOIN apro_terr A ON G.territorio = A.territorio "; 
 	$tot.=whe_rute(); */
 	$tot="SELECT COUNT(*) total from eac_ruteo er 	LEFT JOIN hog_geo G ON er.idgeo = G.idgeo  LEFT JOIN apro_terr A ON G.territorio = A.territorio". whe_rute();
-	// var_dump($tot);
+	var_dump($tot);
 	$info=datos_mysql($tot);
 	$total=$info['responseResult'][0]['total'];
 	$regxPag=10;

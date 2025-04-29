8139ee8544fa0c1ed9c5d535b92cffa14e6e0b48
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-04-29 00:07:05 -0500

    04-28 FixAgendamiento  00:06

--- afrecuencia/lib.php
+++ afrecuencia/lib.php
@@ -195,6 +195,8 @@ function opc_tdocit($id=''){
 	}
 	//aplica para ambos sexos
 	if ($edad >= 0 && $edad <18) $categorias[] = 24;
+	if ($edad >= 50)$categorias[] = 16;
+	if ($edad > 18)$categorias[] = 20;
 	
 	if ($sexo === 'M') { // Mujer
 		if ($edad >= 25 && $edad <= 69) $categorias[] = 14;
@@ -203,10 +205,8 @@ function opc_tdocit($id=''){
 	} elseif ($sexo === 'H') { // Hombre
 		if ($edad >= 50 && $edad <= 75) $categorias[] = 13;
 	}
-	if ($edad >= 50) {
-		$categorias[] = 16;
-	}
-	$categorias_comunes = [10, 15, 9, 17, 18, 20, 21, 22, 23, 25, 26, 27];
+	
+	$categorias_comunes = [10, 15, 9, 17, 18, 21, 22, 23, 25, 26, 27];
 	$categorias = array_unique(array_merge($categorias, $categorias_comunes));
 	$lista = implode(',', $categorias);
 	$sql = "SELECT idcatadeta, descripcion
--- agendamient/serage.php
+++ agendamient/serage.php
@@ -129,19 +129,19 @@ function opc_tipo_consservicio($id = '') {
       }
   }
   //aplica para ambos sexos
-  if ($edad >= 0 && $edad <18) $categorias[] = 24;
-  
-  if ($sexo === 'M') { // Mujer
-      if ($edad >= 25 && $edad <= 69) $categorias[] = 14;
-      if ($edad >= 50 && $edad <= 69) $categorias[] = 12;
-      if ($edad >= 10 && $edad <= 59) $categorias[] = 19;
-  } elseif ($sexo === 'H') { // Hombre
-      if ($edad >= 50 && $edad <= 75) $categorias[] = 13;
-  }
-  if ($edad >= 50) {
-      $categorias[] = 16;
-  }
-  $categorias_comunes = [10, 15, 9, 17, 18, 20, 21, 22, 23, 25, 26, 27];
+	if ($edad >= 0 && $edad <18) $categorias[] = 24;
+	if ($edad >= 50)$categorias[] = 16;
+	if ($edad > 18)$categorias[] = 20;
+	
+	if ($sexo === 'M') { // Mujer
+		if ($edad >= 25 && $edad <= 69) $categorias[] = 14;
+		if ($edad >= 50 && $edad <= 69) $categorias[] = 12;
+		if ($edad >= 10 && $edad <= 59) $categorias[] = 19;
+	} elseif ($sexo === 'H') { // Hombre
+		if ($edad >= 50 && $edad <= 75) $categorias[] = 13;
+	}
+	
+	$categorias_comunes = [10, 15, 9, 17, 18, 21, 22, 23, 25, 26, 27,28,29];
   $categorias = array_unique(array_merge($categorias, $categorias_comunes));
   $lista = implode(',', $categorias);
   $sql = "SELECT idcatadeta, descripcion

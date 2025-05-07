83f09aba20ace87486fdc3e00545b70eefa31489
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-07 15:26:51 -0500

    05-07 rqc 15:26

--- srq/lib.php
+++ srq/lib.php
@@ -131,7 +131,7 @@ function men_tamsrq(){
 function cap_menus($a,$b='cap',$con='con') {
     $rta = "";
     $acc=rol($a);
-    if ($a=='srq' && isset($acc['crear']) && $acc['crear']=='SI') {  
+    if ($a=='tamsrq' && isset($acc['crear']) && $acc['crear']=='SI') {  
         $rta .= "<li class='icono $a grabar' title='Grabar' OnClick=\"grabar('$a',this);\"></li>";
     }
     $rta .= "<li class='icono $a actualizar' title='Actualizar' Onclick=\"act_lista('$a',this);\"></li>";  

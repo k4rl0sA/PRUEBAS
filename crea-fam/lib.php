630dd39079fd1063a104a044750064efdc22edfa
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-07 12:30:02 -0500

    05-07 rqc 12:29

--- crea-fam/lib.php
+++ crea-fam/lib.php
@@ -939,6 +939,7 @@ function formato_dato($a,$b,$c,$d){
 				$rta .= acceso('tamApgar') ? "<li title='Tamizaje Apgar' onclick=\"mostrar('tamApgar','pro',event,'','../apgar/lib.php',7);Color('datos-lis');\"><i class='fa-solid fa-people-roof ico' id='".$c['ACCIONES']."'></i></li>": '';
 			}
 		}
+
 		if (is_array($tamiz) && in_array('tamfindrisc', $tamiz)) {
 			$rta .= acceso('tamfindrisc') ? "<li title='Tamizaje Findrisc' onclick=\"mostrar('tamfindrisc','pro',event,'','../findrisc/lib.php',7);Color('datos-lis');\"><i class='fa-solid fa-hospital-user ico' id='".$c['ACCIONES']."'></i></li>": '';
 		}
@@ -968,6 +969,8 @@ function formato_dato($a,$b,$c,$d){
 			$perfiles = ['ADM','LARREL', 'FISREL', 'LEFREL', 'TSOREL','PROFAM','TERAPEUTA'];
 			$rta .= acceso('sesiones') ? "<li title='Sesiónes' onclick=\"mostrar('sesiones','pro',event,'','../relevo/sesiones.php',7);Color('datos-lis');\"> <i class=' fa-solid fa-address-book ico' id='{$c['ACCIONES']}' onclick=\"setTimeout(chanActi,300,'rel_validacion3','act',['" . implode("','", $perfiles) . "']);\"></i></li>" : "";
 		}
+		$rta .= acceso('psicologia') ? "<li title='RQC' onclick=\"mostrar('rqc','pro',event,'','../rqc/lib.php',7,'rqc');Color('datos-lis');\"><i class='fas fa-notes-medical ico' id='{$c['ACCIONES']}'></i></li>":"";
+
 		$rta .= acceso('psicologia') ? "<li title='Psicologia Sesión 1' onclick=\"mostrar('psicologia','pro',event,'','../psicologia/lib.php',7,'psicologia');Color('datos-lis');\"><i class=' fa-solid fa-person-circle-question ico' id='{$c['ACCIONES']}'></i></li>":"";
 
 			if (psiSesi2($c['ACCIONES'])) {

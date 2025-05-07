d69e10d048a757bd8d0c52482209e1052d74d2bb
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-07 14:06:23 -0500

    05-07 rqc 14:06

--- libs/gestion.php
+++ libs/gestion.php
@@ -609,11 +609,22 @@ function divide($a){
 	return ($id);
 }
 
-function rol($a){ //a=modulo, b=perfil c=componente
+/* function rol($a){ //a=modulo, b=perfil c=componente
 	$rta=array();
 	$sql="SELECT perfil,componente,crear,editar,consultar,ajustar,importar FROM adm_roles WHERE modulo = '".$a."' and perfil = (select perfil from usuarios where id_usuario='".$_SESSION['us_sds']."') AND componente= (select componente from usuarios where id_usuario='".$_SESSION['us_sds']."') AND estado = 'A'";
   // echo $sql;
 	$data=datos_mysql($sql);
+	if ($data && isset($data['responseResult'][0])) {
+        $rta = $data['responseResult'][0];
+  }
+	return $rta;
+} */
+
+function rol($a){ //a=modulo, b=perfil c=componente
+	$rta=array();
+	$sql="SELECT perfil,componente,crear,editar,consultar,ajustar,importar FROM adm_roles WHERE modulo = '".$a."' and perfil = FN_PERFIL('".$_SESSION['us_sds']."') AND componente=FN_COMPONENTE('".$_SESSION['us_sds']."') AND estado = 'A'";
+	$data=datos_mysql($sql);
+  //print_r($data);
 	if ($data && isset($data['responseResult'][0])) {
         $rta = $data['responseResult'][0];
   }

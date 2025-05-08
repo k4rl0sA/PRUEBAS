0e3b44af5ec64a110d6cdf64868ee0600f176735
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-08 14:00:51 -0500

    05-08 srq 14:00

--- sesicole/lib.php
+++ sesicole/lib.php
@@ -51,7 +51,7 @@ WHERE gg.estado_v IN ('7')
    ".whe_sesigcole()." 
 	GROUP BY sc.id_cole, sc.fecha, sc.tipo_activ, sc.lugar,sc.tematica1, sc.des_temati1, u.nombre, sc.fecha_create, sc.estado 
 	LIMIT $pag, $regxPag"; 
-//   echo $sql;
+   echo $sql;
 		$datos=datos_mysql($sql);
 	return create_table($total,$datos["responseResult"],"sesigcole",$regxPag); 
 }else{

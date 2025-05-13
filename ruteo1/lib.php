26cdaf873e8d05762b8f1b1bf171774869335351
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-13 09:48:10 -0500

    05-13 CargueRuteo 09:48

--- ruteo1/lib.php
+++ ruteo1/lib.php
@@ -19,13 +19,13 @@ else {
 }
 
 function lis_rute(){
-	$info = datos_mysql("SELECT perfil FROM usuarios  where id_usuario='".$_SESSION['us_sds']."';" );
-	$perfil = $info['responseResult'][0]['perfil'];
-	// $jAproTerr = ($_SESSION['us_sds'] != '80811594') ? "LEFT JOIN apro_terr A ON G.territorio = A.territorio" : ""; // Condiciona el JOIN
-		$jAproTerr = ($perfil != 'ADM') ? "LEFT JOIN apro_terr A ON G.territorio = A.territorio" : ""; // Condiciona el JOIN
-    $info = datos_mysql("SELECT COUNT(*) total 
-        FROM eac_ruteo er 
-        LEFT JOIN hog_geo G ON er.idgeo = G.idgeo 
+	// $info = datos_mysql("SELECT perfil FROM usuarios  where id_usuario='".$_SESSION['us_sds']."';" );
+	// $perfil = $info['responseResult'][0]['perfil'];
+
+	$jAproTerr = ($_SESSION['us_sds'] != '80811594') ? "LEFT JOIN apro_terr A ON G.territorio = A.territorio" : ""; // Condiciona el JOIN
+		// $jAproTerr = ($perfil != 'ADM') ? "LEFT JOIN apro_terr A ON G.territorio = A.territorio" : ""; // Condiciona el JOIN
+    $info = datos_mysql("SELECT COUNT(*) total FROM eac_ruteo er 
+	LEFT JOIN hog_geo G ON er.idgeo = G.idgeo 
         $jAproTerr " . whe_rute());
     $total = $info['responseResult'][0]['total'];
     $regxPag = 10;

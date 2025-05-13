e79a3405347bfcb9cb29031f2e71467ca5b15a2e
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-13 15:12:25 -0500

    05-13 CargueRuteo 15:12

--- ruteo1/cargaRuteo.php
+++ ruteo1/cargaRuteo.php
@@ -54,7 +54,7 @@ function cmp_routing(){
     $o='asignacion';
     $c[]=new cmp($o,'e',null,'ASIGNACIÓN',$w);
     $c[]=new cmp('perfil1','s','3',$d['perfil1']??'',$w.' '.$o,'Perfil','perfil1',null,'',true,true,'','col-3',"selectDepend('perfil1','actividad1','cargaRuteo.php');");
-    $c[]=new cmp('actividad1','s','11',$d['actividad1']??'',$w.' '.$o,'Actividad','actividad1',null,'',true,true,'','col-3');
+    $c[]=new cmp('actividad1','s','11',$d['actividad1']??'',$w.' '.$o,'Usuario (Territorio Acorde al Predio)','actividad1',null,'',true,true,'','col-3');
 
 /*  $c[]=new cmp('estado_ruteo','s','3',$d['estado_ruteo']??'',$w.' '.$o,'Estado Ruteo','estado_ruteo',null,'',true,true,'','col-3');
     $c[]=new cmp('estado_rut','s','10',$d['estado_rut']??'',$w.' '.$o,'Estado Ruta','estado_rut',null,'',true,true,'','col-3');

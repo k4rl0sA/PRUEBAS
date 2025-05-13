cb8b01aa002808a2c988cf12ac54a02df7c5a20d
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-13 09:51:39 -0500

    05-13 CargueRuteo 09:51

--- ruteo1/lib.php
+++ ruteo1/lib.php
@@ -21,7 +21,7 @@ else {
 function lis_rute(){
 	$info = datos_mysql("SELECT perfil FROM usuarios  where id_usuario='".$_SESSION['us_sds']."';" );
 	$perfil = $info['responseResult'][0]['perfil'];
-var_dump($perfil);
+// var_dump($perfil);
 	// $jAproTerr = ($_SESSION['us_sds'] != '80811594') ? "LEFT JOIN apro_terr A ON G.territorio = A.territorio" : ""; // Condiciona el JOIN
 	$jAproTerr = ($perfil != 'ADM') ? " LEFT JOIN apro_terr A ON G.territorio = A.territorio" : ""; // Condiciona el JOIN
 
@@ -74,7 +74,9 @@ function whe_rute() {
     $perfil = perfil1();
     $sql1 = " WHERE G.subred = (SELECT subred FROM usuarios WHERE id_usuario = '" . $us_sds . "')";
     // Agregar condición de apro_terr solo si el perfil no es 'ADM'
-    if ($doc_asignado != '80811594') {
+	$info = datos_mysql("SELECT perfil FROM usuarios  where id_usuario='".$_SESSION['us_sds']."';" );
+	$perfil = $info['responseResult'][0]['perfil'];
+    if ($perfil != 'ADM') {
         $sql1 .= " AND A.doc_asignado = " . intval($doc_asignado);
     }
     if ($_POST['frut']) {

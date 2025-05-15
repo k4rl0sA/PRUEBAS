690ccea18bf1eb91cee04c82ec658f32cf42ac09
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-15 15:12:55 -0500

    05-15 FixRiskFam 15:12

--- admin/lib.php
+++ admin/lib.php
@@ -2315,6 +2315,31 @@ function lis_frecuencia($txt){
 		$rta = array('type' => 'OK','file'=>$txt);
 	echo json_encode($rta);
 }
+
+function lis_agendamiento($txt){
+	$sql="SELECT G.idgeo 'Cod Predio',FN_CATALOGODESC(283,G.territorio) 'Territorio',H.id_fam 'Cod Familia',A.idpeople 'Cod Persona',P.idpersona 'Documento',FN_CATALOGODESC(1,P.tipo_doc) 'Tipo Documento',
+P.fecha_nacimiento 'Fecha de Nacimiento',P.sexo,FN_CATALOGODESC(274,A.punto_atencion ) 'Punto de Atención',FN_CATALOGODESC(275,A.tipo_cita) 'Tipo de Cita',
+	A.fecha_create 'Fecha de Asignación',A.fecha_cita 'Fecha de la Cita',A.hora_cita 'Hora de la Cita',A.nombre_atendio 'Nombre quien Atendió Llamada',A.usu_creo 'Digitador',A.observac_cita 'Observación Cita',IFNULL(A.fecha_llamada,'00-00-0000') 'Fecha Recordación',
+	ifnull(A.nombre_llamada,'-') 'Nombre quien Recibió Llamada' ,ifnull(A.confirma_cita,'-') 'Confirmo Cita',ifnull(A.msjtxt,'-') 'Desea Envio de Msj',
+	ifnull(A.usu_update,'-') 'Digitador1',ifnull(A.observac_llamadas,'-') 'Observaciones de Recordación',ifnull(A.fecha_llamada2,'-') 'Fecha Llamada por Efectividad',ifnull(A.nombre_llamada2,'-') 'Nombre quien Contesto Llamada',ifnull(FN_CATALOGODESC(41,A.motivo_inasistencia),'-') 'Motivo de la Inasistencia',ifnull(A.reasigno,'-') 'Se reasigno la Cita',ifnull(A.usu_update,'-') 'Digitador2',ifnull(A.observac_llamada2,'-') 'Observaciones de Inasistencia',FN_CATALOGODESC(276,A.estado) 'Estado'
+FROM agendamiento A
+	LEFT JOIN person P ON A.idpeople = P.idpeople
+	LEFT JOIN usuarios U ON A.usu_creo = U.id_usuario
+	LEFT JOIN usuarios U1 ON A.usu_update = U1.id_usuario
+	LEFT JOIN hog_fam H ON P.vivipersona = H.id_fam
+	LEFT JOIN hog_geo G ON H.idpre = G.idgeo 
+	 WHERE 1 ";
+	if (perfilUsu()!=='ADM')	$sql.=whe_subred();
+	$sql.=whe_date();
+	// echo $sql;
+	$tot="SELECT COUNT(*) total from hog_agen A LEFT JOIN person P ON A.idpeople = P.idpeople LEFT JOIN usuarios U ON A.usu_creo = U.id_usuario LEFT JOIN usuarios U1 ON A.usu_update = U1.id_usuario LEFT JOIN hog_fam H ON P.vivipersona = H.id_fam LEFT JOIN hog_geo G ON H.idpre = G.idgeo WHERE 1 ";	
+	if (perfilUsu()!=='ADM')	$tot.=whe_subred();
+	$tot.=whe_date();
+	$_SESSION['sql_'.$txt]=$sql;
+	$_SESSION['tot_'.$txt]=$tot;
+	$rta = array('type' => 'OK','file'=>$txt);
+	echo json_encode($rta);
+}
 	
 
 function whe_subred() {

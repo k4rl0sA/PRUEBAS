64843be066b9d002407f925e232be235b3ee9726
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-28 12:56:34 -0500

    05-28 FixRuteo 12:56

--- libs/js/x.js
+++ libs/js/x.js
@@ -1251,13 +1251,19 @@ function enabRutOthSub() {
     });
 }
 function enabRutAgen() {
+	const conditions = [
+		{ id: 'estado_agenda', value: '1', compare: true }
+	];
+EnabDepeDynamic(['AGe'], conditions);
+}
+/* function enabRutAgen2() {
 	const estadoAgenda = document.getElementById('estado_agenda');
 	const isEnabled = estadoAgenda && (estadoAgenda.value === '1' || estadoAgenda.value === '11');
 	const elements = document.querySelectorAll('select.AGe, input.AGe, textarea.AGe');
 	elements.forEach(element => {
 		enaFie(element, !isEnabled);
 	});
-}
+} */
 function enabRutRech() {
 	const conditions = [
 		{ id: 'estado_agenda', value: '2', compare: true }

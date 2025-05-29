e7ea192b6fa1fbfee731c0e7434bb0b8a6090aee
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-05-29 11:14:25 -0500

    05-29 Dashboard  11:14

---
+++ adm-dashboard/lib.php
@@ -0,0 +1,29 @@
+{
+  "viviendas": 1549914,
+  "hogares": 1570758,
+  "personas": 3553128,
+  "mujeres_gestantes": 25933,
+  "personas_discapacidad": 155409,
+  "menores_5": 341866,
+  "mayores_60": 659383,
+  "nivel_educativo": {
+    "labels": ["Preescolar", "Básica Primaria", "Básica Secundaria", "Media", "Técnica", "Profesional", "Maestría", "Doctorado", "Ninguno"],
+    "values": [147700, 1400000, 930700, 80800, 227700, 79400, 2200, 367, 363000]
+  },
+  "etnica": {
+    "labels": ["Indígena", "ROM", "Raizal", "Negro(a)", "Afrocolombiano", "Ninguna"],
+    "values": [297600, 796, 10000, 65300, 167100, 2500000]
+  },
+  "lactancia": {
+    "labels": ["Sí", "No"],
+    "values": [2.01, 97.99]
+  },
+  "sexo": {
+    "labels": ["Hombre", "Mujer", "Indeterminado"],
+    "values": [46.72, 53.21, 0.08]
+  },
+  "estrato": {
+    "labels": ["Bajo", "Medio-Bajo", "Medio", "Alto"],
+    "values": [34.21, 15.74, 4.52, 0.04]
+  }
+}
